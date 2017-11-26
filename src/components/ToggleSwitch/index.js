ddb29b12151b93bf688637b78430139938c381fb
Lior Belinsky <liorbe@wix.com>
2017-11-26 14:35:39 +0200

    wix-ui-tpa: fix import path

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {func, array} from 'prop-types';
 import CoreToggleSwitch from 'wix-ui-core/dist/src/components/ToggleSwitch';
-import {ResponsiveThemedComponent} from '../ResponsiveThemedComponent';
+import {ResponsiveThemedComponent} from '../../ResponsiveThemedComponent';
 
 const ToggleSwitch = ({theme, events, ...coreProps}) => (
   <ResponsiveThemedComponent theme={theme} events={events}>
