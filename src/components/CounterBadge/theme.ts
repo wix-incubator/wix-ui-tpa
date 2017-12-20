c4d0a1bc2d0f57c44ba49938fc1015d7452fa9df
Rokas Mikalkevičius <rokasm@wix.com>
2017-12-20 12:50:59 +0200

    Make it safer

--- src/components/CounterBadge/theme.ts
+++ src/components/CounterBadge/theme.ts
@@ -1,17 +1,17 @@
 export const theme = ({wixBindings, colors, fonts}) => {
-  const badgeBgColor = colors[wixBindings.backgroundColor].value;
-  const badgeColor = colors[wixBindings.color].value;
-  const badgeFonts = fonts[wixBindings.fonts];
+  const badgeBgColor = colors[wixBindings.backgroundColor] || {};
+  const badgeColor = colors[wixBindings.color] || {};
+  const badgeFonts = fonts[wixBindings.fonts] || {};
 
   return {
     fontSize: badgeFonts.size,
     fontFamily: badgeFonts.family,
-    fontStyle: badgeFonts.style.italic ? 'italic' : 'normal',
-    fontWeight: badgeFonts.style.bold ? 'bold' : 'normal',
-    textDecoration: badgeFonts.style.underline ? 'underline' : 'none',
+    fontStyle: badgeFonts.style && badgeFonts.style.italic ? 'italic' : 'normal',
+    fontWeight: badgeFonts.style && badgeFonts.style.bold ? 'bold' : 'normal',
+    textDecoration: badgeFonts.style && badgeFonts.style.underline ? 'underline' : 'none',
 
-    backgroundColor: badgeBgColor,
-    borderColor: badgeBgColor,
-    color: badgeColor
+    backgroundColor: badgeBgColor.value,
+    borderColor: badgeBgColor.value,
+    color: badgeColor.value
   };
 };
