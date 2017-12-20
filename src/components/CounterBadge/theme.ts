cd9b238cfdc0ab4900587295dc3e69e1cb9cdc2f
Rokas Mikalkevičius <rokasm@wix.com>
2017-12-20 16:53:42 +0200

    Fix style

--- src/components/CounterBadge/theme.ts
+++ src/components/CounterBadge/theme.ts
@@ -1,9 +1,17 @@
+const HEIGHT = '18px';
+const PADDING = '3px 6px';
+const BORDER_RADIUS = '12px';
+
 export const theme = ({wixBindings, colors, fonts}) => {
   const badgeBgColor = colors[wixBindings.backgroundColor] || {};
   const badgeColor = colors[wixBindings.color] || {};
   const badgeFonts = fonts[wixBindings.fonts] || {};
 
   return {
+    height: HEIGHT,
+    padding: PADDING,
+    borderRadius: BORDER_RADIUS,
+
     fontSize: badgeFonts.size,
     fontFamily: badgeFonts.family,
     fontStyle: badgeFonts.style && badgeFonts.style.italic ? 'italic' : 'normal',
