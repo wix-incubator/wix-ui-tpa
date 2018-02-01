d4c01917f4c87b78969b75efa929c622e4b5e729
Carmel Cohen <carmelc@wix.com>
2018-02-01 14:53:31 +0200

    feat(Stylable TPA): convert styleableExt to es5 syntax

--- src/tpaStyles/styleableExt.js
+++ src/tpaStyles/styleableExt.js
@@ -1,3 +1,6 @@
-module.exports.wixApply = function (func, ...args) {
-  return `"${func}(${args.map((arg) => arg.replace(/"/g, '')).join(', ')})"`;
+module.exports.wixApply = function (func) {
+  var args = [].slice.call(arguments, 1);
+  return "\"" + func + "(" + args.map(function (arg) {
+    return arg.replace(/"/g, '');
+  }).join(', ') + ")\"";
 };
