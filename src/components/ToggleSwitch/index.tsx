78675c8fb82806fad259b5dca442b7f7ac9d8a3e
Amir Yonatan <amiry@wix.com>
2018-02-06 16:13:24 +0200

    Fixed test running issue

--- src/components/ToggleSwitch/index.tsx
+++ src/components/ToggleSwitch/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {ToggleSwitch, ToggleSwitchProps} from 'wix-ui-core/StylableToggleSwitch';
 import toggleSwitchStyles from './ToggleSwitch.st.css';
-import {withStylable} from 'wix-ui-core/dist/src';
+import {withStylable} from 'wix-ui-core';
 
 export const TpaToggleSwitch = withStylable<ToggleSwitchProps>(ToggleSwitch, toggleSwitchStyles, () => null);
