d06c0499f6e3d3eb38f31938a75250904fb1e190
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-04-04 18:24:33 +0300

    refactor input error state and start creating the Autocomplete component

--- src/testkit/index.ts
+++ src/testkit/index.ts
@@ -2,3 +2,6 @@ import {testkitFactoryCreator} from 'wix-ui-test-utils/vanilla';
 
 import {inputDriverFactory} from '../components/Input/Input.driver';
 export const inputTestkitFactory = testkitFactoryCreator(inputDriverFactory);
+
+import {autocompleteDriverFactory} from '../components/Autocomplete/Autocomplete.driver';
+export const autocompleteTestkitFactory = testkitFactoryCreator(autocompleteDriverFactory);
--- src/testkit/protractor.ts
+++ src/testkit/protractor.ts
@@ -2,3 +2,6 @@ import {protractorTestkitFactoryCreator} from 'wix-ui-test-utils/protractor';
 
 import {inputDriverFactory} from '../components/Input/Input.protractor.driver';
 export const inputTestkitFactory = protractorTestkitFactoryCreator(inputDriverFactory);
+
+import {autocompleteDriverFactory} from '../components/Autocomplete/Autocomplete.protractor.driver';
+export const autocompleteTestkitFactory = protractorTestkitFactoryCreator(autocompleteDriverFactory);
