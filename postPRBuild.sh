7ff8aa9e3336fac7b6345f1b791efa5cb1e11b1d
Ronen Stern <ronens@wix.com>
2018-05-06 13:43:48 +0300

    Use common postPRBuild script and change npm script name to lowecase

--- postPRBuild.sh
+++ postPRBuild.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env bash
-
-export NVM_DIR="$HOME/.nvm"; . "$NVM_DIR/nvm.sh";
-if [ -f .nvmrc ]; then
-  echo "Running nvm use"
-  nvm use &> /dev/null || exit 1
-fi
-
-echo "Now using node $(node -v) (npm v$(npm -v))"
-
-npm run postPRBuild
+/opt/bin/postPRBuild.sh
