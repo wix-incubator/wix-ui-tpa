ce0b1ce0812f77407b2a43fe79ff61e919e1a159
liorMar <lior.marzouk@gmail.com>
2019-01-08 22:23:32 +0200

    added e2e test and protractor driver

---
+++ src/components/Text/Text.protractor.driver.ts
@@ -0,0 +1,16 @@
+import {BaseDriver, DriverFactory} from 'wix-ui-core/src/common/BaseDriver.protractor';
+
+export interface TextDriver extends BaseDriver {
+  getText: () => Promise<string>;
+}
+
+export const textDriverFactory: DriverFactory<TextDriver> = component => {
+  return {
+    /** returns the component element */
+    element: () => component,
+    /** returns the component text */
+    getText: async () => component.getText(),
+    /** returns the component tag name */
+    getTagName: async () => component.getTagName(),
+  };
+};
--- src/testkit/protractor.ts
+++ src/testkit/protractor.ts
@@ -4,6 +4,10 @@ import {inputDriverFactory, InputDriver} from '../components/Input/Input.protrac
 export const inputTestkitFactory = protractorTestkitFactoryCreator<InputDriver>(inputDriverFactory);
 export {InputDriver};
 
+import {textDriverFactory, TextDriver} from '../components/Text/Text.protractor.driver';
+export const textTestkitFactory = protractorTestkitFactoryCreator<TextDriver>(textDriverFactory);
+export {TextDriver};
+
 import {autocompleteDriverFactory, AutocompleteDriver} from '../components/Autocomplete/Autocomplete.protractor.driver';
 export const autocompleteTestkitFactory = protractorTestkitFactoryCreator<AutocompleteDriver>(autocompleteDriverFactory);
 export {AutocompleteDriver};
