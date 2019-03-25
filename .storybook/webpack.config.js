76710af4d243d16bad053decd34a6c22a5476336
liorMar <lior.marzouk@gmail.com>
2019-03-25 12:33:36 +0200

    migrate to StoryBook 5 & Yoshi 3

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,30 @@
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const merge = require('lodash/merge');
+const path = require('path');
 const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
 
-module.exports = (config, env) => {
-  const newConfig = wixStorybookConfig(genDefaultConfig(config, env));
+module.exports = ({config}) => {
+  const newConfig = wixStorybookConfig(config);
 
-  newConfig.module.rules.push({
-    test: /\.story\.[j|t]sx?$/,
-    loader: 'wix-storybook-utils/loader',
-    options: {
-      storyConfig: {
-        moduleName: 'wix-ui-tpa',
-        repoBaseURL: 'https://github.com/wix/wix-ui-tpa/tree/master/src/components'
-      }
+  return merge(newConfig, {
+    context: path.resolve(__dirname, '../src/components'),
+    resolve: {
+      alias: {
+        'wix-ui-tpa': path.resolve(__dirname, '../src/components')
+      },
+    },
+    module: {
+      rules: newConfig.module.rules.concat({
+        test: /\.story\.[j|t]sx?$/,
+        loader: 'wix-storybook-utils/loader',
+
+        options: {
+          storyConfig: {
+            moduleName: 'wix-ui-tpa',
+            repoBaseURL: 'https://github.com/wix-private/wix-ui-tpa/tree/master/src/components/',
+            importFormat: "import {%componentName} from '%moduleName/%componentName'"
+          }
+        }
+      })
     }
   });
-
-  return newConfig;
 };
