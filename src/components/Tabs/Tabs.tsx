43677f35c2a8435535df66c23f1909d5477a9ba2
Jonathan Adler <jonathana@wix.com>
2019-04-30 10:30:44 +0300

    listen to resize event, and toggle scrollable if needed

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactResizeDetector from 'react-resize-detector';
 import style from './Tabs.st.css';
 import classNames from 'classnames';
 import { ALIGNMENT, SKIN, VARIANT } from './constants';
@@ -24,6 +25,12 @@ export interface TabItem {
   title?: string;
 }
 
+interface TabsState {
+  scrollable: boolean;
+}
+
+const EPSILON = 6;
+
 const selectTab = (newActiveTabIndex, activeTabIndex, onTabClick) => {
   if (activeTabIndex !== newActiveTabIndex) {
     onTabClick(newActiveTabIndex);
@@ -45,24 +52,110 @@ const renderTabItem = ({ item, index, onTabClick, activeTabIndex }) => {
   );
 };
 
-const Tabs = (props: TabsProps) => {
-  const { items, activeTabIndex, onTabClick, skin, alignment, variant } = props;
-  return (
-    <div {...style('root', { skin, alignment, variant }, props)}>
-      <nav>
-        {items.map((item, index) =>
-          renderTabItem({ item, index, onTabClick, activeTabIndex }),
-        )}
-      </nav>
-    </div>
-  );
-};
+class Tabs extends React.PureComponent<TabsProps> {
+  _nav: HTMLElement;
+  _wrapper: HTMLElement;
 
-Tabs.defaultProps = {
-  onTabClick: (tabIndex: number) => { },
-  skin: SKIN.fullUnderline,
-  alignment: ALIGNMENT.center,
-  variant: VARIANT.fit,
-};
+  state: TabsState = {
+    scrollable: false,
+  };
+
+  static defaultProps = {
+    onTabClick: (tabIndex: number) => {},
+    skin: SKIN.fullUnderline,
+    alignment: ALIGNMENT.center,
+    variant: VARIANT.fit,
+  };
+
+  constructor(props: Readonly<TabsProps>) {
+    super(props);
+  }
+
+  _wrapperRef = (el: HTMLElement) => {
+    this._wrapper = el;
+    this._setScrollableIfNeeded();
+  };
+
+  _onResize = () => {
+    this._setScrollableIfNeeded();
+  };
+
+  _calculateNavigationWidth = () => {
+    let width = 0;
+
+    this._wrapper.querySelectorAll(`.${style.tab}`).forEach(tab => {
+      width += tab.clientWidth;
+    });
+
+    return width;
+  };
+
+  _shouldShowScroll() {
+    let shouldShowScroll = false;
+
+    if (this._wrapper) {
+      const navWidth = this._calculateNavigationWidth();
+      const wrapperWidth = this._wrapper.offsetWidth;
+      console.log(
+        'adler',
+        'Tabs.tsx:98',
+        this._wrapper.querySelector('nav').scrollLeft,
+      );
+      shouldShowScroll = navWidth > wrapperWidth;
+    }
+
+    return shouldShowScroll;
+  }
+
+  _setScrollableIfNeeded = () => {
+    const { scrollable } = this.state;
+    const shouldShowScroll = this._shouldShowScroll();
+
+    if (shouldShowScroll !== scrollable) {
+      this.setState({ scrollable: shouldShowScroll });
+    }
+  };
+
+  _onScroll = (e: React.SyntheticEvent) => {
+    console.log(e);
+  };
+
+  render() {
+    const {
+      items,
+      activeTabIndex,
+      onTabClick,
+      skin,
+      alignment,
+      variant,
+    } = this.props;
+    const { scrollable } = this.state;
+
+    return (
+      <div
+        {...style('root', { skin, alignment, variant, scrollable }, this.props)}
+      >
+        <ReactResizeDetector handleWidth onResize={this._onResize} />
+        <div
+          className={style.navWrapper}
+          ref={this._wrapperRef}
+          onScroll={this._onScroll}
+        >
+          <div className={classNames(style.scrollBtn, style.scrollBtnLeft)}>
+            &lt;
+          </div>
+          <nav>
+            {items.map((item, index) =>
+              renderTabItem({ item, index, onTabClick, activeTabIndex }),
+            )}
+          </nav>
+          <div className={classNames(style.scrollBtn, style.scrollBtnRight)}>
+            &gt;
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export { Tabs };
