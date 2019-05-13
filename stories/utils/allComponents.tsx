d5c5fb67a8cca1b9cd979d809641f22721c81417
argshook <argshook@gmail.com>
2019-05-13 11:03:21 +0300

    chore(stories/utils/allComponents): use allComponents in code section
    in future this should become entirely obsolete

--- stories/utils/allComponents.tsx
+++ stories/utils/allComponents.tsx
@@ -3,9 +3,9 @@ import * as iconsScope from 'wix-ui-icons-common';
 
 /*
  * This object contains all wix-ui-tpa components including icons
- * It is used mainly for documentation in LiveCodeExample and code section.
+ * It is used in code section of autodocs.
  */
-export const LiveScope = {
+export const allComponents = {
   ...componentsScope,
   Icons: iconsScope,
 };
