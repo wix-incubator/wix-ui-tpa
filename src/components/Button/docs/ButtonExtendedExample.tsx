f1c9a3a4e28b7f193bb855ebe883fe568d122a11
argshook <argshook@gmail.com>
2019-05-30 18:00:59 +0300

    docs(Button): move to components folder

--- src/components/Button/docs/ButtonExtendedExample.tsx
+++ src/components/Button/docs/ButtonExtendedExample.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {ButtonProps, Button} from '../../src/components/Button';
+import {ButtonProps, Button} from '../Button';
 import extendedStyles from './ButtonExtendedExample.st.css';
 
 export const ButtonExtendedExample: React.FunctionComponent<ButtonProps> = (props) =>
--- src/components/Button/docs/examples.tsx
+++ src/components/Button/docs/examples.tsx
@@ -8,36 +8,38 @@ import * as ExtendedCSSRawSource from '!raw-loader!./ButtonExtendedExample.st.cs
 import * as ExtendedCSSRawSourceWithStyleParams from '!raw-loader!./ButtonExtendedWithStyleParamsExample.st.css';
 import { ButtonExtendedExample } from './ButtonExtendedExample';
 import { ButtonExtendedWithStyleParamsExample } from './ButtonExtendedWithStyleParamsExample';
-import { MockSettings } from '../helperComponents/MockSettings';
+import { MockSettings } from '../../../../stories/helperComponents/MockSettings';
 
 export const Examples = () => (
   <div>
-      <div className="tpa-container">
-          <CodeExample
-              title="Button Extended"
-              code={[
-                  '//.st.css',
-                  ExtendedCSSRawSource,
-                  '',
-                  '//.tsx',
-                  ExtendedRawSource,
-              ].join('\n')}
-          >
-              <ButtonExtendedExample>some text</ButtonExtendedExample>
-          </CodeExample>
-      </div>
+    <div className="tpa-container">
+      <CodeExample
+        title="Button Extended"
+        code={[
+          '//.st.css',
+          ExtendedCSSRawSource,
+          '',
+          '//.tsx',
+          ExtendedRawSource,
+        ].join('\n')}
+      >
+        <ButtonExtendedExample>some text</ButtonExtendedExample>
+      </CodeExample>
+    </div>
     <div className="tpa-container">
       <CodeExample
         title="Button with style params"
         code={[
           '//.st.css',
-            ExtendedCSSRawSourceWithStyleParams,
+          ExtendedCSSRawSourceWithStyleParams,
           '',
           '//.tsx',
-            ExtendedRawSourceWithStyleParams,
+          ExtendedRawSourceWithStyleParams,
         ].join('\n')}
       >
-        <ButtonExtendedWithStyleParamsExample>some text</ButtonExtendedWithStyleParamsExample>
+        <ButtonExtendedWithStyleParamsExample>
+          some text
+        </ButtonExtendedWithStyleParamsExample>
       </CodeExample>
     </div>
     <MockSettings
