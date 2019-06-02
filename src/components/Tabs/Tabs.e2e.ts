f6d38639f0662cafae53d80b25db978c20d814d6
Jonathan Adler <jonathana@wix.com>
2019-06-02 11:37:30 +0300

    remove eyes.it tabs test

--- src/components/Tabs/Tabs.e2e.ts
+++ src/components/Tabs/Tabs.e2e.ts
@@ -1,4 +1,3 @@
-import * as eyes from 'eyes.it';
 import { browser } from 'protractor';
 import {
   createStoryUrl,
@@ -8,61 +7,42 @@ import { tabsTestkitFactory } from '../../testkit/protractor';
 import { NavButtonOptions } from './Tabs';
 
 describe('Tabs', () => {
-  describe('snapshot tests', () => {
-    const storyUrl = createStoryUrl({
-      kind: 'Components',
-      story: 'Tabs',
-      withExamples: true,
-    });
-    const dataHook = 'storybook-Tabs';
+  const storyUrl = createStoryUrl({
+    kind: 'Tests',
+    story: 'Tabs',
+    withExamples: true,
+  });
+  const dataHook = 'storybook-e2e-Tabs';
 
-    beforeEach(() => browser.get(storyUrl));
-
-    eyes.it('should render', async () => {
-      const driver = tabsTestkitFactory({ dataHook });
-      await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
-      expect((await driver.element()).isDisplayed()).toBe(true);
-    });
+  beforeEach(async () => {
+    await browser.get(storyUrl);
   });
 
-  describe('interaction tests', () => {
-    const storyUrl = createStoryUrl({
-      kind: 'Tests',
-      story: 'Tabs',
-      withExamples: true,
-    });
-    const dataHook = 'storybook-e2e-Tabs';
+  it('should show nav buttons + click should scroll nav', async () => {
+    const driver = tabsTestkitFactory({ dataHook });
+    await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
+    expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.right);
 
-    beforeEach(async () => {
-      await browser.get(storyUrl);
-    });
+    await driver.clickRightNavButton();
+    await browser.sleep(400);
+    expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.both);
 
-    it('should show nav buttons + click should scroll nav', async () => {
-      const driver = tabsTestkitFactory({ dataHook });
-      await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
-      expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.right);
-
+    while ((await driver.getNavButtonsShown()) === NavButtonOptions.both) {
       await driver.clickRightNavButton();
       await browser.sleep(400);
-      expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.both);
+    }
 
-      while ((await driver.getNavButtonsShown()) === NavButtonOptions.both) {
-        await driver.clickRightNavButton();
-        await browser.sleep(400);
-      }
+    expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.left);
 
-      expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.left);
+    await driver.clickLeftNavButton();
+    await browser.sleep(400);
+    expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.both);
 
+    while ((await driver.getNavButtonsShown()) === NavButtonOptions.both) {
       await driver.clickLeftNavButton();
       await browser.sleep(400);
-      expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.both);
+    }
 
-      while ((await driver.getNavButtonsShown()) === NavButtonOptions.both) {
-        await driver.clickLeftNavButton();
-        await browser.sleep(400);
-      }
-
-      expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.right);
-    });
+    expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.right);
   });
 });
