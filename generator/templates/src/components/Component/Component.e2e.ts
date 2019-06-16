bbae9b4470674176ad6d35a017767c2ba30ff460
Jonathan Adler <jonathana@wix.com>
2019-06-16 15:32:28 +0300

    remove eyes.it from generator

--- generator/templates/src/components/Component/Component.e2e.ts
+++ generator/templates/src/components/Component/Component.e2e.ts
@@ -1,4 +1,3 @@
-import * as eyes from 'eyes.it';
 import { browser } from 'protractor';
 import {
   createStoryUrl,
@@ -6,6 +5,10 @@ import {
 } from 'wix-ui-test-utils/protractor';
 import { {%componentName%}TestkitFactory } from '../../testkit/protractor';
 
+/**
+ * For tests containing interactions.
+ * Can be removed if not used.
+ * */
 describe('{%componentName%}', () => {
   const storyUrl = createStoryUrl({
     kind: 'Components',
@@ -16,7 +19,7 @@ describe('{%componentName%}', () => {
 
   beforeEach(() => browser.get(storyUrl));
 
-  eyes.it('should render', async () => {
+  it('should render', async () => {
     const driver = {%componentName%}TestkitFactory({ dataHook });
     await waitForVisibilityOf(await driver.element(), 'Cannot find {%ComponentName%}');
     expect((await driver.element()).isDisplayed()).toBe(true);
