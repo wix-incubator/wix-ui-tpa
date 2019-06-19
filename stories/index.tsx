01ab163d18ee78543f2aabd0615bcef8b13d3b81
Mantas Pajada <mantasp@wix.com>
2019-06-19 17:55:14 +0300

    order components

--- stories/index.tsx
+++ stories/index.tsx
@@ -7,7 +7,9 @@ import '../src/components/Button/docs/index.story';
 import '../src/components/StatesButton/docs/index.story';
 import '../src/components/Card/docs/index.story';
 import '../src/components/Divider/docs/index.story';
+import '../src/components/IconToggle/docs/index.story';
 import '../src/components/Input/docs/index.story';
+import '../src/components/LikeButton/docs/index.story';
 import '../src/components/OverlappingCard/docs/index.story';
 import '../src/components/StripCard/docs/index.story';
 import '../src/components/Tabs/docs/index.story';
@@ -15,5 +17,3 @@ import '../src/components/Text/docs/index.story';
 import '../src/components/Grid/docs/index.story';
 import '../src/components/ToggleSwitch/docs'; // TODO: convert to .story
 import '../src/components/Tooltip/docs/index.story';
-import '../src/components/IconToggle/docs/index.story';
-import '../src/components/LikeButton/docs/index.story';
