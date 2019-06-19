a5ae846c8a48742d8283e100268da5ad3f0cd1e7
Jonathan Adler <jonathana@wix.com>
2019-06-19 17:09:31 +0300

    use tree shaking again + fix e2e tests

--- stories/index.tsx
+++ stories/index.tsx
@@ -2,16 +2,16 @@ import styleProcessor from 'wix-style-processor';
 
 styleProcessor.init();
 
-import '../src/components/Autocomplete/docs/index.story';
-import '../src/components/Button/docs/index.story';
-import '../src/components/StatesButton/docs/index.story';
-import '../src/components/Card/docs/index.story';
-import '../src/components/Divider/docs/index.story';
-import '../src/components/Input/docs/index.story';
-import '../src/components/OverlappingCard/docs/index.story';
-import '../src/components/StripCard/docs/index.story';
-import '../src/components/Tabs/docs/index.story';
-import '../src/components/Text/docs/index.story';
-import '../src/components/Grid/docs/index.story';
-import '../src/components/ToggleSwitch/docs'; // TODO: convert to .story
-import '../src/components/Tooltip/docs/index.story';
+require('../src/components/Autocomplete/docs/index.story');
+require('../src/components/Button/docs/index.story');
+require('../src/components/StatesButton/docs/index.story');
+require('../src/components/Card/docs/index.story');
+require('../src/components/Divider/docs/index.story');
+require('../src/components/Input/docs/index.story');
+require('../src/components/OverlappingCard/docs/index.story');
+require('../src/components/StripCard/docs/index.story');
+require('../src/components/Tabs/docs/index.story');
+require('../src/components/Text/docs/index.story');
+require('../src/components/Grid/docs/index.story');
+require('../src/components/ToggleSwitch/docs'); // TODO: convert to .story
+require('../src/components/Tooltip/docs/index.story');
