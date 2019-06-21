355d5bbd54b1be62a7d1cab94a2393af02974f3f
medasiaudvytyte <medas@wix.com>
2019-06-21 11:27:26 +0300

    remove mobile visual tests from <LikeButton />

--- src/components/LikeButton/LikeButton.visual.tsx
+++ src/components/LikeButton/LikeButton.visual.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { TPAComponentsProvider } from '../TPAComponentsConfig';
 import { LikeButton } from './';
 
 class LikeButtonVisual extends React.Component<any> {
-  static defaultProps = {
-    mobile: false,
-  };
-
   render() {
-    const { mobile } = this.props;
-
     return (
-      <TPAComponentsProvider value={{ mobile }}>
-        <LikeButton data-hook={'storybook-e2e-LikeButton'} {...this.props} />
-      </TPAComponentsProvider>
+      <LikeButton data-hook={'storybook-e2e-LikeButton'} {...this.props} />
     );
   }
 }
@@ -25,22 +16,15 @@ const tests = [
     its: [
       {
         it: 'default',
-        props: {},
-      },
-      {
-        it: 'mobile',
-        props: {
-          mobile: true,
-        },
       },
     ],
   },
 ];
 
 tests.forEach(({ describe, its }) => {
-  its.forEach(({ it, props }) => {
+  its.forEach(({ it }) => {
     storiesOf(`LikeButton/${describe}`, module).add(it, () => (
-      <LikeButtonVisual {...props} />
+      <LikeButtonVisual />
     ));
   });
 });
