bf80228ebf348e407befdbffe860c4e50228b72e
argshook <argshook@gmail.com>
2019-06-28 18:58:25 +0300

    fix(LikeButton): use correct export name

--- src/components/LikeButton/LikeButton.protractor.driver.ts
+++ src/components/LikeButton/LikeButton.protractor.driver.ts
@@ -1,9 +1,4 @@
-import {
-  checkboxDriverFactory as iconToggleDriverFactory,
-  CheckboxDriver as IconToggleDriver,
-} from '../IconToggle/IconToggle.protractor.driver';
-
 export {
   iconToggleDriverFactory as likeButtonDriverFactory,
   IconToggleDriver as LikeButtonDriver,
-};
+} from '../IconToggle/IconToggle.protractor.driver';
