ce5baa3707f8f85ea0a717edee357a1db6488c29
argshook <argshook@gmail.com>
2019-07-04 14:30:34 +0300

    chore(.wuf/testkits/definitions.js): invert unidriver setting
    all new components are unidrivers. To avoid growing the list of
    components in `definitions`, better set legacy drivers as non
    conventional ones

--- .wuf/testkits/definitions.js
+++ .wuf/testkits/definitions.js
@@ -1,38 +1,24 @@
 /*
- * This file exports object with config for component testkits.
+ * This file exports object with config for components testkits.
+ * The values are used in `.wuf/testkits/*.template.ejs` files
+ *
  * more info: https://github.com/wix/wix-ui/blob/master/packages/wix-ui-framework/docs/testkits-definitions.md
  */
 
+// legacyDriver means not unidriver
+// TODO: legacy drivers should eventually be migrated to unidriver
 module.exports = {
-  Button: {
-    unidriver: true
-  },
-
-  Card: {
-    unidriver: true
-  },
-
-  OverlappingCard: {
-    unidriver: true
-  },
-
-  StripCard: {
-    unidriver: true
-  },
-
-  Grid: {
-    unidriver: true
-  },
-
-  StatesButton: {
-    unidriver: true
-  },
-
-  Tabs: {
-    unidriver: true
-  },
+  Autocomplete: { legacyDriver: true },
+  IconToggle: { legacyDriver: true },
+  Input: { legacyDriver: true },
+  LikeButton: { legacyDriver: true },
+  Pagination: { legacyDriver: true },
+  Text: { legacyDriver: true },
+  ToggleSwitch: { legacyDriver: true },
+  Tooltip: { legacyDriver: true },
 
   Divider: {
+    // component does not have a testkit at all
     noTestkit: true
   }
 };
