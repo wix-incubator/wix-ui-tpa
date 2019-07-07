a175ac94aaecf91093f0bc2e0ead8621a3a2f4a9
Jonathan Adler <jonathana@wix.com>
2019-07-07 19:18:29 +0300

    testing pr version publish

--- scripts/pr-build.js
+++ scripts/pr-build.js
@@ -1,7 +1,30 @@
-console.log('@@@@@@@@@@@@@@@@@@@@@@@@@  PR_BUILD START  @@@@@@@@@@@@@@@@@@@@@@@@@');
-console.log('BUILD_ID', process.env.BUILD_ID);
-console.log('BUILD_NAME', process.env.BUILD_NAME);
-console.log('BRANCH', process.env.BRANCH);
-console.log('VCS_BRANCH_NAME', process.env.VCS_BRANCH_NAME);
-console.log('IS_BUILD_AGENT', process.env.IS_BUILD_AGENT);
-console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@  PR_BUILD END  @@@@@@@@@@@@@@@@@@@@@@@@@@');
+const fs = require('fs');
+const child_process = require('child_process');
+
+console.log('PR publish version - start');
+
+try {
+    console.log('Checking if should publish PR version');
+
+    const pr = process.env.VCS_BRANCH_NAME && process.env.VCS_BRANCH_NAME.replace(/\D+/g, '') || false;
+    if (process.env.IS_BUILD_AGENT && pr) {
+        console.log('preparing publish');
+        const packageContent = require('../package');
+        const packageCopy = Object.assign({}, packageContent);
+        const version = `${packageContent.version}-pr.${pr}`;
+        console.log('version to be published: ', version);
+        packageContent.version = version;
+        console.log('writing package.json with new version');
+        fs.writeFileSync('../package.json', JSON.stringify(packageContent));
+        console.log('publishing version', version);
+        child_process.execSync('npx yoshi release');
+        console.log('published successfully');
+        console.log('reverting package change');
+        fs.writeFileSync('../package.json', JSON.stringify(packageCopy));
+    }
+
+} catch (e) {
+    console.log('Failed PR publish', e);
+}
+
+console.log('PR publish version - END');
