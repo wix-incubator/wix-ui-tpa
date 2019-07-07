1197322b920b34526aaae0160b36d243d8d587e1
Jonathan Adler <jonathana@wix.com>
2019-07-07 19:28:57 +0300

    revert

--- scripts/pr-build.js
+++ scripts/pr-build.js
@@ -8,19 +8,19 @@ try {
 
     const pr = process.env.VCS_BRANCH_NAME && process.env.VCS_BRANCH_NAME.replace(/\D+/g, '') || false;
     if (process.env.IS_BUILD_AGENT && pr) {
-        console.log('preparing publish');
-        const packageContent = require('../package');
-        const packageCopy = Object.assign({}, packageContent);
-        const version = `${packageContent.version}-pr.${pr}`;
-        console.log('version to be published: ', version);
-        packageContent.version = version;
-        console.log('writing package.json with new version');
-        fs.writeFileSync('../package.json', JSON.stringify(packageContent));
-        console.log('publishing version', version);
-        child_process.execSync('npx yoshi release');
-        console.log('published successfully');
-        console.log('reverting package change');
-        fs.writeFileSync('../package.json', JSON.stringify(packageCopy));
+        // console.log('preparing publish');
+        // const packageContent = require('../package');
+        // const packageCopy = Object.assign({}, packageContent);
+        // const version = `${packageContent.version}-pr.${pr}`;
+        // console.log('version to be published: ', version);
+        // packageContent.version = version;
+        // console.log('writing package.json with new version');
+        // fs.writeFileSync('../package.json', JSON.stringify(packageContent));
+        // console.log('publishing version', version);
+        // child_process.execSync('npx yoshi release');
+        // console.log('published successfully');
+        // console.log('reverting package change');
+        // fs.writeFileSync('../package.json', JSON.stringify(packageCopy));
     }
 
 } catch (e) {
