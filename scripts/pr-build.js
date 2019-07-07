5fc59cbc27d73d0665ea9020cfa3c383a34bd7cf
Jonathan Adler <jonathana@wix.com>
2019-07-07 16:20:09 +0300

    add scripts folder and print env variables

---
+++ scripts/pr-build.js
@@ -0,0 +1,7 @@
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@  PR_BUILD START  @@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('BUILD_ID', process.env.BUILD_ID);
+console.log('BUILD_NAME', process.env.BUILD_NAME);
+console.log('BRANCH', process.env.BRANCH);
+console.log('VCS_BRANCH_NAME', process.env.VCS_BRANCH_NAME);
+console.log('IS_BUILD_AGENT', process.env.IS_BUILD_AGENT);
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@  PR_BUILD END  @@@@@@@@@@@@@@@@@@@@@@@@@@');
