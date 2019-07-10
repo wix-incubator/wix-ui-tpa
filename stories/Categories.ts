fa8e362678c6be0b42cf4b0d456b571972b4ac18
Jonathan Adler <jonathana@wix.com>
2019-07-10 11:44:15 +0300

    add categories file

---
+++ stories/Categories.ts
@@ -0,0 +1,16 @@
+export const Categories = {
+  Components: {
+    Elements: '1. Elements',
+    Buttons: '2. Buttons',
+    Navigation: '3. Navigation',
+    Inputs: '4. Inputs',
+    Selections: '5. Selections',
+    Overlays: '6. Overlays',
+    Cards: '7. Cards',
+    Carousel: '8. Carousel',
+  },
+  Foundation: {
+    Typography: 'Typography',
+    Icons: 'Icons',
+  },
+};
