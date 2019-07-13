b94774c30473d39ddcb002ccd8d71d4f52efe75c
Jonathan Adler <jonathana@wix.com>
2019-07-13 21:16:34 +0300

    no style-processor in config + add to generator

--- .storybook-test/config.js
+++ .storybook-test/config.js
@@ -1,10 +1,8 @@
 import {configure} from '@storybook/react';
-import styleProcessor from 'wix-style-processor';
 import './stories.scss';
 
 function loadStories() {
   require('../mocks');
-  styleProcessor.init();
 
   const req = require.context('../src', true, /\.visual\.tsx$/);
   req.keys().forEach(filename => req(filename));
