f9e93768af86b7be33f28ead5a6776797388ca5b
Andrius Kripaitis <andriusk@wix.com>
2019-07-16 14:54:38 +0300

    Some leftower removed;

--- src/components/Avatar/Avatar.driver.ts
+++ src/components/Avatar/Avatar.driver.ts
@@ -2,14 +2,11 @@ import {
   BaseUniDriver,
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
-import { StylableUnidriverUtil, UniDriver } from 'wix-ui-test-utils/unidriver';
-import style from './Avatar.st.css';
+import { UniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface AvatarDriver extends BaseUniDriver {}
 
 export const avatarDriverFactory = (base: UniDriver): AvatarDriver => {
-  const stylableUtil = new StylableUnidriverUtil(style);
-
   return {
     ...baseUniDriverFactory(base),
   };
