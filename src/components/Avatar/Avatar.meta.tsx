88f933e239b300f30699d53bb6cdaf2f030138f0
Andrius Kripaitis <andriusk@wix.com>
2019-07-16 17:23:58 +0300

    Comment-out meta test as ui-autotools doesn support svg;

--- src/components/Avatar/Avatar.meta.tsx
+++ src/components/Avatar/Avatar.meta.tsx
@@ -1,28 +1,30 @@
-import { Avatar } from '.';
-import Registry from '@ui-autotools/registry';
-import { AvatarSize } from './Avatar';
+// The `ui-autotools` tool, doesn’t support importing svg files as react elements.
 
-const AvatarMetadata = Registry.getComponentMetadata(Avatar);
-AvatarMetadata.nonReactStrictModeCompliant = true;
+// import { Avatar } from '.';
+// import Registry from '@ui-autotools/registry';
+// import { AvatarSize } from './Avatar';
 
-AvatarMetadata.addSim({
-  title: 'render anonymous',
-  props: {},
-});
+// const AvatarMetadata = Registry.getComponentMetadata(Avatar);
+// AvatarMetadata.nonReactStrictModeCompliant = true;
 
-AvatarMetadata.addSim({
-  title: 'render small with image',
-  props: {
-    size: AvatarSize.small,
-    name: 'John',
-  },
-});
+// AvatarMetadata.addSim({
+//   title: 'render anonymous',
+//   props: {},
+// });
 
-AvatarMetadata.addSim({
-  title: 'render medium with image',
-  props: {
-    size: AvatarSize.medium,
-    name: 'John',
-    src: 'https://randomuser.me/api/portraits/men/65.jpg',
-  },
-});
+// AvatarMetadata.addSim({
+//   title: 'render small with image',
+//   props: {
+//     size: AvatarSize.small,
+//     name: 'John',
+//   },
+// });
+
+// AvatarMetadata.addSim({
+//   title: 'render medium with image',
+//   props: {
+//     size: AvatarSize.medium,
+//     name: 'John',
+//     src: 'https://randomuser.me/api/portraits/men/65.jpg',
+//   },
+// });
