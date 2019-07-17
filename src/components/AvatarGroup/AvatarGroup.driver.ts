48fe7d51f37c64a28987226ab417a17f10b5ffd4
Andrius Kripaitis <andriusk@wix.com>
2019-07-17 15:21:28 +0300

    Add simulations;

--- src/components/AvatarGroup/AvatarGroup.driver.ts
+++ src/components/AvatarGroup/AvatarGroup.driver.ts
@@ -2,20 +2,15 @@ import {
   BaseUniDriver,
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
-import { StylableUnidriverUtil, UniDriver } from 'wix-ui-test-utils/unidriver';
-import style from './AvatarGroup.st.css';
+import { UniDriver } from 'wix-ui-test-utils/unidriver';
 
-export interface AvatarGroupDriver extends BaseUniDriver {
-  isMobile(): Promise<boolean>;
-}
+export interface AvatarGroupDriver extends BaseUniDriver {}
 
 export const avatarGroupDriverFactory = (
   base: UniDriver,
 ): AvatarGroupDriver => {
-  const stylableUtil = new StylableUnidriverUtil(style);
 
   return {
     ...baseUniDriverFactory(base),
-    isMobile: async () => stylableUtil.hasStyleState(base, 'mobile'),
   };
 };
--- src/components/AvatarGroup/AvatarGroup.meta.tsx
+++ src/components/AvatarGroup/AvatarGroup.meta.tsx
@@ -1,5 +1,6 @@
 import { AvatarGroup } from '.';
 import Registry from '@ui-autotools/registry';
+import { AvatarGroupSize } from './AvatarGroup';
 
 const AvatarGroupMetadata = Registry.getComponentMetadata(AvatarGroup);
 AvatarGroupMetadata.nonReactStrictModeCompliant = true;
@@ -8,5 +9,58 @@ AvatarGroupMetadata.addSim({
   title: 'render',
   props: {
     items: [],
+  },
+});
+
+const items = [
+  {},
+  {name: 'anonymous'},
+  {name: 'Eve', src: 'https://randomuser.me/api/portraits/women/87.jpg'},
+  {name: 'John', src: 'https://randomuser.me/api/portraits/men/69.jpg'},
+];
+
+AvatarGroupMetadata.addSim({
+  title: 'render',
+  props: {
+    items: [
+     ...items,
+    ],
+    size: AvatarGroupSize.large
+  },
+});
+
+AvatarGroupMetadata.addSim({
+  title: 'render',
+  props: {
+    items: [
+     ...items,
+     ...items,
+    ],
+    size: AvatarGroupSize.small
+  },
+});
+
+AvatarGroupMetadata.addSim({
+  title: 'render',
+  props: {
+    items: [
+     ...items,
+     ...items,
+     ...items,
+    ],
+    size: AvatarGroupSize.xSmall
+  },
+});
+
+AvatarGroupMetadata.addSim({
+  title: 'render',
+  props: {
+    items: [
+     ...items,
+     ...items,
+     ...items,
+    ],
+    size: AvatarGroupSize.xxSmall,
+    maxAmount: 9
   },
 });
