99fb1a0126e7fa4837f2a604015de49c38cbd102
Andrius Kripaitis <andriusk@wix.com>
2019-07-17 15:58:46 +0300

    Revert linted files;

--- src/testkit/protractor.ts
+++ src/testkit/protractor.ts
@@ -10,87 +10,55 @@ import {
   protractorUniTestkitFactoryCreator,
 } from 'wix-ui-test-utils/protractor';
 
+
 import { autocompleteDriverFactory } from '../components/Autocomplete/Autocomplete.protractor.driver';
-export const autocompleteTestkitFactory = protractorTestkitFactoryCreator(
-  autocompleteDriverFactory,
-);
+export const autocompleteTestkitFactory = protractorTestkitFactoryCreator(autocompleteDriverFactory)
 
 import { avatarDriverFactory } from '../components/Avatar/Avatar.driver';
-export const avatarTestkitFactory = protractorUniTestkitFactoryCreator(
-  avatarDriverFactory,
-);
+export const avatarTestkitFactory = protractorUniTestkitFactoryCreator(avatarDriverFactory)
 
 import { buttonDriverFactory } from '../components/Button/Button.driver';
-export const buttonTestkitFactory = protractorUniTestkitFactoryCreator(
-  buttonDriverFactory,
-);
+export const buttonTestkitFactory = protractorUniTestkitFactoryCreator(buttonDriverFactory)
 
 import { cardDriverFactory } from '../components/Card/Card.driver';
-export const cardTestkitFactory = protractorUniTestkitFactoryCreator(
-  cardDriverFactory,
-);
+export const cardTestkitFactory = protractorUniTestkitFactoryCreator(cardDriverFactory)
 
 import { gridDriverFactory } from '../components/Grid/Grid.driver';
-export const gridTestkitFactory = protractorUniTestkitFactoryCreator(
-  gridDriverFactory,
-);
+export const gridTestkitFactory = protractorUniTestkitFactoryCreator(gridDriverFactory)
 
 import { iconToggleDriverFactory } from '../components/IconToggle/IconToggle.protractor.driver';
-export const iconToggleTestkitFactory = protractorTestkitFactoryCreator(
-  iconToggleDriverFactory,
-);
+export const iconToggleTestkitFactory = protractorTestkitFactoryCreator(iconToggleDriverFactory)
 
 import { inputDriverFactory } from '../components/Input/Input.protractor.driver';
-export const inputTestkitFactory = protractorTestkitFactoryCreator(
-  inputDriverFactory,
-);
+export const inputTestkitFactory = protractorTestkitFactoryCreator(inputDriverFactory)
 
 import { likeButtonDriverFactory } from '../components/LikeButton/LikeButton.protractor.driver';
-export const likeButtonTestkitFactory = protractorTestkitFactoryCreator(
-  likeButtonDriverFactory,
-);
+export const likeButtonTestkitFactory = protractorTestkitFactoryCreator(likeButtonDriverFactory)
 
 import { overlappingCardDriverFactory } from '../components/OverlappingCard/OverlappingCard.driver';
-export const overlappingCardTestkitFactory = protractorUniTestkitFactoryCreator(
-  overlappingCardDriverFactory,
-);
+export const overlappingCardTestkitFactory = protractorUniTestkitFactoryCreator(overlappingCardDriverFactory)
 
 import { paginationDriverFactory } from '../components/Pagination/Pagination.protractor.driver';
-export const paginationTestkitFactory = protractorTestkitFactoryCreator(
-  paginationDriverFactory,
-);
+export const paginationTestkitFactory = protractorTestkitFactoryCreator(paginationDriverFactory)
 
 import { statesButtonDriverFactory } from '../components/StatesButton/StatesButton.driver';
-export const statesButtonTestkitFactory = protractorUniTestkitFactoryCreator(
-  statesButtonDriverFactory,
-);
+export const statesButtonTestkitFactory = protractorUniTestkitFactoryCreator(statesButtonDriverFactory)
 
 import { stripCardDriverFactory } from '../components/StripCard/StripCard.driver';
-export const stripCardTestkitFactory = protractorUniTestkitFactoryCreator(
-  stripCardDriverFactory,
-);
+export const stripCardTestkitFactory = protractorUniTestkitFactoryCreator(stripCardDriverFactory)
 
 import { tabsDriverFactory } from '../components/Tabs/Tabs.driver';
-export const tabsTestkitFactory = protractorUniTestkitFactoryCreator(
-  tabsDriverFactory,
-);
+export const tabsTestkitFactory = protractorUniTestkitFactoryCreator(tabsDriverFactory)
 
 import { textDriverFactory } from '../components/Text/Text.protractor.driver';
-export const textTestkitFactory = protractorTestkitFactoryCreator(
-  textDriverFactory,
-);
+export const textTestkitFactory = protractorTestkitFactoryCreator(textDriverFactory)
 
 import { textButtonDriverFactory } from '../components/TextButton/TextButton.driver';
-export const textButtonTestkitFactory = protractorUniTestkitFactoryCreator(
-  textButtonDriverFactory,
-);
+export const textButtonTestkitFactory = protractorUniTestkitFactoryCreator(textButtonDriverFactory)
 
 import { tooltipDriverFactory } from '../components/Tooltip/Tooltip.protractor.driver';
-export const tooltipTestkitFactory = protractorTestkitFactoryCreator(
-  tooltipDriverFactory,
-);
+export const tooltipTestkitFactory = protractorTestkitFactoryCreator(tooltipDriverFactory)
 
 import { avatarGroupDriverFactory } from '../components/AvatarGroup/AvatarGroup.driver';
-export const avatarGroupTestkitFactory = protractorUniTestkitFactoryCreator(
-  avatarGroupDriverFactory,
-);
+export const avatarGroupTestkitFactory = protractorUniTestkitFactoryCreator(avatarGroupDriverFactory)
+
