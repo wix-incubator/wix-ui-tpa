0a698e8dcf1da469d8f3bdd9aca3ce9086137af5
Jonathan Adler <jonathana@wix.com>
2019-07-18 17:55:15 +0300

    change storybook

--- src/components/NewCard/docs/index.story.tsx
+++ src/components/NewCard/docs/index.story.tsx
@@ -1,23 +1,6 @@
 import * as React from 'react';
 import { NewCard } from '../';
-import * as examples from './examples';
-import {
-  header,
-  api,
-  divider,
-  importExample,
-  playground,
-  tab,
-  code as baseCode,
-  tabs,
-  testkit,
-  title,
-} from 'wix-storybook-utils/Sections';
-import { allComponents } from '../../../../stories/utils/allComponents';
 import { NewCardExample } from './NewCardExample';
-
-const code = config =>
-  baseCode({ components: allComponents, compact: true, ...config });
 
 export default {
   category: 'Components',
@@ -31,32 +14,5 @@ export default {
     //
   },
   dataHook: 'storybook-NewCard',
-  sections: [
-    header(),
-    tabs([
-      tab({
-        title: 'Usage',
-        sections: [
-          importExample({
-            source: examples.importExample,
-          }),
-
-          divider(),
-
-          title('Examples'),
-
-          ...[
-            { title: 'Example', source: examples.example },
-          ].map(code),
-        ],
-      }),
-
-      ...[
-        { title: 'API', sections: [api()] },
-        { title: 'TestKit', sections: [testkit()] },
-        { title: 'Playground', sections: [playground()] },
-      ].map(tab),
-    ]),
-  ],
-  examples: <NewCardExample/>
+  examples: <NewCardExample />,
 };
