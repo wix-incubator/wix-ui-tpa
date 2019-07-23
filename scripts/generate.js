974e7a74760dcf0276e0883e9cea15ddbc328f38
Jonathan Adler <jonathana@wix.com>
2019-07-23 12:02:02 +0300

    use node instead of bash for child process

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,34 +1,37 @@
 const prompts = require('prompts');
+const spawn = require('child_process').spawn;
+
+const questions = [
+  {
+    type: 'confirm',
+    name: 'mobile',
+    message: 'Does the new component have a specific mobile design?',
+    initial: false,
+  },
+];
+const handlers = {
+  onCancel: () => {
+    console.error('Aborted');
+    process.exit(1);
+  },
+};
 
 async function main() {
-  let promptAborted = false;
-
-  const questions = [
-    {
-      type: 'confirm',
-      name: 'mobile',
-      message: 'Does the new component have a specific mobile design?',
-      initial: false,
-    },
-  ];
-
-  const handlers = {
-    onCancel: () => {
-      promptAborted = true;
-    },
-  };
-
   const answers = await prompts(questions, handlers);
-
-  if (promptAborted) {
-    console.error('Aborted.');
+  try {
+    const generation = spawn(
+      'npm',
+      ['run', `generate:component${answers.mobile ? ':mobile' : ''}`],
+      { detached: false, stdio: 'inherit' },
+    );
+    generation.on('close', code => {
+      if (code !== 0) {
+        process.exit(code);
+      }
+    });
+  } catch (e) {
+    console.error(e);
     process.exit(1);
-  }
-
-  if (answers.mobile) {
-    process.exit(101);
-  } else {
-    process.exit(102);
   }
 }
 
