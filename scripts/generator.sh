ea91a416e3ae804ea8c6c9e56cd08d92525ed7e8
Jonathan Adler <jonathana@wix.com>
2019-07-23 16:00:16 +0300

    upgrade generator a bit - have the same styles as wuf's generator

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
+v='\xE2\x9c\x94'
+x='\xe2\x9c\x96'
+dots='\xE2\x80\xa6'
+caret='\xE2\x80\xba'
 boldText=$(tput bold)
 normalText=$(tput sgr0)
 whiteText=$(tput setaf 7)
+question="Does the new component have a special design/behaviour for mobile (y/n)"
+
+print_success () {
+    echo -en "\033[1A\033[2K"
+    echo -e "$(tput setaf 2)${v}$(tput setaf 7) ${whiteText}${boldText}${question}${normalText} ${dots} $1"
+}
+
+print_fail () {
+    echo -en "\033[1A\033[2K"
+    echo -e "$(tput setaf 1)${x}$(tput setaf 7) ${whiteText}${boldText}${question}${normalText} ${dots} $1"
+}
+
 tput setaf 6
-read -p "? ${whiteText}${boldText}Does the new component have a special design/behaviour for mobile (y/n)${normalText} › " CHOICE
+echo -ne "? ${whiteText}${boldText}${question}${normalText} ${caret} "
+read CHOICE
 
 case "$CHOICE" in
 y|Y )
+    print_success ${CHOICE}
     npm run generate:component:mobile
     ;;
 n|N )
+    print_success ${CHOICE}
     npm run generate:component
     ;;
 * )
+    print_fail ${CHOICE}
     echo "please choose y or n"
+    exit 1
     ;;
 esac
