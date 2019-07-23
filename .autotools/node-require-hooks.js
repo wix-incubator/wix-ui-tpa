43298673ad05c9bf2496936103d4295d11a35bcf
Jonathan Adler <jonathana@wix.com>
2019-07-23 10:53:06 +0300

    Fix ui-autotools sanity tests with svg's (#151)
    * fix ssr tests of components with svgs - but having hydration tests filing
    
    * fix configuration for svg ui-autotools tests to work

---
+++ .autotools/node-require-hooks.js
@@ -0,0 +1,41 @@
+const { resolveNamespaceFactory } = require('@stylable/node');
+const project = require('yoshi-config');
+require('yoshi-helpers/require-hooks').setupRequireHooks();
+require('@stylable/node').attachHook({
+  stylableConfig: {
+    resolveNamespace: resolveNamespaceFactory(project.name),
+  },
+});
+const path = require('path');
+const fs = require('fs');
+const svgr = require('@svgr/core');
+const babelCore = require('@babel/core');
+
+require.extensions['.svg'] = mockSvg;
+
+function mockSvg(module) {
+  const filePathName = path.basename(module.filename);
+  const svgFilename = JSON.stringify(filePathName);
+  const svgContent = fs.readFileSync(module.filename, 'utf8');
+  const parsedSvg = svgr.default.sync(svgContent, {
+    plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+    svgoConfig: {
+      plugins: {
+        removeViewBox: false,
+      },
+    },
+  });
+  const parsed = babelCore.transform(parsedSvg, {
+    plugins: [
+      '@babel/plugin-transform-react-jsx',
+      '@babel/plugin-transform-modules-commonjs',
+    ],
+  });
+  // tslint:disable-next-line:no-eval
+  const component = eval(parsed.code);
+
+  module.exports = {
+    default: svgFilename,
+    ReactComponent: component,
+  };
+}
--- src/components/Avatar/Avatar.meta.tsx
+++ src/components/Avatar/Avatar.meta.tsx
@@ -1,30 +1,27 @@
-// The `ui-autotools` tool, doesn’t support importing svg files as react elements.
+import { Avatar, AvatarSize } from '..';
+import Registry from '@ui-autotools/registry';
 
-// import { Avatar } from '.';
-// import Registry from '@ui-autotools/registry';
-// import { AvatarSize } from './Avatar';
+const AvatarMetadata = Registry.getComponentMetadata(Avatar);
+AvatarMetadata.nonReactStrictModeCompliant = true;
 
-// const AvatarMetadata = Registry.getComponentMetadata(Avatar);
-// AvatarMetadata.nonReactStrictModeCompliant = true;
+AvatarMetadata.addSim({
+  title: 'render anonymous',
+  props: {},
+});
 
-// AvatarMetadata.addSim({
-//   title: 'render anonymous',
-//   props: {},
-// });
+AvatarMetadata.addSim({
+  title: 'render small with image',
+  props: {
+    size: AvatarSize.small,
+    name: 'John',
+  },
+});
 
-// AvatarMetadata.addSim({
-//   title: 'render small with image',
-//   props: {
-//     size: AvatarSize.small,
-//     name: 'John',
-//   },
-// });
-
-// AvatarMetadata.addSim({
-//   title: 'render medium with image',
-//   props: {
-//     size: AvatarSize.medium,
-//     name: 'John',
-//     src: 'https://randomuser.me/api/portraits/men/65.jpg',
-//   },
-// });
+AvatarMetadata.addSim({
+  title: 'render medium with image',
+  props: {
+    size: AvatarSize.medium,
+    name: 'John',
+    src: 'https://randomuser.me/api/portraits/men/65.jpg',
+  },
+});
