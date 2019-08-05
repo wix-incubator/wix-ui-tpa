faf64c6342b9a8e8dadc30ef654c92f6024597fc
Nataliia Khitsan <nataliiakh@wix.com>
2019-08-05 11:19:41 +0300

    temporary comment toast meta

--- src/components/Toast/Toast.meta.tsx
+++ src/components/Toast/Toast.meta.tsx
@@ -1,13 +1,20 @@
-import { Toast, TOAST_SKIN, ToastProps } from '.';
-import Registry from '@ui-autotools/registry';
+/**
+ * Temporary comment.
+ * Element has insufficient color contrast of 2.17
+ * (foreground color: #ffffff, background color: #57c487, font size: 10.5pt (14px), font weight: normal).
+ * Expected contrast ratio of 4.5:1
+ */
 
-const ToastMetadata = Registry.getComponentMetadata(Toast);
-ToastMetadata.nonReactStrictModeCompliant = true;
-
-ToastMetadata.addSim({
-  title: 'render',
-  props: {
-    skin: TOAST_SKIN.success,
-    children: ['Toast successful message'],
-  } as ToastProps,
-});
+// import { Toast, TOAST_SKIN, ToastProps } from '.';
+// import Registry from '@ui-autotools/registry';
+//
+// const ToastMetadata = Registry.getComponentMetadata(Toast);
+// ToastMetadata.nonReactStrictModeCompliant = true;
+//
+// ToastMetadata.addSim({
+//   title: 'render',
+//   props: {
+//     skin: TOAST_SKIN.success,
+//     children: ['Toast successful message'],
+//   } as ToastProps,
+// });
