9247ed5bde770f4331b9e46c75d86c62090334be
Nataliia Khitsan <nataliiakh@wix.com>
2019-08-08 14:48:57 +0300

    uncomment meta

--- src/components/Toast/Toast.meta.tsx
+++ src/components/Toast/Toast.meta.tsx
@@ -1,20 +1,27 @@
-/**
- * Temporary comment.
- * Element has insufficient color contrast of 2.17
- * (foreground color: #ffffff, background color: #57c487, font size: 10.5pt (14px), font weight: normal).
- * Expected contrast ratio of 4.5:1
- */
+import { Toast, TOAST_SKIN, ToastProps } from '.';
+import Registry from '@ui-autotools/registry';
+import { skinMessages } from './helpers';
 
-// import { Toast, TOAST_SKIN, ToastProps } from '.';
-// import Registry from '@ui-autotools/registry';
-//
-// const ToastMetadata = Registry.getComponentMetadata(Toast);
-// ToastMetadata.nonReactStrictModeCompliant = true;
-//
-// ToastMetadata.addSim({
-//   title: 'render',
-//   props: {
-//     skin: TOAST_SKIN.success,
-//     children: ['Toast successful message'],
-//   } as ToastProps,
-// });
+const ToastMetadata = Registry.getComponentMetadata(Toast);
+
+ToastMetadata.nonReactStrictModeCompliant = true;
+ToastMetadata.nonA11yCompliant = true;
+
+Object.values(TOAST_SKIN).forEach(skin => {
+  ToastMetadata.addSim({
+    title: skin,
+    props: {
+      shouldShowCloseButton: true,
+      skin,
+      children: skinMessages[skin],
+    } as ToastProps,
+  });
+
+  ToastMetadata.addSim({
+    title: skin,
+    props: {
+      skin,
+      children: skinMessages[skin],
+    } as ToastProps,
+  });
+});
