4b4f04baea7ced4a6b37d075fda53db348af0fa6
Jonathan Adler <jonathana@wix.com>
2019-08-20 16:36:04 +0300

    Add timeout for avatargroup test

--- src/components/AvatarGroup/AvatarGroup.visual.tsx
+++ src/components/AvatarGroup/AvatarGroup.visual.tsx
@@ -18,7 +18,7 @@ class AvatarGroupVisual extends React.Component<any> {
   render() {
     return (
       <VisualTestContainer
-        hook={() => new Promise(res => setTimeout(res, 500))}
+        hook={() => new Promise(res => setTimeout(res, 1000))}
       >
         <AvatarGroup {...this.props} />
       </VisualTestContainer>
