f228b726fa9262b1b80f9b52653bfffe51cdd308
Jonathan Adler <jonathana@wix.com>
2019-08-27 12:12:46 +0300

    Remove styleabledomutils from generators (#181)
    * In generator - remove unused stylableDomUtils + replace it with data- where needed
    
    * fix generated mobile comp driver

--- .wuf/generator/templates/desktop/Component/Component.driver.ts
+++ .wuf/generator/templates/desktop/Component/Component.driver.ts
@@ -2,16 +2,13 @@ import {
   BaseUniDriver,
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
-import { StylableUnidriverUtil, UniDriver } from 'wix-ui-test-utils/unidriver';
-import style from './{%ComponentName%}.st.css';
+import { UniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface {%ComponentName%}Driver extends BaseUniDriver {
 
 }
 
 export const {%componentName%}DriverFactory = (base: UniDriver): {%ComponentName%}Driver => {
-  const stylableUtil = new StylableUnidriverUtil(style);
-
   return {
     ...baseUniDriverFactory(base),
   };
--- .wuf/generator/templates/mobile/Component/Component.driver.ts
+++ .wuf/generator/templates/mobile/Component/Component.driver.ts
@@ -2,18 +2,14 @@ import {
   BaseUniDriver,
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
-import { StylableUnidriverUtil, UniDriver } from 'wix-ui-test-utils/unidriver';
-import style from './{%ComponentName%}.st.css';
 
 export interface {%ComponentName%}Driver extends BaseUniDriver {
   isMobile(): Promise<boolean>;
 }
 
 export const {%componentName%}DriverFactory = (base: UniDriver): {%ComponentName%}Driver => {
-  const stylableUtil = new StylableUnidriverUtil(style);
-
   return {
     ...baseUniDriverFactory(base),
-    isMobile: async () => stylableUtil.hasStyleState(base, 'mobile'),
+    isMobile: async () => (await base.attr('data-mobile')) === 'true',
   };
 };
