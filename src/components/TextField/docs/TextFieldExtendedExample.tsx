9d01d31507792f60aba9d2361afc673d37e21e49
Andrii Tsarenko <andriits@wix.com>
2019-08-30 15:36:05 +0300

    feat(TextField): changes according to design review

--- src/components/TextField/docs/TextFieldExtendedExample.tsx
+++ src/components/TextField/docs/TextFieldExtendedExample.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 import extendedStyles from './TextFieldExtendedExample.st.css';
 import { TextField, TextFieldProps } from '../TextField';
+import { TextFieldTheme } from '../TextFieldEnums';
 
 export const TextFieldExtendedExample: React.FunctionComponent<
   TextFieldProps
-> = props => <TextField {...props} {...extendedStyles('root', {}, props)} />;
+> = props => (
+  <>
+    <TextField {...props} {...extendedStyles('root', {}, props)} />
+    <br />
+    <TextField
+      {...{ ...props, theme: TextFieldTheme.Line }}
+      {...extendedStyles('root', {}, props)}
+    />
+  </>
+);
