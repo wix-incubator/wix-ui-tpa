3fc6be1d578995e0d2808582f3e04ae86d3e6026
Jonathan Adler <jonathana@wix.com>
2019-09-03 16:12:16 +0300

    comment out textField e2e tests - temporaray - // TODO move to visual tests

--- src/components/TextField/TextField.e2e.ts
+++ src/components/TextField/TextField.e2e.ts
@@ -7,31 +7,31 @@ import {
 import { textFieldTestkitFactory } from '../../testkit/protractor';
 import { dataHooks } from './docs/testData';
 
-['TextFieldLTR', 'TextFieldRLT'].forEach(story => {
-  describe(`Visual tests ${story}`, () => {
-    beforeAll(async () => {
-      const storyUrl = createStoryUrl({
-        kind: 'Tests',
-        story,
-        withExamples: true,
-      });
-      await browser.get(storyUrl);
-    });
-
-    dataHooks.forEach(dataHook => {
-      eyes.it(`${story}-${dataHook}-hover`, async () => {
-        const driver = textFieldTestkitFactory({ dataHook });
-        await waitForVisibilityOf(driver.element(), 'Cannot find TextField');
-        expect(await driver.element().isDisplayed()).toBe(true);
-        await driver.hover();
-      });
-
-      eyes.it(`${story}-${dataHook}-focus`, async () => {
-        const driver = textFieldTestkitFactory({ dataHook });
-        await waitForVisibilityOf(driver.element(), 'Cannot find TextField');
-        expect(await driver.element().isDisplayed()).toBe(true);
-        await driver.focus();
-      });
-    });
-  });
-});
+// ['TextFieldLTR', 'TextFieldRLT'].forEach(story => {
+//   describe(`Visual tests ${story}`, () => {
+//     beforeAll(async () => {
+//       const storyUrl = createStoryUrl({
+//         kind: 'Tests',
+//         story,
+//         withExamples: true,
+//       });
+//       await browser.get(storyUrl);
+//     });
+//
+//     dataHooks.forEach(dataHook => {
+//       eyes.it(`${story}-${dataHook}-hover`, async () => {
+//         const driver = textFieldTestkitFactory({ dataHook });
+//         await waitForVisibilityOf(driver.element(), 'Cannot find TextField');
+//         expect(await driver.element().isDisplayed()).toBe(true);
+//         await driver.hover();
+//       });
+//
+//       eyes.it(`${story}-${dataHook}-focus`, async () => {
+//         const driver = textFieldTestkitFactory({ dataHook });
+//         await waitForVisibilityOf(driver.element(), 'Cannot find TextField');
+//         expect(await driver.element().isDisplayed()).toBe(true);
+//         await driver.focus();
+//       });
+//     });
+//   });
+// });
