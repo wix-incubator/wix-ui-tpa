3c07430afb017f67dda69ad6f7519aebcca8b8d1
Dovile Lapinskaite <dovilel@wix.com>
2019-09-12 15:18:03 +0300

    add more tests

--- src/components/AvatarGroup/AvatarGroup.spec.tsx
+++ src/components/AvatarGroup/AvatarGroup.spec.tsx
@@ -7,6 +7,7 @@ import { avatarGroupDriverFactory } from './AvatarGroup.driver';
 import { AvatarGroup } from './';
 import { avatarGroupTestkitFactory } from '../../testkit';
 import { avatarGroupTestkitFactory as enzymeAvatarGroupTestkitFactory } from '../../testkit/enzyme';
+import { TextButton } from '../TextButton';
 
 describe('AvatarGroup', () => {
   const createDriver = createUniDriverFactory(avatarGroupDriverFactory);
@@ -63,14 +64,56 @@ describe('AvatarGroup', () => {
     expect(await driver.getAvatarCount()).toBe(9);
   });
 
-  it('should have optional text link', async () => {
-    const onClick = jest.fn();
-    const driver = createDriver(
-      <AvatarGroup items={[...items]}>
-        <AvatarGroup.TextButton onClick={onClick}>Link</AvatarGroup.TextButton>
-      </AvatarGroup>,
-    );
-    await driver.textButton.click();
-    expect(onClick).toHaveBeenCalled();
+  describe('optional text button', () => {
+    it('should have optional text button', async () => {
+      const onClick = jest.fn();
+      const driver = createDriver(
+        <AvatarGroup items={[...items]}>
+          <AvatarGroup.TextButton onClick={onClick}>
+            Link
+          </AvatarGroup.TextButton>
+        </AvatarGroup>,
+      );
+      expect(await driver.textButton.exists()).toBe(true);
+      await driver.textButton.click();
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should accept regular TextButton', async () => {
+      const onClick = jest.fn();
+      const driver = createDriver(
+        <AvatarGroup items={[...items]}>
+          <TextButton onClick={onClick}>Link</TextButton>
+        </AvatarGroup>,
+      );
+      await driver.textButton.click();
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should pass props on to TextButton', async () => {
+      const driver = createDriver(
+        <AvatarGroup items={[...items]}>
+          <AvatarGroup.TextButton as="a" href="https://some-url.com">
+            Link
+          </AvatarGroup.TextButton>
+        </AvatarGroup>,
+      );
+      expect((await driver.textButton.element()).getAttribute('href')).toBe(
+        'https://some-url.com',
+      );
+      expect((await driver.textButton.element()).tagName.toLowerCase()).toBe(
+        'a',
+      );
+    });
+
+    it('should ignore anything else than (AvatarGroup.)TextButton', async () => {
+      const driver = createDriver(
+        <AvatarGroup items={[...items]}>
+          <div>Imposter</div>
+          Another imposter
+        </AvatarGroup>,
+      );
+      expect(await driver.textButton.exists()).toBe(false);
+    });
   });
 });
