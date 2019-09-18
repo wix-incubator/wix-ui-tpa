cd3b739c0cadf7decde42a65b4d658646f1afe6e
Jonathan Adler <jonathana@wix.com>
2019-09-18 11:11:04 +0300

    feat: Add settings panel to generator (#199)
    Add wiring example to component generators

---
+++ .wuf/generator/templates/mobile/Component/docs/ComponentWiringExample.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { {%ComponentName%} } from '../';
+import styles from './{%ComponentName%}WiringExample.st.css';
+
+export const {%ComponentName%}WiringExample = () => {
+  return <{%ComponentName%} className={styles.component} />;
+};
