d5afa7a77df52f82751be352ff53412d37e01ae8
Jonathan Adler <jonathana@wix.com>
2019-09-22 17:18:47 +0300

    Add print

--- applitools.config.js
+++ applitools.config.js
@@ -6,4 +6,6 @@ try {
   privateConfig = require('./applitools.private.config.js');
 } catch (e) {}
 
+console.log('adler', 'applitools.config.js:9', process.env.APPLITOOLS_SERVER_URL);
+
 module.exports = applitoolsConfig({ config: merge(privateConfig, { serverUrl: process.env.APPLITOOLS_SERVER_URL}) });
