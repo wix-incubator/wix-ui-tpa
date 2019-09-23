70384a82de51b7b0be33db9c10aadfc2ffc92c04
Nikita Shulipa <mykytas@wix.com>
2019-09-23 12:25:19 +0300

    Fix item content centering and settings panel example

--- src/components/ActionsMenu/Divider/Divider.tsx
+++ src/components/ActionsMenu/Divider/Divider.tsx
@@ -3,7 +3,7 @@ import { Divider } from '../../Divider';
 import styles from './Divider.st.css';
 
 const ActionsMenuDivider: React.FC = props => (
-    <Divider {...props} {...styles('root', {}, props)} />
+  <Divider {...props} {...styles('root', {}, props)} />
 );
 ActionsMenuDivider.displayName = 'ActionsMenu.Divider';
 
