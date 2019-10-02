8c8b8c08feea8d388a40d860995086ea7169d345
Jonathan Adler <jonathana@wix.com>
2019-10-02 13:39:28 +0300

    remove logs

--- applitools.config.js
+++ applitools.config.js
@@ -6,18 +6,10 @@ try {
   privateConfig = require('./applitools.private.config.js');
 } catch (e) {}
 
-console.log(
-  'adler',
-  'applitools.config.js:9',
-  process.env.APPLITOOLS_SERVER_URL,
-);
-
 const finalConfig = applitoolsConfig({
   config: merge(privateConfig, {
     serverUrl: process.env.APPLITOOLS_SERVER_URL,
   }),
 });
-
-console.log('adler', 'applitools.config.js:21', finalConfig);
 
 module.exports = finalConfig;
