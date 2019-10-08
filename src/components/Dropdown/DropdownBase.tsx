67f09abe00dcfa39ea7392c71a6b7f8e36855ff6
Nataliia Khitsan <nataliiakh@wix.com>
2019-10-08 15:37:30 +0300

    use TPAbutton instead of TPAInput for DropdownBase

--- src/components/Dropdown/DropdownBase.tsx
+++ src/components/Dropdown/DropdownBase.tsx
@@ -1,26 +1,35 @@
 import * as React from 'react';
 import { ChevronDownSmall } from 'wix-ui-icons-common/dist/src';
-import { Input, InputProps } from '../Input';
 import { DATA_HOOKS, ICON_SIZE } from './constants';
 
 import styles from './DropdownBase.st.css';
+import { Button } from '../Button';
 
-export const DropdownBase = (props: InputProps) => {
+interface DropdownBaseProps {
+  value: string;
+  placeholder: string;
+  disabled: boolean;
+  error: boolean;
+}
+
+export const DropdownBase = (props: DropdownBaseProps) => {
+  const { value, placeholder, error, disabled } = props;
   return (
-    <Input
-      {...props}
-      {...styles('root', { error: props.error }, props)}
+    <Button
+      fullWidth
+      {...styles('root', { error, placeholder: !value }, props)}
       data-hook={DATA_HOOKS.base}
-      data-dropdown-base-error={props.error}
-      readOnly
-      aria-label="selected-value"
-      suffix={
+      data-dropdown-base-error={error}
+      disabled={disabled}
+      suffixIcon={
         <ChevronDownSmall
           className={styles.arrowIcon}
           width={ICON_SIZE}
           height={ICON_SIZE}
         />
       }
-    />
+    >
+      {value || placeholder}
+    </Button>
   );
 };
