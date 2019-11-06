e0dec5f0751ecd8579ecbd2283979cb8fe8429ce
Nataliia Khitsan <nataliiakh@wix.com>
2019-11-06 19:26:04 +0200

    more meta tests

--- src/components/Modal/Modal.meta.tsx
+++ src/components/Modal/Modal.meta.tsx
@@ -1,14 +1,43 @@
-import {Modal, ModalProps} from '.';
+import { Modal, ModalProps } from '.';
 import Registry from '@ui-autotools/registry';
+import {bigContent} from "./helpers";
 
 const ModalMetadata = Registry.getComponentMetadata(Modal);
 ModalMetadata.nonReactStrictModeCompliant = true;
 
 ModalMetadata.addSim({
-  title: 'render',
+  title: 'Minimal height',
   props: {
     isOpen: true,
     onRequestClose: () => {},
-    children: ['Hello from modal']
+    children: ['Hello from modal'],
+  } as ModalProps,
+});
+
+ModalMetadata.addSim({
+  title: 'Maximum height',
+  props: {
+    isOpen: true,
+    onRequestClose: () => {},
+    children: [bigContent],
+  } as ModalProps,
+});
+
+ModalMetadata.addSim({
+  title: 'Without button',
+  props: {
+    isOpen: true,
+    onRequestClose: () => {},
+    withCloseButton: false,
+  } as ModalProps,
+});
+
+
+ModalMetadata.addSim({
+  title: 'Without background',
+  props: {
+    isOpen: true,
+    onRequestClose: () => {},
+    withBackground: false,
   } as ModalProps,
 });
