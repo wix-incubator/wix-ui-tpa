9591a2a59d425e0d93962ab391ee0a9f2067a7c6
Jonathan Adler <jonathana@wix.com>
2019-11-14 14:16:19 +0200

    save

--- scripts/tag.js
+++ scripts/tag.js
@@ -6,11 +6,12 @@ function run() {
     try {
       const version = process.env.npm_package_version;
       const tagName = `${version.startsWith('v') ? '' : 'v'}${version}`;
+      console.log('adler', 'tag.js:9', version, tagName, `git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`);
 
-      execute(`git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`, true);
-      execute(`git push origin ${tagName}`, true);
+    //   execute(`git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`, true);
+    //   execute(`git push origin ${tagName}`, true);
     } catch (e) {
-      logger.error("Couldn't add a tag");
+      logger.error("Couldn't add a tag", e);
     }
   } else {
     logger.warn('Not in a CI agent. Git tagging is skipped.');
