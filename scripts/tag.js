deac4e4a1e68ccf4e10db141628ef393985cf51f
Jonathan Adler <jonathana@wix.com>
2019-11-17 14:33:13 +0200

    fix cmd executor

--- scripts/tag.js
+++ scripts/tag.js
@@ -6,10 +6,9 @@ function run() {
     try {
       const version = process.env.npm_package_version;
       const tagName = `${version.startsWith('v') ? '' : 'v'}${version}`;
-      console.log('adler', 'tag.js:9', version, tagName, `git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`);
 
-    //   execute(`git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`, true);
-    //   execute(`git push origin ${tagName}`, true);
+      execute(`git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`, true);
+      execute(`git push origin ${tagName}`, true);
     } catch (e) {
       logger.error("Couldn't add a tag", e);
     }
