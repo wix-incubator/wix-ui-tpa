8f8aa4e1fdf9481b15378b7a9eac944bba6de044
Vytenis Urbonavičius <vytenisu@wix.com>
2019-11-18 13:39:27 +0200

    Automatic settings panel

--- src/components/TextArea/docs/index.story.tsx
+++ src/components/TextArea/docs/index.story.tsx
@@ -13,9 +13,7 @@ import {
   title,
 } from 'wix-storybook-utils/Sections';
 import { allComponents } from '../../../../stories/utils/allComponents';
-import { settingsPanel } from '../../../../stories/utils/SettingsPanel';
-import * as extendedRawSource from '!raw-loader!../../../generated/examples/TextArea/index.example.tsx';
-import * as extendedCSSRawSource from '!raw-loader!../../../generated/examples/TextArea/TextArea.example.st.css';
+import { autoSettingsPanel } from '../../../../stories/utils/SettingsPanel';
 import { TextArea } from '../../../generated/examples/TextArea';
 import { TextAreaTheme } from '../TextAreaEnums';
 import { settingsApi } from '../../../../stories/utils/settingsApi';
@@ -96,72 +94,13 @@ export default {
       ...[
         { title: 'API', sections: [api()] },
         { title: 'TestKit', sections: [testkit()] },
-        { title: 'Playground', sections: [playground()] },
+        {
+          title: 'Playground',
+          sections: [playground(), autoSettingsPanel('TextArea')],
+        },
         {
           title: 'Settings API',
           sections: [settingsApi('TextArea')],
-        },
-        {
-          title: 'Settings Panel',
-          sections: [
-            settingsPanel({
-              example: <TextArea />,
-              rawSource: extendedRawSource,
-              title: 'Text area preferences',
-              rawCSSSource: extendedCSSRawSource,
-              params: {
-                fonts: [
-                  {
-                    label: 'Font',
-                    wixParam: 'textFont',
-                    defaultFont: 'arial',
-                  },
-                ],
-                colors: [
-                  {
-                    label: 'ErrorBorderColor',
-                    wixParam: 'errorBorderColor',
-                    defaultColor: 'color-13',
-                  },
-                  {
-                    label: 'Disabled border color',
-                    wixParam: 'disabledBorderColor',
-                    defaultColor: 'color-3',
-                  },
-                  {
-                    label: 'SuccessBorderColor',
-                    wixParam: 'successBorderColor',
-                    defaultColor: 'color-18',
-                  },
-                  {
-                    label: 'BackgroundColor',
-                    wixParam: 'backgroundColor',
-                    defaultColor: 'color-1',
-                  },
-                  {
-                    label: 'BorderColor',
-                    wixParam: 'borderColor',
-                    defaultColor: 'color-5',
-                  },
-                  {
-                    label: 'TextColor',
-                    wixParam: 'textColor',
-                    defaultColor: 'color-5',
-                  },
-                  {
-                    label: 'DisabledTextColor',
-                    wixParam: 'disabledTextColor',
-                    defaultColor: 'color-3',
-                  },
-                  {
-                    label: 'PlaceholderColor',
-                    wixParam: 'placeholderColor',
-                    defaultColor: 'color-3',
-                  },
-                ],
-              },
-            }),
-          ],
         },
       ].map(tab),
     ]),
--- stories/utils/SettingsPanel.tsx
+++ stories/utils/SettingsPanel.tsx
@@ -6,6 +6,21 @@ import {
   IWixNumberParam,
   MockSettings,
 } from '../helperComponents/MockSettings';
+import manifest from '../../ui-tpa-manifest.json';
+import Markdown from 'wix-storybook-utils/Markdown';
+
+const enum VariableType {
+  color = 'color',
+  font = 'font',
+  numnber = 'number',
+}
+
+interface IVariableManifest {
+  name: string;
+  type: VariableType;
+  defaultValue: string;
+  description: string;
+}
 
 interface ISettingsPanelConfig {
   example: JSX.Element;
@@ -17,6 +32,66 @@ interface ISettingsPanelConfig {
   rawSource: typeof import('*.tsx');
   rawCSSSource: typeof import('*.st.css');
   title: string;
+}
+
+export function autoSettingsPanel(componentName: string) {
+  const componentManifest = manifest[componentName];
+
+  if (!componentManifest) {
+    return 'Could not load Settings API';
+  }
+
+  const { variables } = componentManifest.stylable;
+
+  // TODO: implement numbers
+  const numbers = [];
+
+  const fonts = Object.values(variables)
+    .filter(
+      (variableInfo: IVariableManifest) =>
+        variableInfo.type === VariableType.font,
+    )
+    .map((variableInfo: IVariableManifest) => ({
+      label: variableInfo.name,
+      wixParam: `${componentName}-${variableInfo.name}`,
+      defaultFont: 'arial',
+    }));
+
+  const colors = Object.values(variables)
+    .filter(
+      (variableInfo: IVariableManifest) =>
+        variableInfo.type === VariableType.color,
+    )
+    .map((variableInfo: IVariableManifest) => ({
+      label: variableInfo.name,
+      wixParam: `${componentName}-${variableInfo.name}`,
+      defaultColor: variableInfo.defaultValue,
+    }));
+
+  const styleExample = require(`!raw-loader!../../src/generated/examples/${componentName}/${componentName}.example.st.css`);
+  const logicExample = require(`!raw-loader!../../src/generated/examples/${componentName}/index.example.tsx`);
+
+  return (
+    <div>
+      <MockSettings
+        wixNumberParams={numbers}
+        wixColorParams={colors}
+        wixFontParams={fonts}
+      />
+      <Markdown
+        source={[
+          '#### .st.css',
+          '```css',
+          styleExample,
+          '```',
+          '#### .tsx',
+          '```javascript',
+          logicExample,
+          '```',
+        ].join('\n')}
+      />
+    </div>
+  );
 }
 
 export function settingsPanel({
