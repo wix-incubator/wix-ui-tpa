e85ebfb080c91d8cf44caff4d4ffdb20e25930ef
Jonathan Adler <jonathana@wix.com>
2019-11-20 11:56:00 +0200

    fix pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-
 import {
   Pagination as CorePagination,
   PaginationProps as CorePaginationProps,
 } from 'wix-ui-core/pagination';
-
 import { DoubleChevronLeft } from '../../assets/icons/components/DoubleChevronLeft';
 import { DoubleChevronRight } from '../../assets/icons/components/DoubleChevronRight';
 import { ChevronLeft } from '../../assets/icons/components/ChevronLeft';
 import { ChevronRight } from '../../assets/icons/components/ChevronRight';
-
 import { TPAComponentsConsumer } from '../TPAComponentsConfig';
-
 import styles from './Pagination.st.css';
 import { TPAComponentProps } from '../../types';
 
@@ -27,10 +23,10 @@ export const Pagination: React.FunctionComponent<PaginationProps> = props => {
         return (
           <CorePagination
             {...styles('root', { mobile, rtl }, props)}
-            previousLabel={ChevronLeft}
-            nextLabel={ChevronRight}
-            firstLabel={DoubleChevronLeft}
-            lastLabel={DoubleChevronRight}
+            previousLabel={<ChevronLeft />}
+            nextLabel={<ChevronRight />}
+            firstLabel={<DoubleChevronLeft />}
+            lastLabel={<DoubleChevronRight />}
             rtl={rtl}
             showFirstLastNavButtons={props.totalPages > props.maxPagesToShow}
             paginationMode={mobile ? 'compact' : 'pages'}
