c554699b6c4b6f29ffde3636ea230ce8207341b4
Vytenis Urbonavičius <vytenisu@wix.com>
2019-11-21 15:50:57 +0200

    PR

--- webpack.config.storybook.js
+++ webpack.config.storybook.js
@@ -7,7 +7,7 @@ const { resolveNamespaceFactory } = require('@stylable/node');
 const autoprefixer = require('autoprefixer')({ grid: true, overrideBrowserslist: ['>1%'] });
 const postcss = require('postcss');
 const autoprefixProcessor = postcss([autoprefixer]);
-const stylableMetadataPlugin = require('./scripts/stylable-metadata-plugin')
+const stylableMetadataPlugin = require('./.storybook/stylable-metadata-plugin')
 
 //TODO - this should be configured inside yoshi
 function reconfigureStylable(config) {
