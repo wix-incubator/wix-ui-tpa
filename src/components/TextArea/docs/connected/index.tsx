a1bfc3fd754a36f3602b4290b2f7347b605a98d3
Vytenis Urbonavičius <vytenisu@wix.com>
2019-11-21 16:21:08 +0200

    Rename to make Stylable file names unique (limitation on TPA project)

--- src/components/TextArea/docs/connected/index.tsx
+++ src/components/TextArea/docs/connected/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styles from './TextArea.st.css';
+import styles from './TextAreaOverrides.st.css';
 import { TextArea as TextAreaOrig } from '../../TextArea';
 
 export const TextArea = props => (
