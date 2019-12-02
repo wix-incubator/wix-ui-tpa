41c3d86756140d6974e50c0aa706fa1880235c2a
Jonathan Adler <jonathana@wix.com>
2019-12-02 14:21:30 +0200

    add icons to index

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,10 @@
+export { ReactComponent as CaretDown } from './CaretDown.svg';
 export { ReactComponent as Check } from './Check.svg';
 export { ReactComponent as CheckSuccess } from './CheckSuccess.svg';
+export { ReactComponent as Close } from './Close.svg';
+export { ReactComponent as Error } from './Error.svg';
+export { ReactComponent as Heart } from './Heart.svg';
 export { ReactComponent as Minus } from './minus.svg';
 export { ReactComponent as Plus } from './plus.svg';
-export { ReactComponent as Error } from './Error.svg';
-export { ReactComponent as Close } from './Close.svg';
-export { ReactComponent as Heart } from './Heart.svg';
+export { ReactComponent as Share } from './Share.svg';
+export { ReactComponent as Star } from './Star.svg';
