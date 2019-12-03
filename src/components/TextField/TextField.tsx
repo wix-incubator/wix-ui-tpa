763a4573856fff39e4715b0b159d74fa9965a594
ariel7342 <ariell@wix.com>
2019-12-03 16:32:22 +0200

    #EE-21534 | fix e2e in wix-ui-tpa

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -51,7 +51,8 @@ export class TextField extends React.Component<TextFieldProps> {
           placement="top-end"
           skin={TooltipSkin.Error}
           content={errorMessage}
-        moveBy={{ x: 5, y: 0 }}>
+          moveBy={{ x: 5, y: 0 }}
+        >
           <ErrorIcon />
         </Tooltip>
       );
