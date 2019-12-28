63ac9900ed01887882ec5474c9ee17a13b5408a9
roib <roib@wix.com>
2019-12-28 22:43:28 +0200

    npm run build is failing. can't run tests.

--- src/components/ProGallery/ProGallery.driver.ts
+++ src/components/ProGallery/ProGallery.driver.ts
@@ -5,37 +5,17 @@ import {
 import { UniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface ProGalleryDriver extends BaseUniDriver {
-  clickOnArrow(): Promise<void>;
-
+  clickOnArrow(direction: string): Promise<void>;
   getArrowsShown(): Promise<string>;
-
-  clickOnArrowBack(): Promise<void>;
-
-  getContainerShown(): Promise<string>;
-
-  getGallerIdShown(): Promise<string>;
-
   getInnerHTML(): Promise<string>;
 }
 
 export const proGalleryDriverFactory = (base: UniDriver): ProGalleryDriver => {
-  // const hasArrows = async (direction: string) => {
-  //   const dataHookOfButton = `button[data-hook='nav-arrow-${direction}']`;
-  //
-  //   const hasArrowElement = await base.$(dataHookOfButton);
-  //   return !!hasArrowElement;
-  // };
-  // const clickOnArrow = async (direction: string) => {
-  //   const dataHookOfButton = `button[data-hook='nav-arrow-${direction}']`;
-  //   await base.$(dataHookOfButton).click();
-  // };
   return {
     ...baseUniDriverFactory(base),
     getArrowsShown: () => base.attr("[data-hook='nav-arrow-next']"),
-    getContainerShown: () => base.attr('#pro-gallery-container'),
-    getGallerIdShown: () => base.attr('#pro-gallery-1618'),
-    clickOnArrow: () => base.$("[data-hook='nav-arrow-next']").click(),
-    clickOnArrowBack: () => base.$("[data-hook='nav-arrow-back']").click(),
+    clickOnArrow: (direction: string) =>
+      base.$(`[data-hook='nav-arrow-${direction}']`).click(),
     getInnerHTML: () => base._prop('innerHTML'),
   };
 };
--- src/components/ProGallery/ProGallery.e2e.ts
+++ src/components/ProGallery/ProGallery.e2e.ts
@@ -6,6 +6,10 @@ import {
 import * as eyes from 'eyes.it';
 import { proGalleryTestkitFactory } from '../../testkit/protractor';
 
+const ARROW_DIRECTION = {
+  Next: 'next',
+  Back: 'back',
+};
 /**
  * For tests containing interactions.
  * Can be removed if not used.
@@ -17,68 +21,26 @@ describe('proGallery', () => {
     withExamples: true,
   });
   const dataHook = 'storybook-e2e-ProGallery';
-  // const dataHook = 'item-container';
-  // const dataHook = '#pro-gallery-container';
-  // const dataHook = 'storybook-ProGallery';
 
   beforeEach(() => browser.get(storyUrl));
-
-  eyes.fit('should render gallery', async () => {
-    const driver = proGalleryTestkitFactory({ dataHook });
-    const element = await driver.element();
-    const toPrint = await waitForVisibilityOf(
-      element,
-      'Cannot find ProGallery',
-    );
-    // console.log('toPrint', Object.keys(element));
-    // const test = await driver.getInnerHTML();
-    // console.log('toPrint', test);
-    expect(await driver.exists()).toBe(true);
-    // await browser.sleep(10000);
-    // await waitForVisibilityOf(await driver.element(), 'Cannot find ProGallery');
-    // expect((await driver.element()).isDisplayed()).toBe(true);
+  describe('Thumbnail gallery', () => {
+    eyes.it('should render gallery', async () => {
+      const driver = proGalleryTestkitFactory({ dataHook });
+      const element = await driver.element();
+      await waitForVisibilityOf(element, 'Cannot find ProGallery');
+      expect(await driver.exists()).toBe(true);
+    });
+    eyes.it('Arrows should be clickable', async () => {
+      // not working yet.
+      const driver = proGalleryTestkitFactory({ dataHook });
+      const element = await driver.getArrowsShown();
+      await waitForVisibilityOf(element, 'Cannot find ProGallery');
+      await driver.clickOnArrow(ARROW_DIRECTION.Next);
+      await waitForVisibilityOf(
+        await driver.element(),
+        'Cannot find ProGallery',
+      );
+      expect(true).toBe(true);
+    });
   });
-  // it('my test with puppeteer', async () => {
-  //   const browser = await puppeteer.launch({ devtools: true });
-  //   const page = await browser.newPage();
-  //   await page.goto(`localhost:6006/${storyUrl}`);
-  // });
-  // eyes.fit('should click on ', async () => {
-  //   const driver = proGalleryTestkitFactory({ dataHook });
-  //   await driver.getGallerIdShown();
-  //   await driver.clickOnArrow();
-  //   await waitForVisibilityOf(await driver.element(), 'Cannot find ProGallery');
-  //   await driver.clickOnArrow();
-  //   expect(true).toBe(true);
-  // });
-
-  //   expect(true).toBe(true)
-  //   const driver = proGalleryTestkitFactory({ dataHook });
-  //   await waitForVisibilityOf(await driver.element(), 'Cannot find ProGallery');
-  //   // console.log(
-  //   //   await waitForVisibilityOf(
-  //   //     await driver.element(),
-  //   //     'Cannot find ProGallery',
-  //   //   ),
-  //   // );
-  //   // await driver.clickOnArrow();
-  //
-  //   await browser.sleep(10000);
-  //   await driver.clickOnArrow();
-  //
-  //   // expect(await driver.getArrowsShown()).toBe("[data-hook='nav-arrow-next']");
-  //   // while ((await driver.getArrowsShown()) === "[data-hook='nav-arrow-next']") {
-  //   //   await browser.sleep(10000);
-  //   //   await driver.clickOnArrow();
-  //   //   await browser.sleep(800);
-  //   //   await driver.clickOnArrowBack();
-  //   //   await browser.sleep(800);
-  //   // }
-  //
-  //   // expect(await driver.hasArrow('back')).toBe(false);
-  //   // await driver.clickOnArrow('next');
-  //   // // expect(await driver.hasArrow('back')).toBe(true);
-  //   // expect(await driver.hasArrow('next')).toBe(true);
-  //   // await driver.clickOnArrow('next');
-  // });
 });
