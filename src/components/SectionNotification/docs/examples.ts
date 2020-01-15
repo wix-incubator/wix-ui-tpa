cd1b8dce78e0fcea4bffabbdf8faa16b2d379784
Kristina Lyba <kristinal@wix.com>
2020-01-15 11:37:34 +0200

    Use <Error /> in storybook

--- src/components/SectionNotification/docs/examples.ts
+++ src/components/SectionNotification/docs/examples.ts
@@ -4,7 +4,7 @@ export const importExample = `
 
 import { NOTIFICATION_TYPE } from '../types';
 
-const errorIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm.5 17h-1v-9h1v9zm-.5-12c.466 0 .845.378.845.845 0 .466-.379.844-.845.844-.466 0-.845-.378-.845-.844 0-.467.379-.845.845-.845z"/></svg>`;
+const errorIcon = `<Error />`;
 const exampleText = {
   error: `We're having some trouble authorizing and charging your card. Please make sure it's valid or try a different payment method.`,
   notification: `You will be redirected to the payment provider's page.`,
