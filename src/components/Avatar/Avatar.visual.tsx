4a2eb97e30aa700d74a16822e1a170a33cbafaf1
Jonathan Adler <jonathana@wix.com>
2020-02-12 13:23:08 +0200

    build: add delay for Avatar visual test

--- src/components/Avatar/Avatar.visual.tsx
+++ src/components/Avatar/Avatar.visual.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { visualize, story, snap } from 'storybook-snapper';
 import { Avatar } from './';
 import { AvatarSize } from './Avatar';
+import { delay } from '../../test/utils';
 
 const IMG_SRC =
   'https://static.wixstatic.com/media/11062b_c09ec4aab8204a38addeb492a8cb8c3e~mv2_d_1670_2299_s_2.jpg/v1/fill/w_120,h_120/11062b_c09ec4aab8204a38addeb492a8cb8c3e~mv2_d_1670_2299_s_2.jpg';
@@ -17,7 +18,15 @@ visualize('Avatar', () => {
         <Avatar size={AvatarSize[avatarSize]} name={'Peter'} />,
       );
       snap('With image', done => (
-        <Avatar size={size} name={'Eve'} src={IMG_SRC} onLoad={done} />
+        <Avatar
+          size={size}
+          name={'Eve'}
+          src={IMG_SRC}
+          onLoad={async () => {
+            await delay(500);
+            done();
+          }}
+        />
       ));
     });
   });
