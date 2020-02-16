cfd8dbed1db59516693fdbd2d0f9359382cd5403
Jonathan Adler <jonathana@wix.com>
2020-02-16 19:00:14 +0200

    remove id's from svg's + change socialbar example a bit

--- src/components/SocialBar/docs/SocialBarWiringExample.tsx
+++ src/components/SocialBar/docs/SocialBarWiringExample.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { SocialBar } from '../';
 import { Facebook } from '../../Icons/components/Facebook';
+import { Tumblr } from '../../Icons/components/Tumblr';
 import { Instagram } from '../../Icons/components/Instagram';
 import { Linkedin } from '../../Icons/components/Linkedin';
-import { Tumblr } from '../../Icons/components/Tumblr';
 import { Pinterest } from '../../Icons/components/Pinterest';
 import { CopyUrlButton } from '../../CopyUrlButton';
 
@@ -11,10 +11,15 @@ export const SocialBarWiringExample = () => {
   return (
     <SocialBar>
       <SocialBar.Icon
+        tooltip="Linkedin"
+        icon={<Linkedin />}
+        href="https://www.facebook.com/sharer/sharer.php?u=wix.com"
+      />
+
+      <SocialBar.Icon
         tooltip="Facebook"
-        icon={
-          <Facebook href="https://www.facebook.com/sharer/sharer.php?u=wix.com" />
-        }
+        icon={<Facebook />}
+        href="https://www.facebook.com/sharer/sharer.php?u=wix.com"
       />
 
       <SocialBar.Icon
@@ -24,20 +29,14 @@ export const SocialBarWiringExample = () => {
       />
 
       <SocialBar.Icon
-        tooltip="Linkedin"
-        icon={<Linkedin />}
+        tooltip="Pinterest"
+        icon={<Pinterest />}
         href="https://www.facebook.com/sharer/sharer.php?u=wix.com"
       />
 
       <SocialBar.Icon
         tooltip="Tumblr"
         icon={<Tumblr />}
-        href="https://www.facebook.com/sharer/sharer.php?u=wix.com"
-      />
-
-      <SocialBar.Icon
-        tooltip="Pinterest"
-        icon={<Pinterest />}
         href="https://www.facebook.com/sharer/sharer.php?u=wix.com"
       />
 
