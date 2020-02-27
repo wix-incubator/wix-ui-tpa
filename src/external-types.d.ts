99ab0e8584e738a07434ea717da25aeaa234660c
Jonathan Adler <jonathana@wix.com>
2020-02-27 13:01:25 +0200

    dont include wix-ui-core definitions + copy paste definitions needed

--- src/external-types.d.ts
+++ src/external-types.d.ts
@@ -3,3 +3,36 @@ declare module '*.json';
 declare module '*.tsx';
 declare module '*.md';
 declare module '*.scss';
+
+// temporarily declare wix-ui-core's declaration here:
+type StateValue = boolean | number | string;
+
+interface StateMap {
+  [stateName: string]: StateValue;
+}
+
+interface AttributeMap {
+  className?: string;
+  [attributeName: string]: StateValue | undefined;
+}
+
+interface InheritedAttributes {
+  className?: string;
+  [props: string]: any;
+}
+
+type RuntimeStylesheet = {
+  (
+    className: string,
+    states?: StateMap,
+    inheritedAttributes?: InheritedAttributes,
+  ): AttributeMap;
+  $root: string;
+  $namespace: string;
+  $depth: number;
+  $id: string | number;
+  $css?: string;
+
+  $get(localName: string): string | undefined;
+  $cssStates(stateMapping?: StateMap | null): StateMap;
+} & { [localName: string]: string };
