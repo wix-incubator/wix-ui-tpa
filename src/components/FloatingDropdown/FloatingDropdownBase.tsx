dd6893ee0e8966cb37a2044d874709fdb3019250
Andrius Kripaitis <andriusk@wix.com>
2020-04-01 12:36:22 +0300

    Remove unused states and styles;

--- src/components/FloatingDropdown/FloatingDropdownBase.tsx
+++ src/components/FloatingDropdown/FloatingDropdownBase.tsx
@@ -6,9 +6,9 @@ import { TPAComponentProps } from '../../types';
 import { DATA_HOOKS } from './constants';
 import { FloatingDropdownOptionProps } from './FloatingDropdownOption';
 
-import styles from './DropdownBase.st.css';
+import styles from './FloatingDropdownBase.st.css';
 
-interface DropdownBaseProps {
+interface FloatingDropdownBaseProps {
   selectedOption: FloatingDropdownOptionProps;
   placeholder: string;
   disabled: boolean;
@@ -17,31 +17,23 @@ interface DropdownBaseProps {
   'aria-labelledby'?: string;
 }
 
-export const DropdownBase = (props: DropdownBaseProps & TPAComponentProps) => {
+export const FloatingDropdownBase = (props: FloatingDropdownBaseProps & TPAComponentProps) => {
   const {
     selectedOption,
     placeholder,
-    error,
     disabled,
     ['aria-label']: ariaLabel,
     ['aria-labelledby']: ariaLabelledBy,
   } = props;
-  const hasPlaceholder = !selectedOption || !selectedOption.value;
   return (
     <Button
       fullWidth
-      {...styles('root', { error, placeholder: hasPlaceholder }, props)}
+      {...styles('root', { }, props)}
       data-hook={DATA_HOOKS.base}
-      data-dropdown-base-error={error}
       disabled={disabled}
       aria-label={ariaLabel}
       aria-labelledby={ariaLabelledBy}
       aria-haspopup
-      prefixIcon={
-        selectedOption && selectedOption.icon ? (
-          <div className={styles.optionIcon}>{selectedOption.icon}</div>
-        ) : null
-      }
     >
       {placeholder} {selectedOption && selectedOption.value}
     </Button>
