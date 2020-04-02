73bc1ccae9ca510df7a9ae932c06700cf62dfb04
Andrius Kripaitis <andriusk@wix.com>
2020-04-02 13:02:16 +0300

    Make it clickable;

--- src/components/FloatingDropdown/docs/examples.ts
+++ src/components/FloatingDropdown/docs/examples.ts
@@ -3,10 +3,10 @@ export const importExample = `import { FloatingDropdown } from 'wix-ui-tpa/Float
 export const example = `
 <FloatingDropdown
   options={[
-    {id: '1', value: 'Most recent'},
-    {id: '2', value: 'Most viewed'},
-    {id: '3', value: 'Most liked'},
-    {id: '4', value: 'Most comments'}
+    {id: '1', value: 'Most recent', isSelectable: true},
+    {id: '2', value: 'Most viewed', isSelectable: true},
+    {id: '3', value: 'Most liked', isSelectable: true},
+    {id: '4', value: 'Most comments', isSelectable: true}
   ]}
   label="Sort by:"
   placeholder="Most recent"
