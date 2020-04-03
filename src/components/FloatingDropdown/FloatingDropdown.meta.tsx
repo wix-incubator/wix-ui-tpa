a5c3bba340ca746cfb6630e4feac4d23bfd9208e
Andrius Kripaitis <andriusk@wix.com>
2020-04-03 08:43:20 +0300

    Making tests to work;

--- src/components/FloatingDropdown/FloatingDropdown.meta.tsx
+++ src/components/FloatingDropdown/FloatingDropdown.meta.tsx
@@ -9,6 +9,18 @@ FloatingDropdownMetadata.nonReactStrictModeCompliant = true;
 FloatingDropdownMetadata.addSim({
   title: 'render',
   props: {
-    buttonText: 'Click me!',
+    options: [
+      { id: '1', value: 'Most recent', isSelectable: true },
+      { id: '2', value: 'Most viewed', isSelectable: true },
+      { id: '3', value: 'Most liked', isSelectable: true },
+      { id: '4', value: 'Most comments', isSelectable: true },
+      {
+        id: '5',
+        value: 'Most recent posts in the whole galaxy',
+        isSelectable: true,
+      },
+    ],
+    label: 'Sort by:',
+    placeholder: 'Most recent',
   },
 });
