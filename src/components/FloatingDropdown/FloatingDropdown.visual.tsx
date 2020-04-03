cb7d0d3d225e540483b7035acb3356ce5082118b
Andrius Kripaitis <andriusk@wix.com>
2020-04-03 12:54:52 +0300

    Added disabled visual tests;

--- src/components/FloatingDropdown/FloatingDropdown.visual.tsx
+++ src/components/FloatingDropdown/FloatingDropdown.visual.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { VisualTestContainer } from '../../../test/visual/VisualTestContainer';
 import { FloatingDropdown } from './';
 import { getFloatingDropdownTestProps } from './test-props';
+import { FloatingDropdownProps } from './FloatingDropdown';
 
 class FloatingDropdownVisual extends React.Component<any> {
   props = getFloatingDropdownTestProps();
@@ -16,8 +17,10 @@ class FloatingDropdownVisual extends React.Component<any> {
   }
 }
 
-const extendedProps = { forceContentElementVisibility: true };
-const preselectedProps = {};
+const extendedProps: Partial<FloatingDropdownProps> = {
+  forceContentElementVisibility: true,
+};
+const preselectedProps: Partial<FloatingDropdownProps> = { value: '3' };
 
 const tests = [
   {
@@ -38,7 +41,23 @@ const tests = [
     its: [
       {
         it: 'default',
-        props: getFloatingDropdownTestProps(),
+        props: getFloatingDropdownTestProps({ ...preselectedProps }),
+      },
+      {
+        it: 'expanded',
+        props: getFloatingDropdownTestProps({
+          ...preselectedProps,
+          ...extendedProps,
+        }),
+      },
+    ],
+  },
+  {
+    describe: 'disabled',
+    its: [
+      {
+        it: 'default',
+        props: getFloatingDropdownTestProps({ disabled: true }),
       },
     ],
   },
