23f7179c89a75ee02b6df8f053d8e248e790a2b0
Andrius Kripaitis <andriusk@wix.com>
2020-04-03 09:33:25 +0300

    Pass props to tests;

--- src/components/FloatingDropdown/FloatingDropdown.meta.tsx
+++ src/components/FloatingDropdown/FloatingDropdown.meta.tsx
@@ -1,6 +1,8 @@
 import { FloatingDropdown } from '.';
 import Registry from '@ui-autotools/registry';
 
+import { getFloatingDropdownTestProps } from './test-props';
+
 const FloatingDropdownMetadata = Registry.getComponentMetadata(
   FloatingDropdown,
 );
@@ -8,19 +10,5 @@ FloatingDropdownMetadata.nonReactStrictModeCompliant = true;
 
 FloatingDropdownMetadata.addSim({
   title: 'render',
-  props: {
-    options: [
-      { id: '1', value: 'Most recent', isSelectable: true },
-      { id: '2', value: 'Most viewed', isSelectable: true },
-      { id: '3', value: 'Most liked', isSelectable: true },
-      { id: '4', value: 'Most comments', isSelectable: true },
-      {
-        id: '5',
-        value: 'Most recent posts in the whole galaxy',
-        isSelectable: true,
-      },
-    ],
-    label: 'Sort by:',
-    placeholder: 'Most recent',
-  },
+  props: getFloatingDropdownTestProps(),
 });
