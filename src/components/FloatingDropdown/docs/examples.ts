a6dd946d782ec0f88361068394eddb90d97b55d1
Andrius Kripaitis <andriusk@wix.com>
2020-04-03 12:39:33 +0300

    Added preselected example;

--- src/components/FloatingDropdown/docs/examples.ts
+++ src/components/FloatingDropdown/docs/examples.ts
@@ -32,3 +32,16 @@ export const disabled = `
   disabled={true}
 />
 `;
+
+export const preselected = `<FloatingDropdown
+options={[
+  {id: '1', value: 'Most recent', isSelectable: true},
+  {id: '2', value: 'Most viewed', isSelectable: true},
+  {id: '3', value: 'Most liked', isSelectable: true},
+  {id: '4', value: 'Most comments', isSelectable: true},
+  {id: '5', value: 'Most recent posts in the whole galaxy', isSelectable: true},
+]}
+label="Sort by:"
+placeholder="Most recent"
+value={{id: '3', value: 'Most liked', isSelectable: true}}
+/>`;
