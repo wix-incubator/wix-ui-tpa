f6202a765db7cce9e2d621921621ea7dfb276e81
NitzanSirota <nitzansi@wix.com>
2020-05-11 14:44:43 +0300

    Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import style from './Button.st.css';
+import { st, classes } from './Button.st.css';
 import { ButtonNext } from 'wix-ui-core/button-next';
 import { ButtonProps as ButtonNextProps } from 'wix-ui-core/dist/src/components/button-next/button-next';
 import {
@@ -67,7 +67,7 @@ class ButtonComponent extends React.Component<ButtonProps> {
       innerRef,
       upgrade,
       children,
-      ...rest
+      className,
     } = this.props;
     return (
       <TPAComponentsConsumer>
@@ -75,12 +75,12 @@ class ButtonComponent extends React.Component<ButtonProps> {
           <ButtonNext
             {...this._getDataAttributes(mobile)}
             ref={innerRef}
-            {...rest}
             children={children}
-            {...style(
-              'root',
+            data-hook={this.props['data-hook']}
+            className={st(
+              classes.root,
               { priority, size, fullWidth, mobile, upgrade },
-              rest,
+              className,
             )}
           />
         )}
