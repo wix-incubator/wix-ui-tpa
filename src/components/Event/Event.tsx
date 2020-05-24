47d906fbc2b7aefe094fe592d252644f7b9ec827
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2020-05-24 14:33:27 +0300

    selected (clicked)

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -9,26 +9,39 @@ export interface EventProps {
   isMultiday: boolean;
 }
 
+interface State {
+  isClicked: boolean;
+}
+
 interface DefaultProps {
   isTimeShown: boolean;
   isMultiday: boolean;
 }
 
 /** Event */
-export class Event extends React.Component<EventProps> {
+export class Event extends React.Component<EventProps, State> {
   static displayName = 'Event';
   static defaultProps: DefaultProps = {
     isTimeShown: true,
     isMultiday: false,
   };
 
+  state: State = {
+    isClicked: false
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({isClicked: !prevState.isClicked}))
+  }
+
   render() {
+    const { isClicked } = this.state;
     const { time, title, isTimeShown, isMultiday, ...rest } = this.props;
     const timeComponent = isTimeShown ? (
       <Text className={styles.time}>{time}</Text>
     ) : null;
     return (
-      <div {...styles('root', { isMultiday }, rest)}>
+      <div onClick={this.handleClick} {...styles('root', { isMultiday, isClicked }, rest)}>
         {timeComponent}
         <Text className={styles.title}>{title}</Text>
       </div>
