e2473c8da8dc3365420f70efe10c415bec188e35
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2020-05-31 19:20:36 +0300

    Update CalendarCell.visual.tsx

--- src/components/CalendarCell/CalendarCell.visual.tsx
+++ src/components/CalendarCell/CalendarCell.visual.tsx
@@ -5,20 +5,17 @@ import { storiesOf } from '@storybook/react';
 import { VisualTestContainer } from '../../../test/visual/VisualTestContainer';
 import { CalendarCell, Times, CalendarCellProps } from './';
 
-const VisualCalendarCell = (
-  props: CalendarCellProps
-) => (
-  <CalendarCell
-    title={'12'}
-    {...props}
-  />
+const VisualCalendarCell = (props: CalendarCellProps) => (
+  <CalendarCell title={'12'} {...props} />
 );
 
 visualize('ShareButton', () => {
   story('render', () => {
-    snap('title', <VisualCalendarCell title='12' />);
-    snap('current-day', <VisualCalendarCell title='12' timeType={Times.currentDay} />);
-    snap('stretchable', <VisualCalendarCell title='12' stretchable />);
+    snap('title', <VisualCalendarCell title="12" />);
+    snap(
+      'current-day',
+      <VisualCalendarCell title="12" timeType={Times.currentDay} />,
+    );
+    snap('stretchable', <VisualCalendarCell title="12" stretchable />);
   });
 });
-
