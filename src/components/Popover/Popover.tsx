14ff3cd85d3906da5221edabbdd64d0ca3446b91
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2020-06-01 10:53:38 +0300

    right to left

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -3,20 +3,26 @@ import { ReactComponent as Close } from '../../assets/icons/Close.svg';
 import { IconButton } from '../IconButton';
 import styles from './Popover.st.css';
 
-export interface PopoverProps {}
+export interface PopoverProps {
+  rightToLeft?: boolean;
+}
 
-interface DefaultProps {}
+interface DefaultProps {
+  rightToLeft: boolean;
+}
 
 /** Popover */
 export class Popover extends React.Component<PopoverProps> {
   static displayName = 'Popover';
-  static defaultProps: DefaultProps = {};
+  static defaultProps: DefaultProps = {
+    rightToLeft: false
+  };
 
   render() {
-    const { children, ...rest } = this.props;
+    const { children, rightToLeft, ...rest } = this.props;
 
     return (
-      <div {...styles('root', {}, rest)}>
+      <div {...styles('root', { rightToLeft }, rest)}>
         {children}
         <IconButton {...styles('close', {})} as="a" icon={<Close />} />
       </div>
