58b0c5d8aef5fbac044bcb44aafc46c0b3ff09fe
Jonathan Adler <jonathana@wix.com>
2020-06-04 00:33:10 +0300

    run foobar script

---
+++ scripts/testBase.js
@@ -0,0 +1,41 @@
+import { execSync } from 'child_process';
+
+const PULL_REQUEST_PARENT_HASH_INDEX = 2;
+const HEAD_HASH_INDEX = 0;
+
+function getHeadHash() {
+  return execSync('git rev-parse --verify HEAD').toString();
+}
+
+function getParentsHashArray() {
+  const headCommitHash = getHeadHash();
+  return execSync(`git rev-list --parents -n 1 ${headCommitHash}`)
+    .toString()
+    .split(' ');
+}
+
+function getPRHeadHash() {
+  const parentsHashArr = getParentsHashArray();
+  console.log('adler', 'testBase.js:19', 'parentsHashArr', parentsHashArr);
+  const isPullRequest = parentsHashArr.length === 3;
+  const parentHashIndex = isPullRequest
+    ? PULL_REQUEST_PARENT_HASH_INDEX
+    : HEAD_HASH_INDEX;
+  return parentsHashArr[parentHashIndex].trim();
+}
+
+function setApplitoolsId() {
+  let batchId;
+  try {
+    batchId = getPRHeadHash();
+  } catch (e) {
+    console.log('adler', 'testBase.js:31', 'catched', e);
+    batchId = process.env.BUILD_VCS_NUMBER;
+  }
+
+  console.log('adler', 'testBase.js:34', 'batchId is', batchId);
+
+  process.env.APPLITOOLS_BATCH_ID = batchId;
+}
+
+setApplitoolsId();
