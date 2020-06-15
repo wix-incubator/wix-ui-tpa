adcecdbc51cd18e4fbbbf2550817189d54789e1a
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2020-06-15 17:48:23 +0300

    Revert "beautify"
    This reverts commit db6bb303a2539b08db7ad26d4d5fb4f63206f937.

--- src/components/Picker/Picker.spec.tsx
+++ src/components/Picker/Picker.spec.tsx
@@ -10,7 +10,7 @@ import { pickerTestkitFactory as enzymePickerTestkitFactory } from '../../testki
 
 const noop = () => {};
 const defProps = {
-  options: [],
+  options: []
 };
 
 describe('Picker', () => {
--- src/components/Picker/Picker.visual.tsx
+++ src/components/Picker/Picker.visual.tsx
@@ -11,10 +11,8 @@ const options = ['sep, oct, nov'];
 
 visualize('Picker', () => {
   story('render', () => {
-    snap('default', () => <Picker {...defaultProps} />);
-    snap('options', () => <Picker {...defaultProps} options={options} />);
-    snap('options with index', () => (
-      <Picker {...defaultProps} options={options} currentIndex={2} />
-    ));
-  });
+    snap('default', () => <Picker {...defaultProps}/>);
+    snap('options', () => <Picker {...defaultProps} options={options}/>);
+    snap('options with index', () => <Picker {...defaultProps} options={options} currentIndex={2}/>);
+  })
 });
