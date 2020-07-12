3ed6293a639d555da589f6091bf3e2fcb8e7e1ca
Jonathan Adler <jonathana@wix.com>
2020-07-12 18:53:32 +0300

    pass branches if they exist only

--- applitools.config.js
+++ applitools.config.js
@@ -6,11 +6,19 @@ try {
   privateConfig = require('./applitools.private.config.js');
 } catch (e) {}
 
-module.exports = applitoolsConfig({
+const config = {
   config: merge(privateConfig, {
     serverUrl: process.env.APPLITOOLS_SERVER_URL,
     concurrency: 50,
-    branchName: process.env.TPA_BRANCH_NAME,
-    baselineBranchName: process.env.TPA_BASELINE_BRANCH_NAME,
   }),
-});
+};
+
+if (process.env.TPA_BRANCH_NAME) {
+  config.branchName = process.env.TPA_BRANCH_NAME;
+}
+
+if (process.env.TPA_BASELINE_BRANCH_NAME) {
+  config.baselineBranchName = process.env.TPA_BASELINE_BRANCH_NAME;
+}
+
+module.exports = applitoolsConfig(config);
