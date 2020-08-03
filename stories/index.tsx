657d25ef2eebc9aab53fb6ecad9c7220d29f888f
mariak <mariak@wix.com>
2020-08-03 14:41:23 +0300

    radiobuttongroup match master

--- stories/index.tsx
+++ stories/index.tsx
@@ -44,7 +44,6 @@ require('../src/components/NewCard/docs/index.story');
 require('../src/components/SocialBar/docs/index.story');
 require('../src/components/ShareButton/docs/index.story');
 require('../src/components/CopyUrlButton/docs/index.story');
-require('../src/components/SectionNotification/docs/index.story');
 require('../src/components/RadioButton/docs/index.story');
 require('../src/components/Picker/docs/index.story');
 require('../src/components/CalendarCell/docs/index.story');
