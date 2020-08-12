c77eddd81ca1c93488d4a6f8be9440990b1960ea
Jonathan Adler <jonathana@wix.com>
2020-08-12 17:00:50 +0300

    pass naive branches

--- applitools.config.js
+++ applitools.config.js
@@ -6,8 +6,19 @@ try {
   privateConfig = require('./applitools.private.config.js');
 } catch (e) {}
 
+const branches = {};
+
+if (process.env.IS_BUILD_AGENT) {
+  if (process.env.VCS_BRANCH_NAME) {
+    branches.branchName = process.env.VCS_BRANCH_NAME;
+    branches.baselineBranchName = 'master';
+  } else {
+    branches.branchName = 'master';
+  }
+}
+
 module.exports = applitoolsConfig({
   config: merge(privateConfig, {
     concurrency: 50,
-  }),
+  }, branches),
 });
