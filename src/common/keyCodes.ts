af774ef84cc68ac2cb6470cd16c13f9a49ad46c2
Jonathan Adler <jonathana@wix.com>
2020-08-12 17:45:58 +0300

    build: Applitools change + keyCode change

--- src/common/keyCodes.ts
+++ src/common/keyCodes.ts
@@ -1,3 +1,14 @@
+import * as React from 'react';
+
+interface KEYBOARD_KEYS {
+  Enter: string | number;
+  Spacebar: string | number;
+  ArrowUp: string | number;
+  ArrowDown: string | number;
+  ArrowRight: string | number;
+  ArrowLeft: string | number;
+}
+
 export enum KEY_CODES {
   Enter = 13,
   Spacebar = 32,
@@ -7,10 +18,38 @@ export enum KEY_CODES {
   ArrowLeft = 37,
 }
 
-export function isSelectKey(keyCode: number) {
-  switch (keyCode) {
-    case KEY_CODES.Enter:
-    case KEY_CODES.Spacebar:
+export enum KEYS {
+  Enter = 'Enter',
+  Spacebar = ' ',
+  ArrowUp = 'ArrowUp',
+  ArrowUpIE = 'Up',
+  ArrowDown = 'ArrowDown',
+  ArrowDownIE = 'Down',
+  ArrowRight = 'ArrowRight',
+  ArrowRightIE = 'Right',
+  ArrowLeft = 'ArrowLeft',
+  ArrowLeftIE = 'Left',
+}
+
+const getCorrectKeys = (
+  event: React.KeyboardEvent,
+): { keys: KEYBOARD_KEYS; code: string | number } => {
+  // tslint:disable-next-line
+  if (event.which || event.keyCode) {
+    // tslint:disable-next-line
+    return { keys: KEY_CODES, code: event.keyCode || event.which };
+  }
+  if (event.key) {
+    return { keys: KEYS, code: event.key };
+  }
+};
+
+export function isSelectKey(event: React.KeyboardEvent) {
+  const { code, keys } = getCorrectKeys(event);
+
+  switch (code) {
+    case keys.Enter:
+    case keys.Spacebar:
       return true;
     default:
       return false;
