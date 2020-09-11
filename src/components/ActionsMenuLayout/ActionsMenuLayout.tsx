7c06e4249947d29ac26f39364bd1b9fcd7371aab
Artem Bordiian <bordyanartem@gmail.com>
2020-09-11 13:44:51 +0300

    refactor addref; add onfocus

--- src/components/ActionsMenuLayout/ActionsMenuLayout.tsx
+++ src/components/ActionsMenuLayout/ActionsMenuLayout.tsx
@@ -12,6 +12,7 @@ export interface ActionsMenuLayoutProps extends TPAComponentProps {
   focusedIndex?: number;
   /** a11y */
   'aria-labeledby'?: string;
+  'aria-label'?: string;
 }
 
 /** ActionsMenuLayout */
@@ -20,31 +21,25 @@ export class ActionsMenuLayout extends React.Component<ActionsMenuLayoutProps> {
   static Divider = ActionsMenuLayoutDivider;
 
   static displayName = 'ActionsMenuLayout';
+
   state = {
     focusedIdx: 0,
   };
 
-  liRefs = [];
+  liRefs: {
+    [key: number]: {
+      liRef: React.RefObject<HTMLLIElement>;
+    };
+  } = {};
 
   componentDidMount() {
     const { focusedIndex } = this.props;
 
     if (focusedIndex) {
-      this.focusItem(focusedIndex);
+      this._focusItem(focusedIndex);
     } else {
-      this.focusItem(this.state.focusedIdx);
+      this._focusItem(this.state.focusedIdx);
     }
-  }
-
-  focusItem(idx: number) {
-    this.setState(
-      {
-        focusedIdx: idx,
-      },
-      () => {
-        this.liRefs[idx].liRef?.current.focus();
-      },
-    );
   }
 
   getDataAttributes(mobile: boolean) {
@@ -53,43 +48,64 @@ export class ActionsMenuLayout extends React.Component<ActionsMenuLayoutProps> {
     };
   }
 
-  getNextItemIdx(direction: 1 | -1): number {
+  _getNextItemIdx(direction: 1 | -1): number {
     const nextItem = this.state.focusedIdx + direction;
 
     if (direction === 1) {
-      return nextItem === this.liRefs.length ? 0 : nextItem;
+      return nextItem === Object.values(this.liRefs).length ? 0 : nextItem;
     }
 
-    return nextItem < 0 ? this.liRefs.length - 1 : nextItem;
+    return nextItem < 0 ? Object.values(this.liRefs).length - 1 : nextItem;
+  }
+
+  _addRef(ref, index: number) {
+    if (ref) {
+      this.liRefs[index] = ref;
+    }
+  }
+
+  _focusItem(idx: number) {
+    this.setState(
+      {
+        focusedIdx: idx,
+      },
+      () => {
+        Object.values(this.liRefs)[idx].liRef?.current.focus();
+      },
+    );
   }
 
   _onKeyDown = (e: React.KeyboardEvent<HTMLUListElement>) => {
     if (e.key === KEYS.ArrowDown || e.key === KEYS.ArrowDownIE) {
       e.preventDefault(); // remove scroll on space
-      this.focusItem(this.getNextItemIdx(1));
+      this._focusItem(this._getNextItemIdx(1));
     }
 
     if (e.key === KEYS.ArrowUp || e.key === KEYS.ArrowUpIE) {
       e.preventDefault(); // remove scroll on space
-      this.focusItem(this.getNextItemIdx(-1));
+      this._focusItem(this._getNextItemIdx(-1));
     }
   };
 
-  addRef(ref, index: number) {
-    if (ref) {
-      if (
-        this.liRefs.find(_ref => {
-          return _ref?.props.content === ref.props.content;
-        })
-      ) {
-        return;
-      }
+  _onFocus = () => {
+    const { focusedIdx } = this.state;
+    const { focusedIndex } = this.props;
 
-      this.liRefs[
-        index === 0 || this.liRefs[index - 1] ? index : index - 1
-      ] = ref;
+    this._focusItem(
+      typeof focusedIndex !== 'undefined' ? focusedIndex : focusedIdx,
+    );
+  };
+
+  // to get proper index on mouse click
+  _onClick = (e) => {
+    const indexValue = Object.values(this.liRefs).findIndex((i) => {
+      return i.liRef?.current === e.target;
+    });
+
+    if (indexValue >= 0) {
+      this._focusItem(indexValue);
     }
-  }
+  };
 
   render() {
     const { alignment, children, className } = this.props;
@@ -97,20 +113,24 @@ export class ActionsMenuLayout extends React.Component<ActionsMenuLayoutProps> {
       <TPAComponentsConsumer>
         {({ mobile }) => (
           <ul
+            onFocus={this._onFocus}
+            onClick={this._onClick}
             onKeyDown={this._onKeyDown}
             className={st(classes.root, { mobile }, className)}
             {...this.getDataAttributes(mobile)}
             data-hook={this.props['data-hook']}
             role="menu"
             aria-labelledby={this.props['aria-labeledby']}
+            aria-label={this.props['aria-label']}
+            tabIndex={-1}
           >
             {React.Children.map(children, (child: React.ReactElement, index) =>
               child.type === ActionsMenuLayoutItem
                 ? React.cloneElement(child, {
                     className: classes.item,
                     alignment,
-                    ref: _ref => {
-                      this.addRef(_ref, index);
+                    ref: (_ref) => {
+                      this._addRef(_ref, index);
                     },
                   })
                 : child,
