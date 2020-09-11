728570d3e11268a175bed5e084c8c6bcb0dee539
Artem <bordyanartem@gmail.com>
2020-09-11 15:24:40 +0300

    fix(ActionsMenuLayout): A11Y fixes
    Correct aria attributes and correct keyboard interaction

--- src/components/ActionsMenuLayout/ActionsMenuLayout.spec.tsx
+++ src/components/ActionsMenuLayout/ActionsMenuLayout.spec.tsx
@@ -8,6 +8,7 @@ import { actionsMenuLayoutDriverFactory } from './ActionsMenuLayout.driver';
 import { ActionsMenuLayout } from './';
 import { actionsMenuLayoutTestkitFactory } from '../../testkit';
 import { actionsMenuLayoutTestkitFactory as enzymeActionsMenuLayoutTestkitFactory } from '../../testkit/enzyme';
+import { KEYS } from '../../common/keyCodes';
 
 describe('ActionsMenuLayout', () => {
   const createDriver = createUniDriverFactory(actionsMenuLayoutDriverFactory);
@@ -31,6 +32,77 @@ describe('ActionsMenuLayout', () => {
 
     await driver.clickItem('test');
     expect(onClick).toHaveBeenCalled();
+  });
+
+  it('should focus first element if focusedIndex undefined', async function() {
+    const driver = createDriver(
+      <ActionsMenuLayout>
+        <ActionsMenuLayout.Item onClick={() => {}} content="test" />
+      </ActionsMenuLayout>,
+    );
+
+    const item = await driver.item('test').getNative();
+    expect(document.activeElement).toEqual(item);
+  });
+
+  it('should focus 2nd element if focusedIndex=1', async function() {
+    const driver = createDriver(
+      <ActionsMenuLayout focusedIndex={1}>
+        <ActionsMenuLayout.Item onClick={() => {}} content="test1" />
+        <ActionsMenuLayout.Item onClick={() => {}} content="test2" />
+      </ActionsMenuLayout>,
+    );
+
+    const item = await driver.item('test2').getNative();
+    expect(document.activeElement).toEqual(item);
+  });
+
+  it('should navigate by keyboard keys', async function() {
+    const driver = createDriver(
+      <ActionsMenuLayout>
+        <ActionsMenuLayout.Item onClick={() => {}} content="test1" />
+        <ActionsMenuLayout.Item onClick={() => {}} content="test2" />
+        <ActionsMenuLayout.Item onClick={() => {}} content="test3" />
+      </ActionsMenuLayout>,
+    );
+
+    const item1 = await driver.item('test1').getNative();
+    const item2 = await driver.item('test2').getNative();
+    const item3 = await driver.item('test3').getNative();
+    const layout = driver.layout();
+
+    expect(document.activeElement).toEqual(item1);
+
+    await layout.pressKey(KEYS.ArrowDown);
+
+    expect(document.activeElement).toEqual(item2);
+
+    await layout.pressKey(KEYS.ArrowDown);
+
+    expect(document.activeElement).toEqual(item3);
+
+    await layout.pressKey(KEYS.ArrowDown);
+    expect(document.activeElement).toEqual(item1);
+
+    await layout.pressKey(KEYS.ArrowUp);
+    expect(document.activeElement).toEqual(item3);
+  });
+
+  it('should fire onClick on space and enter keys', async function() {
+    const onClick = jest.fn();
+
+    const driver = createDriver(
+      <ActionsMenuLayout>
+        <ActionsMenuLayout.Item onClick={onClick} content="test1" />
+      </ActionsMenuLayout>,
+    );
+
+    const item = driver.item('test1');
+
+    await item.pressKey(KEYS.Spacebar);
+    await item.pressKey(KEYS.Enter);
+
+    expect(onClick).toHaveBeenCalledTimes(2);
   });
 
   it('should use mobile design', async () => {
