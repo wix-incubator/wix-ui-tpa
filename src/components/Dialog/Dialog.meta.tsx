71b23c3f0f7a016cfe7b320f99d0258b6650a326
sivanharel <sivanh@wix.com>
2020-09-21 14:40:58 +0300

    Update the meta.tsx file

--- src/components/Dialog/Dialog.meta.tsx
+++ src/components/Dialog/Dialog.meta.tsx
@@ -1,29 +1,27 @@
 import { Dialog, DialogProps } from '.';
 import Registry from '@ui-autotools/registry';
-import * as React from "react";
+import * as React from 'react';
 
 const DialogMetadata = Registry.getComponentMetadata(Dialog);
 DialogMetadata.nonReactStrictModeCompliant = true;
 
 const DialogContent = () => (
-    <div>
-      <h2 id="dialog4_label" className="dialog_label">
-        End of the Road!
-      </h2>
-      <p id="dialog4_desc" className="dialog_desc">
-        You activated a fake link or button that goes nowhere!
-        The link or button is present for demonstration purposes only.
-      </p>
-    </div>
+  <div>
+    <h2 id="dialog4_label" className="dialog_label">
+      End of the Road!
+    </h2>
+    <p id="dialog4_desc" className="dialog_desc">
+      You activated a fake link or button that goes nowhere! The link or button
+      is present for demonstration purposes only.
+    </p>
+  </div>
 );
 
 DialogMetadata.addSim({
   title: 'render',
   props: {
     isOpen: true,
-    children: (
-        <DialogContent/>
-    ),
+    children: <DialogContent />,
     'aria-label': 'Dialog',
   } as DialogProps,
 });
@@ -32,9 +30,7 @@ DialogMetadata.addSim({
   title: 'Manual Focus',
   props: {
     isOpen: true,
-    children: (
-        <div>This is the content!</div>
-    ),
+    children: <div>This is the content!</div>,
     manualFocus: true,
     closeButtonRef: React.createRef<HTMLButtonElement>(),
   } as DialogProps,
@@ -44,10 +40,8 @@ DialogMetadata.addSim({
   title: 'A11Y',
   props: {
     isOpen: true,
-    children: (
-        <DialogContent/>
-    ),
+    children: <DialogContent />,
     'aria-labelledby': 'dialog_label',
-    'aria-describedby': 'dialog4_desc'
+    'aria-describedby': 'dialog4_desc',
   } as DialogProps,
 });
