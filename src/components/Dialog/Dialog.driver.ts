a132768f14e81f6a5d77b90273d48da209b64115
sivanharel <sivanh@wix.com>
2020-09-21 16:56:44 +0300

    unidriver

--- src/components/Dialog/Dialog.driver.ts
+++ src/components/Dialog/Dialog.driver.ts
@@ -2,14 +2,51 @@ import {
   BaseUniDriver,
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
+import { UniDriver } from 'wix-ui-test-utils/unidriver';
+
+import { modalDriverFactory } from '../Modal/Modal.driver';
+import { iconButtonDriverFactory } from '../IconButton/IconButton.driver';
 
 export interface DialogDriver extends BaseUniDriver {
   isMobile(): Promise<boolean>;
+  isDialogOpen(): Promise<boolean>;
+  childExists(predicate: string): Promise<boolean>;
+  clickOnCloseButton(): Promise<void>;
 }
 
 export const dialogDriverFactory = (base: UniDriver): DialogDriver => {
   return {
     ...baseUniDriverFactory(base),
+
+    /**
+     * Checks whether it is mobile
+     * @returns {Promise<boolean>}
+     */
     isMobile: async () => (await base.attr('data-mobile')) === 'true',
+
+    /**
+     *  Checks whether the dialog is open
+     *  @returns {Promise<boolean>}
+     */
+    isDialogOpen: async () => {
+      const modalDriver = modalDriverFactory(base);
+      return modalDriver.isModalShowed();
+    },
+
+    /**
+     * Checks whether a node with the provided predicate exist
+     * @param {string} predicate - a predicate for the child node
+     * @returns {Promise<boolean>}
+     */
+    childExists: async (predicate: string) => base.$(predicate).exists(),
+
+    /**
+     * Clicks on close button
+     * @return {Promise<void>}
+     */
+    clickOnCloseButton: async () => {
+      const iconButtonDriver = iconButtonDriverFactory(base);
+      await iconButtonDriver.click();
+    },
   };
 };
