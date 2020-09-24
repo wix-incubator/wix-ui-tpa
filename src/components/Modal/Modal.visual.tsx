f81bab066748d116199aaa69ad1150d0dfb4188f
sivanharel <sivanh@wix.com>
2020-09-24 17:05:51 +0300

    Some visual tests & fix the Modal.visual.tsx

--- src/components/Modal/Modal.visual.tsx
+++ src/components/Modal/Modal.visual.tsx
@@ -6,7 +6,9 @@ visualize('Modal', () => {
   story('simple', () => {
     const content = (
       <Modal isOpen>
-        <p>Inner Content.</p>
+        <div style={{ padding: 20, display: 'flex', justifyContent: 'center', width: '100%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -30%)', background: '#fff' }}>
+          <p>Inner Content.</p>
+        </div>
       </Modal>
     );
 
