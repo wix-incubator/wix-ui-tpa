440faf2f9a844220d37a256842285f413dc1c45a
sivanharel <sivanh@wix.com>
2020-09-24 17:07:00 +0300

    Some visual tests & fix the Modal.visual.tsx

--- src/components/Dialog/Dialog.visual.tsx
+++ src/components/Dialog/Dialog.visual.tsx
@@ -7,28 +7,32 @@ import { Button, PRIORITY } from '../Button';
 visualize('Dialog', () => {
   story('simple', () => {
     snap('default props', <Dialog isOpen />);
-    snap('Dialog with some content',(
-        <Dialog isOpen>
-          <div className="content" style={{ textAlign: 'center' }}>
-            <Text typography={TYPOGRAPHY.largeTitle}>Are You Sure?</Text>
-            <div className="text-container" style={{ marginTop: '24px', marginBottom: '36px' }}>
-              <Text typography={TYPOGRAPHY.listText} tagName="div">
-                <div>Do you really want to delete the selected files?</div>
-                <div>Once removed, cannot be undone.</div>
-              </Text>
-            </div>
-            <Button
-                upgrade
-                priority={PRIORITY.basicSecondary}
-                style={{ marginLeft: '10px' }}
-            >
-              SECONDARY
-            </Button>
-            <Button upgrade style={{ marginLeft: '10px' }}>
-              PRIMARY
-            </Button>
+    snap(
+      'Dialog with some content',
+      <Dialog isOpen>
+        <div className="content" style={{ textAlign: 'center' }}>
+          <Text typography={TYPOGRAPHY.largeTitle}>Are You Sure?</Text>
+          <div
+            className="text-container"
+            style={{ marginTop: '24px', marginBottom: '36px' }}
+          >
+            <Text typography={TYPOGRAPHY.listText} tagName="div">
+              <div>Do you really want to delete the selected files?</div>
+              <div>Once removed, cannot be undone.</div>
+            </Text>
           </div>
-        </Dialog>
-    ));
+          <Button
+            upgrade
+            priority={PRIORITY.basicSecondary}
+            style={{ marginLeft: '10px' }}
+          >
+            SECONDARY
+          </Button>
+          <Button upgrade style={{ marginLeft: '10px' }}>
+            PRIMARY
+          </Button>
+        </div>
+      </Dialog>,
+    );
   });
 });
