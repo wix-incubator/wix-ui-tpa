6b5f45eeb9e76aaab2627b50cdc16c5729dad45e
sivanharel <sivanh@wix.com>
2020-10-13 18:05:31 +0300

    remove debugger

--- src/components/AddItem/WSRTheme.js
+++ src/components/AddItem/WSRTheme.js
@@ -5,17 +5,14 @@ import { ReactComponent as PlusL }  from '../../assets/icons/PlusL.svg';
 import { ReactComponent as PlusM }  from '../../assets/icons/PlusM.svg';
 import { ReactComponent as PlusS }  from '../../assets/icons/PlusS.svg';
 
-export const theme = (className) => {
-    debugger;
-    return ({
-        className: className,
-        icons: {
-            AddItemButton: {
-                tiny: () => <PlusS style={{flexShrink: 0}}/>,
-                small: PlusM,
-                medium: PlusL,
-                large: PlusXL,
-            },
+export const theme = (className) => ({
+    className: className,
+    icons: {
+        AddItemButton: {
+            tiny: () => <PlusS style={{flexShrink: 0}}/>,
+            small: PlusM,
+            medium: PlusL,
+            large: PlusXL,
         },
-    });
-};
+    },
+});
