d4edf8ffd233c96cf46141d53cd18db002831124
sivanharel <sivanh@wix.com>
2020-10-15 10:55:48 +0300

    fix the e2e tests

--- src/components/AddItem/AddItem.e2e.ts
+++ src/components/AddItem/AddItem.e2e.ts
@@ -1,10 +1,11 @@
 import * as eyes from 'eyes.it';
-import { browser, Key } from 'protractor';
+import {browser, by, element, Key} from 'protractor';
 import {
   createStoryUrl,
   waitForVisibilityOf,
 } from 'wix-ui-test-utils/protractor';
 import { addItemTestkitFactory } from '../../testkit/protractor';
+import { DATA_HOOKS } from './constants';
 
 describe('AddItem', () => {
   const storyUrl = createStoryUrl({
@@ -13,28 +14,28 @@ describe('AddItem', () => {
     withExamples: true,
   });
   const dataHook = 'storybook-AddItem';
+  let driver;
+  let addItemElement;
 
-  beforeEach(() => browser.get(storyUrl));
+  beforeEach(async () => {
+    browser.get(storyUrl)
+    driver = addItemTestkitFactory({ dataHook });
+    await waitForVisibilityOf(await driver.element(), 'Cannot find AddItem');
+
+    addItemElement = element(by.css(`[data-hook="${DATA_HOOKS.ADD_ITEM}"]`));
+    expect((addItemElement).isDisplayed()).toBe(true);
+  });
 
   eyes.it('should show the correct design on hover', async () => {
-    const driver = addItemTestkitFactory({ dataHook });
-    await waitForVisibilityOf(await driver.element(), 'Cannot find AddItem');
-    expect((await driver.element()).isDisplayed()).toBe(true);
-
     browser
       .actions()
-      .mouseMove(await driver.element())
+      .mouseMove(addItemElement)
       .perform();
   });
 
   eyes.it('should show the correct design on focus', async () => {
-    const driver = addItemTestkitFactory({ dataHook });
-    await waitForVisibilityOf(await driver.element(), 'Cannot find AddItem');
-    expect((await driver.element()).isDisplayed()).toBe(true);
-
     await driver.click();
 
-    const addItemElement = await driver.element();
     addItemElement.sendKeys(Key.TAB);
     addItemElement.sendKeys(Key.SHIFT, Key.TAB);
 
