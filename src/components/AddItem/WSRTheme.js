a6bf57b0674524032a0c408a3cbbdd9c42e60fed
sivanharel <sivanh@wix.com>
2020-10-15 15:05:41 +0300

    remove the old plus icon

--- src/components/AddItem/WSRTheme.js
+++ src/components/AddItem/WSRTheme.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { ReactComponent as PlusXL }  from '../../assets/icons/PlusXL.svg';
 import { ReactComponent as PlusL }  from '../../assets/icons/PlusL.svg';
-import { ReactComponent as PlusM }  from '../../assets/icons/PlusM.svg';
+import { ReactComponent as PlusM }  from '../../assets/icons/plus.svg';
 import { ReactComponent as PlusS }  from '../../assets/icons/PlusS.svg';
 
 export const theme = (className) => ({
