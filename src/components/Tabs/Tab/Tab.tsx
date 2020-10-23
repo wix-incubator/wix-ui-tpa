ddf693e78d8b5ad984493a3f9584401ec3a0c769
olenazh <olenazh@wix.com>
2020-10-23 10:16:43 +0300

    removed descendant aria-hidden

--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -51,8 +51,12 @@ export const Tab = React.forwardRef<HTMLLIElement, TabProps>((props, ref) => {
       aria-current={isActive}
     >
       <span role="link">{title}</span>
-      <div className={classes.focusIndicatorWrapper} tabIndex={-1} aria-hidden={'true'}>
-        <div className={classes.focusIndicator} tabIndex={-1} aria-hidden={'true'}/>
+      <div
+        className={classes.focusIndicatorWrapper}
+        tabIndex={-1}
+        aria-hidden={'true'}
+      >
+        <div className={classes.focusIndicator} tabIndex={-1} />
       </div>
     </li>
   );
