f822477272458b5d6dd08d1cd990870b7edc85ff
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-10 17:04:59 +0200

    test: visual

--- src/components/Image/Image.visual.tsx
+++ src/components/Image/Image.visual.tsx
@@ -7,24 +7,23 @@ const sampleUrl =
 
 visualize('Image', () => {
   story('render', () => {
-    snap('default', <Image src={sampleUrl} />);
-    snap(
-      'with width & height',
-      <Image src={sampleUrl} width={200} height={200} />,
-    );
+    snap('default', done => <Image src={sampleUrl} onLoad={done} />);
+    snap('with width & height', done => (
+      <Image src={sampleUrl} width={200} height={200} onLoad={done} />
+    ));
     snap(
       'with alt',
       <Image alt="Garfield smiles and puts his hand over chest" />,
     );
-    snap(
-      'with mediaItem',
+    snap('with mediaItem', done => (
       <Image
         mediaItem={{
           uri: 'c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg',
           width: 400,
           height: 400,
         }}
-      />,
-    );
+        onLoad={done}
+      />
+    ));
   });
 });
