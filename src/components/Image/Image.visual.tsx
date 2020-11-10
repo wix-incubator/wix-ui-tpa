63fbc3595b9ba6eceb3ce6eda5a77fbd321c144d
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-10 13:42:46 +0200

    test: visual

--- src/components/Image/Image.visual.tsx
+++ src/components/Image/Image.visual.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { visualize, story, snap } from 'storybook-snapper';
+import { snap, story, visualize } from 'storybook-snapper';
 import { Image } from './';
 
 visualize('Image', () => {
-  story('simple example', () => {
-    // snap('default props', <Image />);
-    snap('custom src', <Image src={'something'} />);
+  story('render', () => {
+    snap(
+      'default',
+      <Image src="https://static.wixstatic.com/media/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg/v1/fill/w_420,h_350,al_c,lg_1,q_80/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.webp" />,
+    );
+    snap(
+      'with width & height',
+      <Image
+        src="https://static.wixstatic.com/media/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg/v1/fill/w_420,h_350,al_c,lg_1,q_80/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.webp"
+        width={200}
+        height={200}
+      />,
+    );
+    snap(
+      'with alt',
+      <Image alt="Garfield smiles and puts his hand over chest" />,
+    );
   });
 });
