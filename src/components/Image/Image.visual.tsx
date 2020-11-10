3c6a886d180a025511c4488af8f4f77fde5b5375
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-10 16:46:26 +0200

    test: visual

--- src/components/Image/Image.visual.tsx
+++ src/components/Image/Image.visual.tsx
@@ -2,19 +2,15 @@ import * as React from 'react';
 import { snap, story, visualize } from 'storybook-snapper';
 import { Image } from './';
 
+const sampleUrl =
+  'https://m.media-amazon.com/images/M/MV5BZGMwOGIwZjUtOWQ1OS00YWRjLWJmZGMtN2Y1OWQ3ZDYwYTM3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg';
+
 visualize('Image', () => {
   story('render', () => {
-    snap(
-      'default',
-      <Image src="https://static.wixstatic.com/media/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg/v1/fill/w_420,h_350,al_c,lg_1,q_80/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.webp" />,
-    );
+    snap('default', <Image src={sampleUrl} />);
     snap(
       'with width & height',
-      <Image
-        src="https://static.wixstatic.com/media/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg/v1/fill/w_420,h_350,al_c,lg_1,q_80/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.webp"
-        width={200}
-        height={200}
-      />,
+      <Image src={sampleUrl} width={200} height={200} />,
     );
     snap(
       'with alt',
