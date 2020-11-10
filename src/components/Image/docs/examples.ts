ec9db51fe7029ecbbca2486ae597ff497ecdced1
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-10 16:01:38 +0200

    docs: improve examples

--- src/components/Image/docs/examples.ts
+++ src/components/Image/docs/examples.ts
@@ -2,7 +2,7 @@ export const importExample = `import { Image } from 'wix-ui-tpa/Image';`;
 
 export const regularImageExample = `
 <Image
-	src="https://static.wixstatic.com/media/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg/v1/fill/w_420,h_350,al_c,lg_1,q_80/c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.webp"
+	src="https://m.media-amazon.com/images/M/MV5BZGMwOGIwZjUtOWQ1OS00YWRjLWJmZGMtN2Y1OWQ3ZDYwYTM3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg"
 	width="300"
 	height="250"
 	alt="Garfield smiles and puts his hand over chest"
--- src/components/Image/docs/index.story.tsx
+++ src/components/Image/docs/index.story.tsx
@@ -1,23 +1,23 @@
+import * as ImageWiringExampleCSSRaw from '!raw-loader!./ImageWiringExample.st.css';
+import * as ImageWiringExampleRaw from '!raw-loader!./ImageWiringExample.tsx';
 import * as React from 'react';
-import * as examples from './examples';
 import {
-  header,
   api,
+  code as baseCode,
   divider,
+  header,
   importExample,
   playground,
   tab,
-  code as baseCode,
   tabs,
   testkit,
   title,
 } from 'wix-storybook-utils/Sections';
+import { Image } from '../';
 import { allComponents } from '../../../../stories/utils/allComponents';
 import { settingsPanel } from '../../../../stories/utils/SettingsPanel';
-import * as ImageWiringExampleRaw from '!raw-loader!./ImageWiringExample.tsx';
-import * as ImageWiringExampleCSSRaw from '!raw-loader!./ImageWiringExample.st.css';
+import * as examples from './examples';
 import { ImageWiringExample } from './ImageWiringExample';
-import { Image } from '../';
 
 const code = config =>
   baseCode({ components: allComponents, compact: true, ...config });
@@ -29,10 +29,12 @@ export default {
   componentPath: '../Image.tsx',
   componentProps: () => ({
     'data-hook': 'storybook-Image',
+    src:
+      'https://m.media-amazon.com/images/M/MV5BZGMwOGIwZjUtOWQ1OS00YWRjLWJmZGMtN2Y1OWQ3ZDYwYTM3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg',
+    width: 300,
+    height: 250,
+    alt: 'Garfield smiles and puts his hand over chest',
   }),
-  exampleProps: {
-    //
-  },
   dataHook: 'storybook-Image',
   sections: [
     header(),
@@ -49,9 +51,16 @@ export default {
           title('Examples'),
 
           ...[
-            { title: 'Regular Image', source: examples.regularImageExample },
+            {
+              title: 'Regular Image',
+              description:
+                'This example demonstrates the usage of an external image with a full URL.',
+              source: examples.regularImageExample,
+            },
             {
               title: 'Media Image',
+              description:
+                'This example demonstrates the usage of an item from the media manager.',
               source: examples.mediaImageExample,
             },
           ].map(code),
