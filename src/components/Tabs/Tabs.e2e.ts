c443b4308bb2dcdeb38473c2caf70fcf97e63faa
Jonathan Adler <jonathana@wix.com>
2020-11-18 16:50:40 +0200

    test(Tabs): Move tabs visual tests to e2e.it test

--- src/components/Tabs/Tabs.e2e.ts
+++ src/components/Tabs/Tabs.e2e.ts
@@ -1,4 +1,5 @@
-import { browser } from 'protractor';
+import * as eyes from 'eyes.it';
+import { browser, by, element } from 'protractor';
 import {
   createStoryUrl,
   waitForVisibilityOf,
@@ -13,6 +14,7 @@ describe('Tabs', () => {
     withExamples: true,
   });
   const dataHook = 'storybook-e2e-Tabs';
+  const rootId = 'tabs-test-root';
 
   beforeEach(async () => {
     await browser.get(storyUrl);
@@ -44,5 +46,70 @@ describe('Tabs', () => {
     }
 
     expect(await driver.getNavButtonsShown()).toBe(NavButtonOptions.right);
+  });
+
+  async function scrollToEnd(driver, direction) {
+    const navShown = await driver.getNavButtonsShown();
+
+    if (navShown === NavButtonOptions.both || navShown === direction) {
+      if (direction === NavButtonOptions.right) {
+        await driver.clickRightNavButton();
+      } else {
+        await driver.clickLeftNavButton();
+      }
+
+      await browser.sleep(600);
+
+      return scrollToEnd(driver, direction);
+    }
+  }
+
+  ['ltr', 'rtl'].forEach((direction: 'ltr' | 'rtl') => {
+    const forwardSide = direction === 'ltr' ? 'right' : 'left';
+
+    describe(`Tabs/Scroll - ${direction}`, () => {
+      async function setDirection() {
+        const rootElement = await element(by.id(rootId));
+        await browser.executeScript(
+          `arguments[0].setAttribute('dir','${direction}');`,
+          rootElement,
+        );
+        await browser.sleep(400);
+      }
+
+      eyes.it(`${forwardSide} nav button only`, async () => {
+        const driver = tabsTestkitFactory({ dataHook });
+        await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
+        await setDirection();
+        await browser.sleep(400);
+      });
+
+      eyes.it('both nav buttons', async () => {
+        const driver = tabsTestkitFactory({ dataHook });
+        await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
+
+        await setDirection();
+
+        if (direction === 'ltr') {
+          await driver.clickRightNavButton();
+        } else {
+          await driver.clickLeftNavButton();
+        }
+        await browser.sleep(400);
+      });
+
+      eyes.it(`scroll to ${forwardSide} end`, async () => {
+        const driver = tabsTestkitFactory({ dataHook });
+        await waitForVisibilityOf(await driver.element(), 'Cannot find Tabs');
+
+        await setDirection();
+
+        await scrollToEnd(
+          driver,
+          direction === 'ltr' ? NavButtonOptions.right : NavButtonOptions.left,
+        );
+        await browser.sleep(400);
+      });
+    });
   });
 });
