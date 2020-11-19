af6620ab1cc690385188a9fc16d170b5c2730bf4
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-11-20 01:02:10 +0200

    some adustments

--- scripts/create-component-entries.js
+++ scripts/create-component-entries.js
@@ -9,6 +9,7 @@ const components = {
     "path": "src/components/ActionsMenuLayout"
   },
 }
+const componentsNames = Object.keys(components);
 
  const exists = util.promisify(fs.exists);
  const mkdir = util.promisify(fs.mkdir);
@@ -21,32 +22,48 @@ const components = {
 
 const getPackageJson = ({compName, compPath}) => ({
   name: compName,
-  main: path.join(commonJsPath({compPath}),'index.js'),
-  module: path.join(esmPath({compPath}),'index.js'),
+  main: path.join(commonJsPath({compPath}), 'index.js'),
+  module: path.join(esmPath({compPath}), 'index.js'),
+  typings: path.join(commonJsPath({compPath}), 'index.d.ts'),
   browser: {
     './index.st.css': './index.es.st.css',
-    './index.js': path.join(esmPath({compPath}),'index.js'),
+    './index.js': path.join(esmPath({compPath}), 'index.js'),
   },
 });
 
 console.log('prepare to be amazed');
-const componentsNames = Object.keys(components);
-(async () => {
+
+
+const cleanup = async () => {
+  /* clean existing folders */
   await Promise.all(componentsNames.map(async (compName) => {
     const entryFolderPath = path.join(process.cwd(), compName);
-    const compPath = components[compName].path;
-
-    /* 1. Create a folder per component */
     if(exists(entryFolderPath)) {
       await rmdir(entryFolderPath, {recursive: true});
     }
-    await mkdir(entryFolderPath, {});
-
-    /* 2. Create the package.json template */
-    await writeFile(path.join(entryFolderPath, 'package.json'), JSON.stringify(getPackageJson({compName, compPath}), null, 2));
-
-    /* 3. Create the index.st.css and index.es.st.css files with symlinks to the folder dir */
-    await symlink(path.join(entryFolderPath, 'index.st.css'), path.join(commonJsPath({compPath}), `${compName}.st.css`))
-    await symlink(path.join(entryFolderPath, 'index.es.st.css'), path.join(esmPath({compPath}), `${compName}.es.st.css`))
   }));
-})();
+}
+
+
+const run = async() => {
+  try {
+    await Promise.all(componentsNames.map(async (compName) => {
+      const entryFolderPath = path.join(process.cwd(), compName);
+      const compPath = components[compName].path;
+      
+      /* 1. Create a folder per component */
+      await mkdir(entryFolderPath, {});
+      
+      /* 2. Create the package.json template */
+      await writeFile(path.join(entryFolderPath, 'package.json'), JSON.stringify(getPackageJson({compName, compPath}), null, 2));
+      
+      /* 3. Create the index.st.css and index.es.st.css files with symlinks to the folder dir */
+      await symlink(path.join(commonJsPath({compPath}), `${compName}.st.css`), path.join(entryFolderPath, 'index.st.css'))
+      await symlink(path.join(esmPath({compPath}), `${compName}.es.st.css`), path.join(entryFolderPath, 'index.es.st.css'))
+    }));
+  } catch (e) {
+    console.error(e);
+    cleanup();
+  }
+}
+(async () => {await run()})();
