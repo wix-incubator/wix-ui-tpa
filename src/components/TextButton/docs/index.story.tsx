eeb4661fd217988ef522f3a9f79360d149d4ce84
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-24 16:49:34 +0200

    docs: improve story

--- src/components/TextButton/docs/index.story.tsx
+++ src/components/TextButton/docs/index.story.tsx
@@ -1,25 +1,21 @@
 import * as React from 'react';
-import { TEXT_BUTTON_PRIORITY, TextButton } from '../';
 import {
-  importExample as importSourceExample,
-  StyledExample,
-} from './examples';
-import {
-  header,
   api,
+  code as baseCode,
   description,
   divider,
+  header,
   importExample,
   playground,
   tab,
-  code as baseCode,
   tabs,
   testkit,
   title,
 } from 'wix-storybook-utils/Sections';
+import { TextButton, TEXT_BUTTON_PRIORITY } from '../';
 import { allComponents } from '../../../../stories/utils/allComponents';
 import { settingsApi } from '../../../../stories/utils/SettingsApi';
-import * as Readme from '../README.md';
+import * as examples from './examples';
 
 const code = config =>
   baseCode({ components: allComponents, compact: true, ...config });
@@ -44,25 +40,32 @@ export default {
       tab({
         title: 'Usage',
         sections: [
-          description(Readme),
+          description(
+            '`TextButton` is a component allowing to render a custom text functioning as a button.',
+          ),
 
           importExample({
-            source: importSourceExample,
+            source: examples.importExample,
           }),
 
           divider(),
 
           title('Examples'),
 
-          code(<StyledExample />),
+          ...[
+            {
+              title: 'Basic Usage',
+              source: examples.basic,
+            },
+          ].map(code),
         ],
       }),
 
       ...[
+        { title: 'Playground', sections: [playground()] },
         { title: 'API', sections: [api()] },
         { title: 'Style API', sections: [settingsApi()] },
         { title: 'TestKit', sections: [testkit()] },
-        { title: 'Playground', sections: [playground()] },
       ].map(tab),
     ]),
   ],
