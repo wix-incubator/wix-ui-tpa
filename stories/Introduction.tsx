2ca0d5c04a83a50b9076eab00777c4497cc71833
Jonathan Adler <jonathana@wix.com>
2020-11-24 16:45:18 +0200

    move Typography to contribution + update doc a bit

--- stories/Introduction.tsx
+++ stories/Introduction.tsx
@@ -5,7 +5,6 @@ const README = require('../README.md').default;
 const Usage = require('../docs/USAGE.md').default;
 const RTL = require('../docs/RTL.md').default;
 const Contribution = require('../docs/CONTRIBUTION.md').default;
-const Typography = require('../docs/Typography.md').default;
 
 storiesOf('Getting Started', module)
   .add('Introduction', () => <Markdown source={README} />)
@@ -13,4 +12,3 @@ storiesOf('Getting Started', module)
   .add('Using RTL', () => <Markdown source={RTL} />)
   .add('Contributing', () => <Markdown source={Contribution} />)
   .add('Using RTL', () => <Markdown source={RTL} />)
-  .add('Typography', () => <Markdown source={Typography} />);
