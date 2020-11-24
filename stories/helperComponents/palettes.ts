599fc2f287bcf8cf095c285338f619253ee2ddf4
Jonathan Adler <jonathana@wix.com>
2020-11-24 21:03:57 +0200

    docs(storybook): Use new color palettes

---
+++ stories/helperComponents/palettes.ts
@@ -0,0 +1,38 @@
+export const mainPalette = [
+  ['#FFFFFF', '#F0F0F0', '#C7C7C7', '#757575', '#000000'],
+  ['#FFCAFA', '#F28DE8', '#ED24D9', '#9E1891', '#4F0C48'],
+  ['#E5D4FF', '#C5A1FF', '#9056EE', '#6436AE', '#451D86'],
+  ['#D5F4F4', '#81DFE0', '#46C1BF', '#00848E', '#00474B'],
+  ['#FFEFD8', '#FFD187', '#F7A502', '#D78302', '#835401'],
+];
+
+export const mainPaletteInverted = [
+  ['#000000', '#757575', '#C7C7C7', '#F0F0F0', '#FFFFFF'],
+  ['#4F0C48', '#9E1891', '#ED24D9', '#F28DE8', '#FFCAFA'],
+  ['#451D86', '#6436AE', '#9056EE', '#C5A1FF', '#E5D4FF'],
+  ['#00474B', '#00848E', '#46C1BF', '#81DFE0', '#D5F4F4'],
+  ['#835401', '#D78302', '#F7A502', '#FFD187', '#FFEFD8'],
+];
+
+export const secondaryPalette = [
+  ['#FFFFFF', '#F0F0F0', '#C7C7C7', '#757575', '#000000'],
+  ['#AECBFF', '#5F98FC', '#1A66EB', '#144AA9', '#092351'],
+  ['#BAFFEF', '#7BE2CA', '#55B8A1', '#28816C', '#28584D'],
+  ['#C1F6B0', '#94D381', '#5B9E46', '#3F712F', '#3F712F'],
+  ['#FFF8BB', '#EFE47F', '#E2CE16', '#CDBB10', '#7A7008'],
+];
+
+export const secondaryPaletteInverted = [
+  ['#000000', '#757575', '#C7C7C7', '#F0F0F0', '#FFFFFF'],
+  ['#092351', '#144AA9', '#1A66EB', '#5F98FC', '#AECBFF'],
+  ['#28584D', '#28816C', '#55B8A1', '#7BE2CA', '#BAFFEF'],
+  ['#3F712F', '#3F712F', '#5B9E46', '#94D381', '#C1F6B0'],
+  ['#7A7008', '#CDBB10', '#E2CE16', '#EFE47F', '#FFF8BB'],
+];
+
+export const palettes = [
+  mainPalette,
+  mainPaletteInverted,
+  secondaryPalette,
+  secondaryPaletteInverted,
+];
