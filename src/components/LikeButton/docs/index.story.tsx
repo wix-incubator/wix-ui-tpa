0b44b3d599e3e9c37afbf4a0fff2b9d16bb000a4
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-25 15:18:23 +0200

    docs: improve story

--- src/components/LikeButton/docs/index.story.tsx
+++ src/components/LikeButton/docs/index.story.tsx
@@ -1,23 +1,31 @@
+import * as LikeButtonWiringExampleCSSRaw from '!raw-loader!./LikeButtonWiringExample.st.css';
+import * as LikeButtonWiringExampleRaw from '!raw-loader!./LikeButtonWiringExample.tsx';
 import * as React from 'react';
-import { LikeButton, LabelPlacement } from '../';
 import {
-  header,
   api,
+  code as baseCode,
+  description,
   divider,
+  header,
   importExample,
   playground,
   tab,
-  code as baseCode,
   tabs,
   testkit,
   title,
-  description,
 } from 'wix-storybook-utils/Sections';
+import { LabelPlacement, LikeButton } from '../';
 import { allComponents } from '../../../../stories/utils/allComponents';
 import { settingsApi } from '../../../../stories/utils/SettingsApi';
+import {
+  autoSettingsPanel,
+  settingsPanel,
+} from '../../../../stories/utils/SettingsPanel';
 import * as examples from './examples';
+import { LikeButtonWiringExample } from './LikeButtonWiringExample';
 
-const code = (config) => baseCode({ components: allComponents, compact: true, ...config });
+const code = config =>
+  baseCode({ components: allComponents, compact: true, ...config });
 
 export default {
   category: 'Components',
@@ -41,7 +49,9 @@ export default {
       tab({
         title: 'Usage',
         sections: [
-          description('`LikeButton` is a component allowing to render a like icon with label.'),
+          description(
+            '`LikeButton` is a component allowing to render a like icon with label.',
+          ),
 
           importExample({
             source: examples.importExample,
@@ -61,10 +71,42 @@ export default {
       }),
 
       ...[
-        { title: 'Playground', sections: [playground()] },
+        { title: 'Playground', sections: [playground(), autoSettingsPanel()] },
         { title: 'API', sections: [api()] },
         { title: 'Style API', sections: [settingsApi()] },
         { title: 'TestKit', sections: [testkit()] },
+        {
+          title: 'Settings Panel',
+          sections: [
+            settingsPanel({
+              title: 'LikeButton Panel',
+              example: <LikeButtonWiringExample />,
+              rawSource: LikeButtonWiringExampleRaw,
+              rawCSSSource: LikeButtonWiringExampleCSSRaw,
+              params: {
+                fonts: [
+                  {
+                    label: 'Label Font',
+                    wixParam: 'labelFont',
+                    defaultFont: 'arial',
+                  },
+                ],
+                colors: [
+                  {
+                    label: 'Icon Color',
+                    wixParam: 'iconColor',
+                    defaultColor: 'color-5',
+                  },
+                  {
+                    label: 'Label Color',
+                    wixParam: 'labelColor',
+                    defaultColor: 'color-5',
+                  },
+                ],
+              },
+            }),
+          ],
+        },
       ].map(tab),
     ]),
   ],
