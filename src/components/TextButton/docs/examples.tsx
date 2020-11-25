0172f1da173b2d531c65180ca980e4a06cc7babd
nitayneeman <nitayneeman@users.noreply.github.com>
2020-11-25 13:40:14 +0200

    docs: improve story

--- src/components/TextButton/docs/examples.tsx
+++ src/components/TextButton/docs/examples.tsx
@@ -1,12 +1,3 @@
-import * as React from 'react';
-import * as ExtendedRawSource from '!raw-loader!./TextButtonWithStylesExample.tsx';
-import * as ExtendedMobileRawSource from '!raw-loader!./TextButtonWithStylesMobileExample.tsx';
-import * as ExtendedCSSRaw from '!raw-loader!./TextButtonWithStylesExample.st.css';
-import { MockSettings } from '../../../../stories/helperComponents/MockSettings';
-import CodeExample from 'wix-storybook-utils/CodeExample';
-import { TextButtonWithStylesExample } from './TextButtonWithStylesExample';
-import { TextButtonWithStylesMobileExample } from './TextButtonWithStylesMobileExample';
-
 export const importExample = `import { TextButton } from 'wix-ui-tpa/TextButton';`;
 
 export const basic = `<TextButton>Text Button</TextButton>`;
