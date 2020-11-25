b929bfab4b24aa50062e0af7a30321b9238acfb3
sivanharel <sivanh@wix.com>
2020-11-25 13:29:29 +0200

    Improve story page: Button

--- src/components/Button/docs/index.story.tsx
+++ src/components/Button/docs/index.story.tsx
@@ -56,7 +56,7 @@ export default {
         sections: [
           description(
             '**ATTENTION: The current API for this component will be deprecated on the next major version.<br>' +
-              'Please use the new API using the `upgrade` prop.<br>**'
+              'Please use the new API using the `upgrade` prop.<br>**',
           ),
           description('A simple Button component.'),
 
