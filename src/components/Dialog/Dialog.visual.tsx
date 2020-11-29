346d14890c65bd5757834000600b2a0d8541f5a9
Jonathan Adler <jonathana@wix.com>
2020-11-29 11:57:03 +0200

    add wired test + add todo

--- src/components/Dialog/Dialog.visual.tsx
+++ src/components/Dialog/Dialog.visual.tsx
@@ -38,11 +38,16 @@ visualize('Dialog', () => {
     snap('Dialog with some content', <DialogWithContent />);
   });
 
-  // story('Wired to palette', () => {
-  //   snap('Dialog with some content', () => {
-  //     // setDarkPalette();
-  //
-  //     return <DialogWithContent wired />;
-  //   });
-  // });
+  story('Wired to palette', () => {
+    snap('Dialog with some content', () => {
+      return <DialogWithContent wired />;
+    });
+
+    // TODO uncomment when we'll support dark theme tests
+    // snap('with dark theme', () => {
+    //   setDarkPalette();
+    //
+    //   return <DialogWithContent wired />;
+    // });
+  });
 });
