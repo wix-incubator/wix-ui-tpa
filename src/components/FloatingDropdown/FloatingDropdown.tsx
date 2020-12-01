dad61bc920ec1f8bd841f3ba221c6d81dfdb2a1b
lior marzouk <lior.marzouk@gmail.com>
2020-12-01 17:23:35 +0200

    <FloatingDropdown/>: moved back all the defaults to FloatingDropdownBase

--- src/components/FloatingDropdown/FloatingDropdown.tsx
+++ src/components/FloatingDropdown/FloatingDropdown.tsx
@@ -85,7 +85,7 @@ export class FloatingDropdown extends React.Component<FloatingDropdownProps> {
         ? OptionFactory.createDivider({ className: classes.optionDivider })
         : OptionFactory.create({
             ...option,
-            render: () => <DropdownOption {...option} />,
+            render: () => <DropdownOption className={classes.option} {...option} />,
           }),
     );
 
