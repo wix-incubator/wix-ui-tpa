c32fbba13be2647fd7299b3a09bdda37679eb6ad
sivanharel <sivanh@wix.com>
2020-12-02 17:28:04 +0200

    trying to add async visual tests

--- src/components/DatePicker/DatePicker.driver.ts
+++ src/components/DatePicker/DatePicker.driver.ts
@@ -3,15 +3,31 @@ import {
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
 import { UniDriver } from 'wix-ui-test-utils/unidriver';
-// import { DATA_HOOKS } from './constants';
+import { DATA_HOOKS } from './constants';
 
 // @ts-ignore
 // import { datePickerDriverFactory as WSRDatePickerDriverFactory } from 'wix-style-react/dist/src/DatePicker/DatePicker.driver';
 
-export interface DatePickerDriver extends BaseUniDriver {}
+export interface DatePickerDriver extends BaseUniDriver {
+  getTextFieldElement(): Promise<HTMLElement>;
+  openCalendarWindow(): Promise<void>;
+}
 
 export const datePickerDriverFactory = (base: UniDriver): DatePickerDriver => {
+  const dateInputElement = base.$(`[data-hook=${DATA_HOOKS.DATE_INPUT}]`);
+
   return {
     ...baseUniDriverFactory(base),
+
+    getTextFieldElement: () => {
+      return dateInputElement.getNative();
+    },
+    /**
+     * open the calender window
+     * @return {Promise<void>}
+     */
+    openCalendarWindow: async () => {
+      await base.$(`[data-hook=${DATA_HOOKS.DATE_INPUT}]`).click();
+    },
   };
 };
--- src/components/DatePicker/DatePicker.visual.tsx
+++ src/components/DatePicker/DatePicker.visual.tsx
@@ -1,13 +1,49 @@
 import * as React from 'react';
 import { visualize, story, snap } from 'storybook-snapper';
-import { DatePicker } from './';
+import { uniTestkitFactoryCreator } from 'wix-ui-test-utils/vanilla';
+import { DatePicker, DatePickerProps } from './';
+import { datePickerDriverFactory } from './DatePicker.driver';
+
+const createDriver = uniTestkitFactoryCreator(datePickerDriverFactory);
+const dataHook = 'storybook-datepicker';
+
+interface DatePickerVisualProps {
+  datePickerProps: DatePickerProps;
+  onDone(): void;
+}
+
+class DatePickerVisualComponent extends React.Component<DatePickerVisualProps> {
+  static defaultProps = {
+    datePickerProps: {},
+    onDone: () => {},
+  };
+
+  async componentDidMount() {
+    const { onDone } = this.props;
+    const driver = createDriver({ wrapper: document.body, dataHook });
+
+    await driver.openCalendarWindow();
+
+    onDone();
+  }
+  render() {
+    const { datePickerProps } = this.props;
+
+    return <DatePicker data-hook={dataHook} {...datePickerProps} />;
+  }
+}
 
 visualize('DatePicker', () => {
   story('simple example', () => {
-    snap(
-      'default props',
-      <DatePicker value={new Date()} onChange={() => {}} />,
-    );
+    snap('default props', done => (
+      <DatePickerVisualComponent
+        datePickerProps={{
+          value: new Date(),
+          onChange: () => {},
+        }}
+        onDone={done}
+      />
+    ));
     snap(
       'Without a selected date',
       <DatePicker
