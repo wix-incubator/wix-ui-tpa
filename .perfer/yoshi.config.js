3aae6df5655d1109d3cb368f7e1683bbd3474ef2
nitayneeman <nitayneeman@users.noreply.github.com>
2020-12-13 15:09:05 +0200

    build: fix broken ci

--- .perfer/yoshi.config.js
+++ .perfer/yoshi.config.js
@@ -5,16 +5,20 @@ const resolvePath = (...args) => path.resolve(ROOT_DIR, ...args);
 
 const componentsToBundle = require(resolvePath('../.wuf/components.json'));
 
-const components = Object.keys(componentsToBundle).reduce(
-  (accu, component) => ({
-    ...accu,
-    [component]: `${componentsToBundle[component].path.replace(
-      'src/',
-      '../../dist/es/src/'
-    )}/index`,
-  }),
-  {}
-);
+// TODO: fix the import issues of the excluded components
+const excludedComponents = ['AddItem'];
+const components = Object.keys(componentsToBundle)
+  .filter((component) => !excludedComponents.includes(component))
+  .reduce(
+    (accu, component) => ({
+      ...accu,
+      [component]: `${componentsToBundle[component].path.replace(
+        'src/',
+        '../../dist/es/src/'
+      )}/index`,
+    }),
+    {}
+  );
 
 module.exports = {
   entry: {
