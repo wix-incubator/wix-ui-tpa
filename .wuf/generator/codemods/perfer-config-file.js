a5bfe44eb7e02fa484418c1eb422f89510bd8b6d
Nitay Neeman <nitayneeman@users.noreply.github.com>
2020-12-14 10:54:36 +0200

    build(Perfer) - add codemod for generator (#525)
    * build: add codemod
    
    * refactor: semantic changes

---
+++ .wuf/generator/codemods/perfer-config-file.js
@@ -0,0 +1,29 @@
+module.exports = (file, api, options) => {
+  const { jscodeshift } = api;
+  const { ComponentName } = options;
+
+  const root = jscodeshift(file.source);
+
+  // The initial KB
+  const thresholdSize = 5;
+
+  // Finds the `files` array
+  const array = root.find(jscodeshift.VariableDeclaration, {
+    declarations: [{ id: { name: 'files' } }],
+  });
+
+  // Creates new entry to be inserted
+  const entry = jscodeshift.arrayExpression([
+    jscodeshift.literal(`${ComponentName}.bundle.min.js`),
+    jscodeshift.numericLiteral(thresholdSize),
+  ]);
+
+  // Pushes the entry into the array
+  array.get(0).node.declarations[0].init.elements.push(entry);
+
+  // Formatting
+  return root.toSource({
+    quote: 'single',
+    trailingComma: true,
+  });
+};
