294bdca9dee5a838f91057e0d4eb01b26650105b
sivanharel <sivanh@wix.com>
2020-12-14 11:14:23 +0200

    use the wsr uni driver

--- src/components/DatePicker/DatePicker.driver.ts
+++ src/components/DatePicker/DatePicker.driver.ts
@@ -3,12 +3,22 @@ import {
   baseUniDriverFactory,
 } from 'wix-ui-test-utils/base-driver';
 import { UniDriver } from 'wix-ui-test-utils/unidriver';
+import { DATA_HOOKS } from './constants';
+
+// @ts-ignore
+import { calendarDriverFactory as WSRCalendarDriverFactory } from 'wix-style-react/dist/src/Calendar/Calendar.driver';
 
 export interface DatePickerDriver extends BaseUniDriver {
   // isMobile(): Promise<boolean>;
 }
 
-export const datePickerDriverFactory = (base: UniDriver): DatePickerDriver => {
+export const datePickerDriverFactory = (
+    base: UniDriver,
+    body: UniDriver,
+): DatePickerDriver => {
+  const wsrCalendarNode = base.$(`[data-hook="${DATA_HOOKS.WSR_CALENDAR}"]`);
+  const WSRCalendarDriver = WSRCalendarDriverFactory(wsrCalendarNode, body);
+
   return {
     ...baseUniDriverFactory(base),
     // isMobile: async () => (await base.attr('data-mobile')) === 'true',
