79887b363dcef69fdf5e74b917cabc6b12191f83
sivanharel <sivanh@wix.com>
2020-12-14 09:53:47 +0200

    Add visual tests

--- src/components/DatePicker/DatePicker.visual.tsx
+++ src/components/DatePicker/DatePicker.visual.tsx
@@ -5,11 +5,53 @@ import { DatePicker } from './';
 
 visualize('DatePicker', () => {
   story('simple', () => {
-    snap('default props', <DatePicker value={new Date('2019/12/15')} onChange={() => {}} />);
-    snap('FilterDate - Prior for today dates (includes today)', <DatePicker value={new Date('2019/12/15')} filterDate={date => date <= new Date('2019/12/15')} onChange={() => {}} />);
-    snap('FilterDate - feature dates only (includes today)', <DatePicker value={new Date('2019/12/15')} filterDate={date => date >= new Date('2019/12/15')} onChange={() => {}} />);
-    snap('Months dropdown', <DatePicker value={new Date('2019/12/15')} showMonthDropdown onChange={() => {}} />);
-    snap('Years dropdown', <DatePicker value={new Date('2019/12/15')} showYearDropdown onChange={() => {}} />);
-    snap('Months and years dropdown', <DatePicker value={new Date('2019/12/15')} showMonthDropdown showYearDropdown onChange={() => {}} />);
+    snap(
+      'default props',
+      <DatePicker
+          value={new Date('2019/12/15')}
+          onChange={() => {}}
+      />,
+    );
+    snap(
+      'FilterDate - Prior for today dates (includes today)',
+      <DatePicker
+        value={new Date('2019/12/15')}
+        filterDate={date => date <= new Date('2019/12/15')}
+        onChange={() => {}}
+      />,
+    );
+    snap(
+      'FilterDate - feature dates only (includes today)',
+      <DatePicker
+        value={new Date('2019/12/15')}
+        filterDate={date => date >= new Date('2019/12/15')}
+        onChange={() => {}}
+      />,
+    );
+    snap(
+      'Months dropdown',
+      <DatePicker
+        value={new Date('2019/12/15')}
+        showMonthDropdown
+        onChange={() => {}}
+      />,
+    );
+    snap(
+      'Years dropdown',
+      <DatePicker
+        value={new Date('2019/12/15')}
+        showYearDropdown
+        onChange={() => {}}
+      />,
+    );
+    snap(
+      'Months and years dropdown',
+      <DatePicker
+        value={new Date('2019/12/15')}
+        showMonthDropdown
+        showYearDropdown
+        onChange={() => {}}
+      />,
+    );
   });
 });
