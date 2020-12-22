2eb09078873af6f8164f0123626139b62c7a7bb7
urig-wix <73787639+urig-wix@users.noreply.github.com>
2020-12-22 13:36:22 +0200

    feat(RadioButton): Added a focus-ring
    Using the new `withFocusRing` prop

--- src/components/RadioButton/dataHooks.ts
+++ src/components/RadioButton/dataHooks.ts
@@ -1,10 +1,12 @@
 export enum RADIOBUTTON_DATA_KEYS {
   Disabled = 'data-disabled',
   Checked = 'data-checked',
+  Focused = 'data-focused',
 }
 
 export enum RADIOBUTTON_DATA_HOOKS {
   RadioButtonWrapper = 'radiobutton-wrapper',
   IconWrapper = 'icon-wrapper',
   LabelWrapper = 'label-wrapper',
+  coreRadioButton = 'core-radio-button',
 }
