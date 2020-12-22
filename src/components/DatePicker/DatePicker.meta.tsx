ab4e2c0968952540f92afb020a80697ea7d50082
sivanharel <sivanh@wix.com>
2020-12-22 12:01:49 +0200

    Add DatePickerMetadata.nonA11yCompliant = true; until we will fix the issue that the colors that are used in the tests are the wsr colors.

--- src/components/DatePicker/DatePicker.meta.tsx
+++ src/components/DatePicker/DatePicker.meta.tsx
@@ -4,6 +4,9 @@ import Registry from '@ui-autotools/registry';
 const DatePickerMetadata = Registry.getComponentMetadata(DatePicker);
 DatePickerMetadata.nonReactStrictModeCompliant = true;
 
+// todo: Remove it after fixing the issue that the colors that are used in the tests are the wsr colors.
+DatePickerMetadata.nonA11yCompliant = true;
+
 DatePickerMetadata.addSim({
   title: 'render',
   props: {
