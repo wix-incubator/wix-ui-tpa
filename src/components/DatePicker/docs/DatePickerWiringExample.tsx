ae95079cbc3cc1c4b9985a5a520976c1ec9a482f
sivanharel <sivanh@wix.com>
2020-12-23 11:45:18 +0200

    some cr changes

--- src/components/DatePicker/docs/DatePickerWiringExample.tsx
+++ src/components/DatePicker/docs/DatePickerWiringExample.tsx
@@ -2,21 +2,20 @@ import * as React from 'react';
 import { DatePicker } from '../';
 import { classes } from './DatePickerWiringExample.st.css';
 
-export const DatePickerWiringExample =
-    class DatePickerExample extends React.Component {
-      state = {
-        value: new Date(),
-      };
-      _onChange = (date) => this.setState({ value: date });
+export const DatePickerWiringExample = class DatePickerExample extends React.Component {
+  state = {
+    value: new Date(),
+  };
+  _onChange = date => this.setState({ value: date });
 
-      render() {
-        const { value } = this.state;
-        return (
-            <DatePicker
-                className={classes.root}
-                value={value}
-                onChange={this._onChange}
-            />
-        );
-      }
+  render() {
+    const { value } = this.state;
+    return (
+      <DatePicker
+        className={classes.root}
+        value={value}
+        onChange={this._onChange}
+      />
+    );
+  }
 };
--- src/components/DatePickerInput/docs/DatePickerInputWiringExample.tsx
+++ src/components/DatePickerInput/docs/DatePickerInputWiringExample.tsx
@@ -2,22 +2,21 @@ import * as React from 'react';
 import { DatePickerInput } from '../';
 import { classes } from './DatePickerInputWiringExample.st.css';
 
-export const DatePickerInputWiringExample =
-    class DatePickerInputExample extends React.Component {
-      state = {
-        value: new Date(),
-      };
+export const DatePickerInputWiringExample = class DatePickerInputExample extends React.Component {
+  state = {
+    value: new Date(),
+  };
 
-      _onChange = (date) => this.setState({value: date});
+  _onChange = date => this.setState({ value: date });
 
-      render() {
-        const {value} = this.state;
-        return (
-            <DatePickerInput
-                className={classes.root}
-                value={value}
-                onChange={this._onChange}
-            />
-        );
-      }
+  render() {
+    const { value } = this.state;
+    return (
+      <DatePickerInput
+        className={classes.root}
+        value={value}
+        onChange={this._onChange}
+      />
+    );
+  }
 };
