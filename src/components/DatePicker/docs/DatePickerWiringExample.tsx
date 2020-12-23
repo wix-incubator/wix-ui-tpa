b55dea5e7c7025091ceca5d4e7c170fea076dea4
sivanharel <sivanh@wix.com>
2020-12-23 11:41:22 +0200

    some cr changes

--- src/components/DatePicker/docs/DatePickerWiringExample.tsx
+++ src/components/DatePicker/docs/DatePickerWiringExample.tsx
@@ -2,12 +2,21 @@ import * as React from 'react';
 import { DatePicker } from '../';
 import { classes } from './DatePickerWiringExample.st.css';
 
-export const DatePickerWiringExample = () => {
-  return (
-    <DatePicker
-      className={classes.root}
-      value={new Date()}
-      onChange={() => {}}
-    />
-  );
+export const DatePickerWiringExample =
+    class DatePickerExample extends React.Component {
+      state = {
+        value: new Date(),
+      };
+      _onChange = (date) => this.setState({ value: date });
+
+      render() {
+        const { value } = this.state;
+        return (
+            <DatePicker
+                className={classes.root}
+                value={value}
+                onChange={this._onChange}
+            />
+        );
+      }
 };
--- src/components/DatePickerInput/docs/DatePickerInputWiringExample.tsx
+++ src/components/DatePickerInput/docs/DatePickerInputWiringExample.tsx
@@ -2,12 +2,22 @@ import * as React from 'react';
 import { DatePickerInput } from '../';
 import { classes } from './DatePickerInputWiringExample.st.css';
 
-export const DatePickerInputWiringExample = () => {
-  return (
-    <DatePickerInput
-      className={classes.root}
-      value={new Date()}
-      onChange={() => {}}
-    />
-  );
+export const DatePickerInputWiringExample =
+    class DatePickerInputExample extends React.Component {
+      state = {
+        value: new Date(),
+      };
+
+      _onChange = (date) => this.setState({value: date});
+
+      render() {
+        const {value} = this.state;
+        return (
+            <DatePickerInput
+                className={classes.root}
+                value={value}
+                onChange={this._onChange}
+            />
+        );
+      }
 };
