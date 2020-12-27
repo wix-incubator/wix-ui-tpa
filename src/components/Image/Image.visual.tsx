be5c726c2fbbdb17e18aace1a40a7570c1a3e9ac
Nitay Neeman <nitayneeman@users.noreply.github.com>
2020-12-27 12:12:50 +0200

    Image - make `src` to support absolute and relative (#551)
    * test: use each
    
    * test: improve visuals
    
    * feat: support relative uri
    
    * feat: support relative uri
    
    * docs: improve examples
    
    * refactor: remove redundant
    
    * docs: change to wip

--- src/components/Image/Image.visual.tsx
+++ src/components/Image/Image.visual.tsx
@@ -2,8 +2,19 @@ import * as React from 'react';
 import { snap, story, visualize } from 'storybook-snapper';
 import { Image, ImageProps } from './';
 
-const sampleUrl =
-  'https://m.media-amazon.com/images/M/MV5BZGMwOGIwZjUtOWQ1OS00YWRjLWJmZGMtN2Y1OWQ3ZDYwYTM3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg';
+const stories: { name: string; src: string; invalidSrc: string }[] = [
+  {
+    name: 'Absolute URL',
+    src:
+      'https://m.media-amazon.com/images/M/MV5BZGMwOGIwZjUtOWQ1OS00YWRjLWJmZGMtN2Y1OWQ3ZDYwYTM3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg',
+    invalidSrc: 'https://invalid.something/resource.jpg',
+  },
+  {
+    name: 'Relative URI',
+    src: 'c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg',
+    invalidSrc: 'invalid-resource.jpg',
+  },
+];
 
 type ImageWrapperProps = ImageProps & { onDone(): void };
 
@@ -28,27 +39,15 @@ class ImageWrapper extends React.Component<ImageWrapperProps> {
 }
 
 visualize('Image', () => {
-  story('render', () => {
-    snap('default', done => <Image src={sampleUrl} onLoad={done} />);
-    snap('with width & height', done => (
-      <Image src={sampleUrl} width={200} height={200} onLoad={done} />
-    ));
-    snap(
-      'with alt',
-      <Image alt="Garfield smiles and puts his hand over chest" />,
-    );
-    snap('with mediaItem', done => (
-      <Image
-        mediaItem={{
-          uri: 'c5f754_dd75514d14fa4057b4f4a6cc8ce7add3~mv2.jpg',
-          width: 400,
-          height: 400,
-        }}
-        onLoad={done}
-      />
-    ));
-    snap('with onError', done => (
-      <ImageWrapper src="invalid-resource" onDone={done} />
-    ));
+  stories.forEach(({ name, src, invalidSrc }) => {
+    story(name, () => {
+      snap('default', (done) => <Image src={src} onLoad={done} />);
+      snap('with width & height', (done) => (
+        <Image src={src} width={200} height={200} onLoad={done} />
+      ));
+      snap('with onError', (done) => (
+        <ImageWrapper src={invalidSrc} onDone={done} />
+      ));
+    });
   });
 });
