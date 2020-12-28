f3f12959ea087e779343826aa8e15514ccc2d396
sivanharel <sivanh@wix.com>
2020-12-28 10:54:47 +0200

    Change the width to 100%

--- src/components/DatePicker/DatePicker.visual.tsx
+++ src/components/DatePicker/DatePicker.visual.tsx
@@ -7,48 +7,71 @@ visualize('DatePicker', () => {
   story('simple', () => {
     snap(
       'default props',
-      <DatePicker value={new Date('2019/12/15')} onChange={() => {}} />,
+      <div style={{ width: '280px' }}>
+          <DatePicker value={new Date('2019/12/15')} onChange={() => {}} />
+      </div>,
+    );
+    snap(
+        'responsiveness',
+        <div style={{ width: '600px' }}>
+          <DatePicker
+              value={new Date('2019/12/15')}
+              showMonthDropdown
+              showYearDropdown
+              onChange={() => {}}
+          />
+        </div>,
     );
     snap(
       'FilterDate - Prior for today dates (includes today)',
-      <DatePicker
-        value={new Date('2019/12/15')}
-        filterDate={date => date <= new Date('2019/12/15')}
-        onChange={() => {}}
-      />,
+      <div style={{ width: '280px' }}>
+        <DatePicker
+          value={new Date('2019/12/15')}
+          filterDate={date => date <= new Date('2019/12/15')}
+          onChange={() => {}}
+        />
+      </div>,
     );
     snap(
       'FilterDate - future dates only (includes today)',
-      <DatePicker
-        value={new Date('2019/12/15')}
-        filterDate={date => date >= new Date('2019/12/15')}
-        onChange={() => {}}
-      />,
+      <div style={{ width: '280px' }}>
+          <DatePicker
+            value={new Date('2019/12/15')}
+            filterDate={date => date >= new Date('2019/12/15')}
+            onChange={() => {}}
+          />
+      </div>,
     );
     snap(
       'Months dropdown',
-      <DatePicker
-        value={new Date('2019/12/15')}
-        showMonthDropdown
-        onChange={() => {}}
-      />,
+      <div style={{ width: '280px' }}>
+          <DatePicker
+            value={new Date('2019/12/15')}
+            showMonthDropdown
+            onChange={() => {}}
+          />
+      </div>,
     );
     snap(
       'Years dropdown',
-      <DatePicker
-        value={new Date('2019/12/15')}
-        showYearDropdown
-        onChange={() => {}}
-      />,
+      <div style={{ width: '280px' }}>
+          <DatePicker
+            value={new Date('2019/12/15')}
+            showYearDropdown
+            onChange={() => {}}
+          />,
+      </div>
     );
     snap(
       'Months and years dropdown',
-      <DatePicker
-        value={new Date('2019/12/15')}
-        showMonthDropdown
-        showYearDropdown
-        onChange={() => {}}
-      />,
+      <div style={{ width: '280px' }}>
+        <DatePicker
+          value={new Date('2019/12/15')}
+          showMonthDropdown
+          showYearDropdown
+          onChange={() => {}}
+        />
+      </div>,
     );
   });
 });
