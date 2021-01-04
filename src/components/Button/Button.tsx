a0b896f146f7700242c0147a535e77113e9676a2
Jonathan Adler <jonathana@wix.com>
2021-01-04 18:22:21 +0200

    Revert "revert: "fix(Button): Fix prefix and suffix alignments""
    This reverts commit 8a18c7b6edb547d7849db63cde2fd8bf98e68728.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classnames from 'classnames';
 import { st, classes } from './Button.st.css';
 import { ButtonNext } from 'wix-ui-core/button-next';
 import { ButtonProps as ButtonNextProps } from 'wix-ui-core/dist/src/components/button-next/button-next';
@@ -57,6 +58,27 @@ class ButtonComponent extends React.Component<ButtonProps> {
     };
   }
 
+  _wrapAffix = (icon, type) => {
+    return (
+      <span className={classnames(classes[`${type}-icon`], classes.affixIcon)}>
+        {icon}
+      </span>
+    );
+  };
+
+  _getAffixes = () => {
+    const { prefixIcon, suffixIcon } = this.props;
+
+    return {
+      prefixIcon: prefixIcon
+        ? this._wrapAffix(prefixIcon, 'prefix')
+        : undefined,
+      suffixIcon: suffixIcon
+        ? this._wrapAffix(suffixIcon, 'suffix')
+        : undefined,
+    };
+  };
+
   render() {
     const {
       priority,
@@ -68,6 +90,7 @@ class ButtonComponent extends React.Component<ButtonProps> {
       className,
       ...rest
     } = this.props;
+
     return (
       <TPAComponentsConsumer>
         {({ mobile }) => (
@@ -88,6 +111,7 @@ class ButtonComponent extends React.Component<ButtonProps> {
               className,
             )}
             {...rest}
+            {...this._getAffixes()}
           />
         )}
       </TPAComponentsConsumer>
--- src/components/Button/docs/ButtonExtendedWithStyleParamsExample.tsx
+++ src/components/Button/docs/ButtonExtendedWithStyleParamsExample.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
 import { ButtonProps, Button } from '../Button';
-import { st, classes } from './ButtonExtendedWithStyleParamsExample.st.css';
+import { classes } from './ButtonExtendedWithStyleParamsExample.st.css';
+import { ReactComponent as Heart } from '../../../assets/icons/Heart.svg';
+import { ReactComponent as Share } from '../../../assets/icons/Share.svg';
+import { Text, TYPOGRAPHY } from '../../Text';
 
 export const ButtonExtendedWithStyleParamsExample: React.FunctionComponent<ButtonProps> = (
   props,
 ) => (
   <div>
-    <Button upgrade {...props} className={st(classes.root)}>
+    <Text typography={TYPOGRAPHY.smallTitle}>Regular Button</Text>
+    <Button upgrade {...props} className={classes.root}>
+      Button
+    </Button>
+    <Text typography={TYPOGRAPHY.smallTitle}>Button with icons</Text>
+    <Button
+      upgrade
+      {...props}
+      prefixIcon={<Heart />}
+      suffixIcon={<Share />}
+      className={classes.root}
+    >
       Button
     </Button>
   </div>
