baa2807b299af32e017a468a0e7c958713ddb33f
Maksym Butsykin <maksymb@wix.com>
2021-01-05 12:51:35 +0200

    Popover component

--- src/components/Popover/Popover.spec.tsx
+++ src/components/Popover/Popover.spec.tsx
@@ -1,40 +1,67 @@
 import * as React from 'react';
-import { createUniDriverFactory } from 'wix-ui-test-utils/uni-driver-factory';
-import { isUniEnzymeTestkitExists } from 'wix-ui-test-utils/enzyme';
-import { isUniTestkitExists } from 'wix-ui-test-utils/vanilla';
 import { mount } from 'enzyme';
-import { popoverDriverFactory } from './Popover.driver';
+
+import { createDriverFactory } from 'wix-ui-test-utils/driver-factory';
+import { isEnzymeTestkitExists } from 'wix-ui-test-utils/enzyme';
+import { isTestkitExists } from 'wix-ui-test-utils/vanilla';
+
 import { popoverTestkitFactory } from '../../testkit';
 import { popoverTestkitFactory as enzymePopoverTestkitFactory } from '../../testkit/enzyme';
-// import { TPAComponentsProvider, TPAComponentsConfig } from '../TPAComponentsConfig';
+
+import { popoverDriverFactory } from './Popover.driver';
 import { Popover, PopoverProps } from './';
 
 describe('Popover', () => {
-  const createDriver = createUniDriverFactory(popoverDriverFactory);
+  const createDriver = createDriverFactory(popoverDriverFactory);
 
-  const bootstrap = (
-    props: Partial<
-      PopoverProps
-    > = {} /*, contextProps: TPAComponentsConfig = {}*/,
-  ) => {
+  const bootstrap = (props: Partial<PopoverProps> = {}) => {
     return createDriver(
-      // <TPAComponentsProvider value={contextProps}>
-      <Popover {...props} />,
-      // </TPAComponentsProvider>
+      <Popover placement="top" {...props}>
+        <Popover.Element>
+          <span>Trigger Element</span>
+        </Popover.Element>
+        <Popover.Content>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus, ut.
+          </span>
+        </Popover.Content>
+      </Popover>,
     );
   };
 
   it('should render', async () => {
     const driver = bootstrap();
-    expect(await driver.exists()).toBe(true);
+    expect(driver.exists()).toBe(true);
+  });
+
+  describe('behaviour', () => {
+    it('should open on element click', async () => {
+      const driver = bootstrap();
+
+      expect(driver.isContentElementExists()).toBeFalsy();
+      await driver.click();
+      expect(driver.isContentElementExists()).toBeTruthy();
+    });
   });
 
   describe('testkit', () => {
     it('should exist', async () => {
       expect(
-        await isUniTestkitExists(<Popover />, popoverTestkitFactory, {
-          dataHookPropName: 'data-hook',
-        }),
+        isTestkitExists(
+          <Popover placement="top">
+            <Popover.Element>
+              <span>element</span>
+            </Popover.Element>
+            <Popover.Content>
+              <div>content</div>
+            </Popover.Content>
+          </Popover>,
+          popoverTestkitFactory,
+          {
+            dataHookPropName: 'data-hook',
+          },
+        ),
       ).toBe(true);
     });
   });
@@ -42,8 +69,15 @@ describe('Popover', () => {
   describe('enzyme testkit', () => {
     it('should exist', async () => {
       expect(
-        await isUniEnzymeTestkitExists(
-          <Popover />,
+        isEnzymeTestkitExists(
+          <Popover placement="top">
+            <Popover.Element>
+              <span>element</span>
+            </Popover.Element>
+            <Popover.Content>
+              <div>content</div>
+            </Popover.Content>
+          </Popover>,
           enzymePopoverTestkitFactory,
           mount,
           {
