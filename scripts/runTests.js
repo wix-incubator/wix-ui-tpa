b05d1897cc4aaa9c6ab13fc52c3cac56c65a875a
Jonathan Adler <jonathana@wix.com>
2021-01-10 11:43:26 +0200

    use draflog - hooray

--- scripts/runTests.js
+++ scripts/runTests.js
@@ -1,54 +1,86 @@
-const packageJson = require('../package.json');
 const { spawn } = require('child_process');
+const readline = require('readline');
+const packageJson = require('../package.json');
+require('draftlog').into(console).addLineListener(process.stdin)
 
 const stdouts = {};
 const stderrs = {};
 const errors = {};
-const tests = Object.keys(packageJson.scripts).filter(scriptName => scriptName.indexOf('test:') === 0 && scriptName.indexOf(':watch') === -1);
-let processCount = tests.length;
 
-tests.forEach(script => {
-  console.log(script);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  console.log('Running', script);
-  console.log('===================');
-  const startTime = Date.now();
-  const spawned = spawn('npm', ['run', script]);
+function createSingleTestLogger (dy, prefix = '', numberOfRows) {
+  return (txt) => {
+    readline.cursorTo(process.stdout, 0, dy + 2, () => {
+      rl.write(`${prefix} ${txt}`);
+      readline.cursorTo(process.stdout, 0, numberOfRows + 2);
+    })
+  }
+}
 
-  spawned.stdout.on('data', (data) => {
-    stdouts[script] = `${stdouts[script] || ''}${data}`;
+console.clear();
+
+runTests();
+
+function runTests() {
+  const tests = Object.keys(packageJson.scripts).filter(scriptName => scriptName.indexOf('test:') === 0 && scriptName.indexOf(':watch') === -1);
+  let processCount = tests.length;
+  console.log(`Running ${processCount} tests:\n`)
+
+  tests.forEach((script, index) => {
+    const prefix = `${index + 1}. ${script}`;
+    const ellipsis = '...';
+    let ellipsisCount = 0;
+    const logger = console.draft(prefix, `running${ellipsis}`);
+    const interval = setInterval(() => {
+      ellipsisCount = (ellipsisCount + 1) % 4;
+      logger(prefix, `running${ellipsis.substr(0, ellipsisCount)}`);
+    }, 500);
+
+    const startTime = Date.now();
+    const spawned = spawn('npm', ['run', script]);
+
+    spawned.stdout.on('data', (data) => {
+      stdouts[script] = `${stdouts[script] || ''}${data}`;
+    });
+
+    spawned.stderr.on('data', (data) => {
+      stdouts[script] = `${stdouts[script] || ''}${data}`;
+      stderrs[script] = `${stderrs[script] || ''}${data}`;
+    });
+
+    spawned.on('close', (code) => {
+      clearInterval(interval);
+      const endTime = `${(Date.now() - startTime) / 1000.} seconds`;
+      processCount -= 1;
+
+      if (code !== 0) {
+        errors[script] = code;
+        logger(prefix, `failed after ${endTime} with code ${code}`);
+      } else {
+        logger(prefix, `successfully ended after ${endTime}`);
+      }
+
+      if (processCount === 0) {
+        finalize(tests);
+      }
+    });
   });
+}
 
-  spawned.stderr.on('data', (data) => {
-    stderrs[script] = `${stderrs[script] || ''}${data}`;
-  });
-
-  spawned.on('close', (code) => {
-    processCount -= 1;
-    console.log('Script', script, 'ended', code === 0 ? 'successfully' : 'with an error', `by ${(Date.now() - startTime) / 1000.} seconds`);
-    console.log('adler', 'runTests.js:29', processCount);
-    if (code !== 0) {
-      errors[script] = code;
-    }
-
-    if (processCount === 0) {
-      finalize();
-    }
-  });
-})
-
-function finalize () {
-  console.log('Finished running tests');
+function finalize (tests) {
   console.log('Summary:');
   tests.filter(script => !errors[script]).forEach(script => {
     console.log(script);
     console.log(stdouts[script]);
   })
-  Object.keys(stderrs).forEach(script => {
+  Object.keys(errors).forEach(script => {
     console.log(script);
     console.log(stderrs[script]);
   })
-  console.log('===================');
   const errorCodes = Object.keys(errors);
 
   if (errorCodes.length) {
