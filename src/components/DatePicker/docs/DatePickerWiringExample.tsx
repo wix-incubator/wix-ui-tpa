2f0459e397983ece672fce72ec9dad0d75a12250
sivanharel <sivanh@wix.com>
2021-01-17 10:20:32 +0200

    DatePicker - Adaptive to font change

--- src/components/DatePicker/docs/DatePickerWiringExample.tsx
+++ src/components/DatePicker/docs/DatePickerWiringExample.tsx
@@ -11,11 +11,29 @@ export const DatePickerWiringExample = class DatePickerExample extends React.Com
   render() {
     const { value } = this.state;
     return (
-      <DatePicker
-        className={classes.root}
-        value={value}
-        onChange={this._onChange}
-      />
+      <>
+          <DatePicker
+            className={classes.root}
+            value={value}
+            onChange={this._onChange}
+          />
+          <br/>
+          <br/>
+          <h3>Calendar With Indications:</h3>
+          <br/>
+          <DatePicker
+            className={classes.root}
+            value={value}
+            onChange={this._onChange}
+            renderDateIndication={date => (
+                date <= new Date() ? (
+                    <div className="Indications" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                      <div className="indication" style={{ borderRadius: '50%', width: '0.2em', height: '0.2em', backgroundColor: '#ED24D9' }}/>
+                    </div>
+                ) : null)
+            }
+          />
+      </>
     );
   }
 };
