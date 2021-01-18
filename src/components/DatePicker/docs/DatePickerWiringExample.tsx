bab793a65d220d4e1dfa87a6a232950b6e66b82f
sivanharel <sivanh@wix.com>
2021-01-18 15:38:14 +0200

    remove br

--- src/components/DatePicker/docs/DatePickerWiringExample.tsx
+++ src/components/DatePicker/docs/DatePickerWiringExample.tsx
@@ -17,38 +17,37 @@ export const DatePickerWiringExample = class DatePickerExample extends React.Com
           value={value}
           onChange={this._onChange}
         />
-        <br />
-        <br />
-        <h3>Calendar With Indications:</h3>
-        <br />
-        <DatePicker
-          className={classes.root}
-          value={value}
-          onChange={this._onChange}
-          dateIndication={(date) =>
-            date <= new Date() ? (
-              <div
-                className="Indications"
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <div
-                  className="indication"
-                  style={{
-                    borderRadius: '50%',
-                    width: '0.2em',
-                    height: '0.2em',
-                    backgroundColor: '#ED24D9',
-                  }}
-                />
-              </div>
-            ) : null
-          }
-        />
+        <div style={{ marginTop: '40px' }}>
+            <h2>With Indications:</h2>
+            <DatePicker
+              className={classes.root}
+              value={value}
+              onChange={this._onChange}
+              dateIndication={({ date }) =>
+                date <= new Date() ? (
+                  <div
+                    className="Indications"
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <div
+                      className="indication"
+                      style={{
+                        borderRadius: '50%',
+                        width: '0.3em',
+                        height: '0.3em',
+                        backgroundColor: '#ED24D9',
+                      }}
+                    />
+                  </div>
+                ) : null
+              }
+            />
+        </div>
       </>
     );
   }
