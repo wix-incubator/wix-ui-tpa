845a2b132be450a945bb9b545b98484d38f426d2
nitayneeman <nitayneeman@users.noreply.github.com>
2021-01-18 18:55:42 +0200

    feat: implement dimensions logic

--- src/components/Image/ImageUtils.spec.tsx
+++ src/components/Image/ImageUtils.spec.tsx
@@ -1,10 +1,50 @@
 import { calculateDimensions } from './ImageUtils';
 
 describe('ImageUtils', () => {
-  it('should return the dimensions as are', async () => {
-    const expectedDimensions = { width: 480, height: 360 };
+  const sampleDimensions = { width: 480, height: 360 };
+  const sampleAspectRatio = 1.77;
 
-    const calculatedDimensions = calculateDimensions(expectedDimensions);
+  it('should return the dimensions as are', async () => {
+    const calculatedDimensions = calculateDimensions(sampleDimensions);
+
+    expect(calculatedDimensions).toEqual(sampleDimensions);
+  });
+
+  it('should calculate both dimensions by the given aspect ratio', async () => {
+    const expectedDimensions = {
+      width: sampleDimensions.width / sampleAspectRatio,
+      height: sampleDimensions.height / sampleAspectRatio,
+    };
+    const calculatedDimensions = calculateDimensions({
+      ...sampleDimensions,
+      aspectRatio: sampleAspectRatio,
+    });
+
+    expect(calculatedDimensions).toEqual(expectedDimensions);
+  });
+
+  it('should calculate the missing height by the given width and aspect ratio', async () => {
+    const expectedDimensions = {
+      width: sampleDimensions.width,
+      height: sampleDimensions.height / sampleAspectRatio,
+    };
+    const calculatedDimensions = calculateDimensions({
+      width: sampleDimensions.width,
+      aspectRatio: sampleAspectRatio,
+    });
+
+    expect(calculatedDimensions).toEqual(expectedDimensions);
+  });
+
+  it('should calculate the missing width by the given height and aspect ratio', async () => {
+    const expectedDimensions = {
+      width: sampleDimensions.width / sampleAspectRatio,
+      height: sampleDimensions.height,
+    };
+    const calculatedDimensions = calculateDimensions({
+      height: sampleDimensions.height,
+      aspectRatio: sampleAspectRatio,
+    });
 
     expect(calculatedDimensions).toEqual(expectedDimensions);
   });
