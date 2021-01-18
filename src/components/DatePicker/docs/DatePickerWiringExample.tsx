537cbd33e48145c7f1e1bf722a0f6b67db7e0615
sivanharel <sivanh@wix.com>
2021-01-18 17:27:59 +0200

    remove br

--- src/components/DatePicker/docs/DatePickerWiringExample.tsx
+++ src/components/DatePicker/docs/DatePickerWiringExample.tsx
@@ -18,35 +18,35 @@ export const DatePickerWiringExample = class DatePickerExample extends React.Com
           onChange={this._onChange}
         />
         <div style={{ marginTop: '40px' }}>
-            <h2>With Indications:</h2>
-            <DatePicker
-              className={classes.root}
-              value={value}
-              onChange={this._onChange}
-              dateIndication={({ date }) =>
-                date <= new Date() ? (
+          <h2>With Indications:</h2>
+          <DatePicker
+            className={classes.root}
+            value={value}
+            onChange={this._onChange}
+            dateIndication={({ date }) =>
+              date <= new Date() ? (
+                <div
+                  className="Indications"
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
                   <div
-                    className="Indications"
+                    className="indication"
                     style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
+                      borderRadius: '50%',
+                      width: '0.3em',
+                      height: '0.3em',
+                      backgroundColor: '#ED24D9',
                     }}
-                  >
-                    <div
-                      className="indication"
-                      style={{
-                        borderRadius: '50%',
-                        width: '0.3em',
-                        height: '0.3em',
-                        backgroundColor: '#ED24D9',
-                      }}
-                    />
-                  </div>
-                ) : null
-              }
-            />
+                  />
+                </div>
+              ) : null
+            }
+          />
         </div>
       </>
     );
