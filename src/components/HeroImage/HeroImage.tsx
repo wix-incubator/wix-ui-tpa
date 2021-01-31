09300e1407f55f2316d888564802ee6a29c814c7
nitayneeman <nitayneeman@users.noreply.github.com>
2021-01-31 11:50:02 +0200

    feat: change default resize

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import { Image } from '../Image';
+import { Image, ResizeOptions } from '../Image';
 import { classes, st } from './HeroImage.st.css';
 import { HeroImageProps } from './types';
+
+type DefaultProps = Pick<HeroImageProps, 'resize'>;
 
 /** HeroImage is a component representing a header with high presence providing context about the page content */
 export class HeroImage extends React.Component<HeroImageProps> {
   static displayName = 'HeroImage';
+  static defaultProps: DefaultProps = {
+    resize: ResizeOptions.cover,
+  };
 
   render() {
     const { className, ...imageProps } = this.props;
