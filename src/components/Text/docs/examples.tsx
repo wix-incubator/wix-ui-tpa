5fb3a27667eeae9f1ae608e0cd11b4cace0459fc
Justas Jaskulis <justasj@wix.com>
2021-02-02 21:46:21 +0200

    update storybook

--- src/components/Text/docs/examples.tsx
+++ src/components/Text/docs/examples.tsx
@@ -1,3 +1,10 @@
+import { TEXT_PRIORITY } from '../constants';
+
 export const importExample = `import { Text } from 'wix-ui-tpa/Text';`;
 
 export const basic = `<Text>Some text</Text>`;
+
+export const priority = (() =>
+  `<div>${Object.values(TEXT_PRIORITY)
+    .map((p) => `<p><Text priority='${p}'>Some text</Text></p>`)
+    .join('')}</div>`)();
--- src/components/Text/docs/index.story.tsx
+++ src/components/Text/docs/index.story.tsx
@@ -3,7 +3,6 @@ import * as TextWiringExampleCSSRaw from '!raw-loader!./TextWiringExample.st.css
 import * as TextWiringExampleRaw from '!raw-loader!./TextWiringExample.tsx';
 import {
   api,
-  code as baseCode,
   description,
   divider,
   header,
@@ -13,8 +12,9 @@ import {
   tabs,
   testkit,
   title,
+  example as baseExample,
 } from 'wix-storybook-utils/Sections';
-import { Text, TYPOGRAPHY } from '..';
+import { Text, TYPOGRAPHY, TEXT_PRIORITY } from '..';
 import { allComponents } from '../../../../stories/utils/allComponents';
 import { settingsApi } from '../../../../stories/utils/SettingsApi';
 import {
@@ -25,8 +25,11 @@ import * as examples from './examples';
 import { TextWiringExample } from './TextWiringExample';
 import { StoryCategory } from '../../../../stories/storyHierarchy';
 
-const code = (config) =>
-  baseCode({ components: allComponents, compact: true, ...config });
+const example = (config) =>
+  baseExample({
+    components: allComponents,
+    ...config,
+  });
 
 export default {
   category: StoryCategory.COMPONENTS,
@@ -42,6 +45,7 @@ export default {
   },
   exampleProps: {
     typography: Object.keys(TYPOGRAPHY).map((key) => TYPOGRAPHY[key]),
+    priority: Object.keys(TEXT_PRIORITY).map((key) => TEXT_PRIORITY[key]),
   },
   sections: [
     header(),
@@ -66,7 +70,13 @@ export default {
               title: 'Basic Usage',
               source: examples.basic,
             },
-          ].map(code),
+            {
+              title: 'Priority',
+              text:
+                'Control text priority. Possible values:  `primary`, `secondary`',
+              source: examples.priority,
+            },
+          ].map(example),
         ],
       }),
 
