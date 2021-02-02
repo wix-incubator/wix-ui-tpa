e8eda504fe7b83c0bb8bf56e3e0e14f6cb9a8694
Jonathan Adler <jonathana@wix.com>
2021-02-02 16:54:02 +0200

    add box theme to radio button settings panel example

--- src/components/RadioButton/docs/RadioButtonWiringExample.tsx
+++ src/components/RadioButton/docs/RadioButtonWiringExample.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { RadioButton } from '../';
-import { st, classes } from './RadioButtonWiringExample.st.css';
+import { RadioButton, RadioButtonTheme } from '../';
+import { classes, st } from './RadioButtonWiringExample.st.css';
 
 interface IState {
   isChecked: boolean;
@@ -11,7 +11,7 @@ class RadioButtonWiringExample extends React.Component<{}, IState> {
   state = {
     isChecked: false,
   };
-  handleChange = (event) => {
+  handleChange = () => {
     this.setState((prevState) => {
       return {
         isChecked: !prevState.isChecked,
@@ -22,13 +22,24 @@ class RadioButtonWiringExample extends React.Component<{}, IState> {
   render() {
     const { isChecked } = this.state;
     return (
-      <RadioButton
-        label={label}
-        checked={isChecked}
-        onChange={this.handleChange}
-        value={label}
-        className={st(classes.root)}
-      />
+      <>
+        <RadioButton
+          label={label}
+          checked={isChecked}
+          onChange={this.handleChange}
+          value={label}
+          className={st(classes.root)}
+        />
+        <br />
+        <RadioButton
+          label={label}
+          checked={isChecked}
+          theme={RadioButtonTheme.Box}
+          onChange={this.handleChange}
+          value={label}
+          className={st(classes.root)}
+        />
+      </>
     );
   }
 }
