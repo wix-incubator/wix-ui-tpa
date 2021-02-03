e5c04988ad0fc24e4ef4d26e20c03ea80ba23619
Jonathan Adler <jonathana@wix.com>
2021-02-03 16:59:21 +0200

    fix badge perf tests

--- src/components/Badge/perf/BadgeExtendedGood.tsx
+++ src/components/Badge/perf/BadgeExtendedGood.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Badge } from '../';
-import { st, classes } from './BadgeExtended.st.css';
+import { st, classes } from './BadgeExtendedGood.st.css';
 
 export const BadgePerfer = () => {
   return <Badge className={st(classes.good)}>text</Badge>;
