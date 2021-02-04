505206f58a87eaba91480ea3ddb54e189b401cee
Jonathan Adler <jonathana@wix.com>
2021-02-04 12:37:49 +0200

    test(perfer): Fix badge perfer tests

--- src/components/Badge/perf/BadgeExtendedBad.tsx
+++ src/components/Badge/perf/BadgeExtendedBad.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Badge } from '../';
-import { st, classes } from './BadgeExtended.st.css';
+import { st, classes } from './BadgeExtendedBad.st.css';
 
 export const BadgePerfer = () => {
   return <Badge className={st(classes.bad)}>text</Badge>;
