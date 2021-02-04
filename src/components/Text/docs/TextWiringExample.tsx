0f80cdaa94d8fbf6557d0e4fa9413e25b10fe78c
Justas Jaškulis <6427408+jjaskulis@users.noreply.github.com>
2021-02-04 14:35:09 +0200

    feat(Text) - add priority prop (#646)
    * add priority prop for <Text />
    
    * update storybook
    
    * add tests
    
    * add priority prop for <Text />
    
    * update storybook
    
    * add tests
    
    * increase perfer thresholds
    
    * update perfer baselines
    
    * update the typography labels in the playground dropdown.
    And update the Tag component bundle size
    
    * update the typography labels in the playground dropdown.
    And update the Tag component bundle size
    
    Co-authored-by: sivanharel <sivanh@wix.com>

--- src/components/Text/docs/TextWiringExample.tsx
+++ src/components/Text/docs/TextWiringExample.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { TEXT_PRIORITY } from '../constants';
 import { Text } from '../Text';
 import { classes } from './TextWiringExample.st.css';
 
 export const TextWiringExample = () => (
-  <Text className={classes.root}>Some text</Text>
+  <>
+    <Text className={classes.root}>Primary Priority</Text> <br />
+    <Text className={classes.root} priority={TEXT_PRIORITY.secondary}>
+      Secondary Priority
+    </Text>
+  </>
 );
