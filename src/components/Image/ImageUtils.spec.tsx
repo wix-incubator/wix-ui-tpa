f9700b392dc3d498f3a0bc501ebb52fa274861d9
nitayneeman <nitayneeman@users.noreply.github.com>
2021-02-07 14:32:24 +0200

    test: implement focalPoint

--- src/components/Image/ImageUtils.spec.tsx
+++ src/components/Image/ImageUtils.spec.tsx
@@ -1,74 +1,98 @@
-import { calculateDimensions } from './ImageUtils';
+import {
+  calculateDimensions,
+  resolveFocalPointCoordinates,
+} from './ImageUtils';
+import { FocalPointPresets } from './types';
 
 describe('ImageUtils', () => {
-  const sampleDimensions = { width: 480, height: 360 };
-  const sampleAspectRatio = 1.77;
+  describe('dimensions', () => {
+    const sampleDimensions = { width: 480, height: 360 };
+    const sampleAspectRatio = 1.77;
 
-  it('should return the dimensions as are', async () => {
-    const calculatedDimensions = calculateDimensions(sampleDimensions);
+    it('should return the dimensions as are', async () => {
+      const calculatedDimensions = calculateDimensions(sampleDimensions);
 
-    expect(calculatedDimensions).toEqual(sampleDimensions);
-  });
-
-  it('should calculate the height when having both dimensions and aspect ratio', async () => {
-    const { width } = sampleDimensions;
-    const expectedDimensions = {
-      width,
-      height: Math.round(width / sampleAspectRatio),
-    };
-    const calculatedDimensions = calculateDimensions({
-      ...sampleDimensions,
-      aspectRatio: sampleAspectRatio,
+      expect(calculatedDimensions).toEqual(sampleDimensions);
     });
 
-    expect(calculatedDimensions).toEqual(expectedDimensions);
-  });
+    it('should calculate the height when having both dimensions and aspect ratio', async () => {
+      const { width } = sampleDimensions;
+      const expectedDimensions = {
+        width,
+        height: Math.round(width / sampleAspectRatio),
+      };
+      const calculatedDimensions = calculateDimensions({
+        ...sampleDimensions,
+        aspectRatio: sampleAspectRatio,
+      });
 
-  it('should calculate the missing height by the given width and aspect ratio', async () => {
-    const { width } = sampleDimensions;
-    const expectedDimensions = {
-      width,
-      height: Math.round(width / sampleAspectRatio),
-    };
-    const calculatedDimensions = calculateDimensions({
-      width,
-      aspectRatio: sampleAspectRatio,
+      expect(calculatedDimensions).toEqual(expectedDimensions);
     });
 
-    expect(calculatedDimensions).toEqual(expectedDimensions);
-  });
+    it('should calculate the missing height by the given width and aspect ratio', async () => {
+      const { width } = sampleDimensions;
+      const expectedDimensions = {
+        width,
+        height: Math.round(width / sampleAspectRatio),
+      };
+      const calculatedDimensions = calculateDimensions({
+        width,
+        aspectRatio: sampleAspectRatio,
+      });
 
-  it('should calculate the missing width by the given height and aspect ratio', async () => {
-    const { height } = sampleDimensions;
-    const expectedDimensions = {
-      width: Math.round(height * sampleAspectRatio),
-      height,
-    };
-    const calculatedDimensions = calculateDimensions({
-      height,
-      aspectRatio: sampleAspectRatio,
+      expect(calculatedDimensions).toEqual(expectedDimensions);
     });
 
-    expect(calculatedDimensions).toEqual(expectedDimensions);
+    it('should calculate the missing width by the given height and aspect ratio', async () => {
+      const { height } = sampleDimensions;
+      const expectedDimensions = {
+        width: Math.round(height * sampleAspectRatio),
+        height,
+      };
+      const calculatedDimensions = calculateDimensions({
+        height,
+        aspectRatio: sampleAspectRatio,
+      });
+
+      expect(calculatedDimensions).toEqual(expectedDimensions);
+    });
+
+    it('should return `null` when both dimensions are missing', async () => {
+      const calculatedDimensions = calculateDimensions({});
+
+      expect(calculatedDimensions).toBeNull();
+    });
+
+    it('should return `null` when having width but aspect ratio is missing', async () => {
+      const { width } = sampleDimensions;
+      const calculatedDimensions = calculateDimensions({ width });
+
+      expect(calculatedDimensions).toBeNull();
+    });
+
+    it('should return `null` when having height but aspect ratio is missing', async () => {
+      const { height } = sampleDimensions;
+      const calculatedDimensions = calculateDimensions({ height });
+
+      expect(calculatedDimensions).toBeNull();
+    });
   });
 
-  it('should return `null` when both dimensions are missing', async () => {
-    const calculatedDimensions = calculateDimensions({});
+  describe('focal point', () => {
+    it('should return an object of coordinates as is', async () => {
+      const sampleCoordinates = { x: 120, y: 120 };
+      const coordinates = resolveFocalPointCoordinates(sampleCoordinates);
 
-    expect(calculatedDimensions).toBeNull();
-  });
+      expect(coordinates).toEqual(sampleCoordinates);
+    });
 
-  it('should return `null` when having width but aspect ratio is missing', async () => {
-    const { width } = sampleDimensions;
-    const calculatedDimensions = calculateDimensions({ width });
+    it('should return an object of coordinates when having preset', async () => {
+      const [[samplePreset, sampleCoordinates]] = Object.entries(
+        FocalPointPresets,
+      );
+      const coordinates = resolveFocalPointCoordinates(samplePreset);
 
-    expect(calculatedDimensions).toBeNull();
-  });
-
-  it('should return `null` when having height but aspect ratio is missing', async () => {
-    const { height } = sampleDimensions;
-    const calculatedDimensions = calculateDimensions({ height });
-
-    expect(calculatedDimensions).toBeNull();
+      expect(coordinates).toEqual(sampleCoordinates);
+    });
   });
 });
